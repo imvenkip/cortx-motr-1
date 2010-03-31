ceaaa235390b028c33249ee5a508c0a309d22202
nikita <nikita.danilov@horizontalscale.com>
2010-04-01 00:31:44 +0400

    add DTM stubs

---
+++ src/dtm/dtm.h
@@ -0,0 +1,27 @@
+/* -*- C -*- */
+
+#ifndef __DTM_DTM_H__
+#define __DTM_DTM_H__
+
+/**
+   @defgroup dtm Distributed transaction manager
+   @{
+*/
+
+struct c2_dtm;
+struct c2_dtx;
+
+/** @} end of copymachine group */
+
+/* __DTM_DTM_H__ */
+#endif
+
+/* 
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
