c5e78cc9a2af445e8f42ce6f19fdc07a37bb9868
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-04-11 21:51:12 +0300

    rename xdr_client_id to xdr_node_id

--- net/net_xdr.c
+++ net/net_xdr.c
@@ -1,10 +1,9 @@
-bool_t
-xdr_client_id (XDR *xdrs, client_id *objp)
-{
-	register int32_t *buf;
+#include "lib/cdefs.h"
+#include "net/net.h"
 
-	int i;
-	 if (!xdr_vector (xdrs, (char *)objp->uuid, ARRAY_SIZE(objp->uuid),
+bool xdr_node_id (XDR *xdrs, node_id *objp)
+{
+	if (!xdr_vector (xdrs, (char *)objp->uuid, ARRAY_SIZE(objp->uuid),
 		sizeof (char), (xdrproc_t) xdr_char))
 		 return FALSE;
 	return TRUE;
