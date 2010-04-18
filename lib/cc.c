a273b89e4964817c2268a7dcc2c0a7514cef2330
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-04-18 16:08:54 +0400

    rename c2_rw_lock to c2_rwlock to match function names.

--- lib/cc.c
+++ lib/cc.c
@@ -1,31 +1,31 @@
 #include "lib/cc.h"
 
-void c2_rwlock_init(struct c2_rw_lock *lock)
+void c2_rwlock_init(struct c2_rwlock *lock)
 {
 
 }
 
-void c2_rwlock_fini(struct c2_rw_lock *lock)
+void c2_rwlock_fini(struct c2_rwlock *lock)
 {
 
 }
 
-void c2_rwlock_write_lock(struct c2_rw_lock *lock)
+void c2_rwlock_write_lock(struct c2_rwlock *lock)
 {
 
 }
 
-void c2_rwlock_write_unlock(struct c2_rw_lock *lock)
+void c2_rwlock_write_unlock(struct c2_rwlock *lock)
 {
 
 }
 
-void c2_rwlock_read_lock(struct c2_rw_lock *lock)
+void c2_rwlock_read_lock(struct c2_rwlock *lock)
 {
 
 }
 
-void c2_rwlock_read_unlock(struct c2_rw_lock *lock)
+void c2_rwlock_read_unlock(struct c2_rwlock *lock)
 {
 
 }
--- lib/cc.h
+++ lib/cc.h
@@ -35,17 +35,17 @@ struct c2_mutex {
 /**
    Blocking read-write lock.
 */
-struct c2_rw_lock {
+struct c2_rwlock {
 };
 
-void c2_rwlock_init(struct c2_rw_lock *lock);
-void c2_rwlock_fini(struct c2_rw_lock *lock);
+void c2_rwlock_init(struct c2_rwlock *lock);
+void c2_rwlock_fini(struct c2_rwlock *lock);
 
-void c2_rwlock_write_lock(struct c2_rw_lock *lock);
-void c2_rwlock_write_unlock(struct c2_rw_lock *lock);
+void c2_rwlock_write_lock(struct c2_rwlock *lock);
+void c2_rwlock_write_unlock(struct c2_rwlock *lock);
 
-void c2_rwlock_read_lock(struct c2_rw_lock *lock);
-void c2_rwlock_read_unlock(struct c2_rw_lock *lock);
+void c2_rwlock_read_lock(struct c2_rwlock *lock);
+void c2_rwlock_read_unlock(struct c2_rwlock *lock);
 
 
 struct c2_semaphore {
