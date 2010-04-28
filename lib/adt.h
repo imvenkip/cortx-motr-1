51052fce3fdc8dee59ad4dd66794b89f970b9d19
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-04-28 17:25:18 +0400

    add
        * c2_bindex_t: byte index (offset) in devices, files, objects, containers,
          etc.;
    
        * c2_bcount_t: number of bytes in extent, IO request, buffer, etc.;
    
        * struct c2_outvec: "scatter" part of scatter-gather IO description;
    
        * struct c2_diovec: "gather" part of scatter-gather IO
          description. This is specifica for direct-IO and will be moved
          elsewhere.

--- lib/adt.h
+++ lib/adt.h
@@ -3,6 +3,8 @@
 #ifndef __COLIBRI_ADT_H__
 #define __COLIBRI_ADT_H__
 
+#include <stdbool.h>
+
 /**
    @defgroup adt Basic abstract data types
    @{
@@ -30,10 +32,13 @@ void c2_stack_link_init(struct c2_stack_link *stack);
 void c2_stack_link_fini(struct c2_stack_link *stack);
 bool c2_stack_link_is_in(const struct c2_stack_link *stack);
 
+typedef uint64_t c2_bcount_t;
+typedef uint64_t c2_bindex_t;
+
 /** extent. */
 struct c2_ext {
-	uint64_t e_start;
-	uint64_t e_end;
+	c2_bindex_t e_start;
+	c2_bindex_t e_end;
 };
 
 int c2_ext_are_overlapping(const struct c2_ext *e0, const struct c2_ext *e1);
@@ -49,7 +54,54 @@ void c2_ext_add(const struct c2_ext *term0, const struct c2_ext *term1,
 		struct c2_ext *sum);
 
 /* what about signed? */
-uint64_t c2_ext_cap(const struct c2_ext *ext2, uint64_t val);
+uint64_t c2_ext_cap(const struct c2_ext *ext2, c2_bindex_t val);
+
+struct c2_outseg;
+
+struct c2_outvec {
+	uint32_t          ov_nr;
+	struct c2_outseg *ov_seg;
+};
+
+struct c2_outseg {
+	c2_bindex_t os_index;
+	c2_bcount_t os_nob;
+};
+
+c2_bcount_t c2_outvec_count   (const struct c2_outvec *vec);
+
+struct c2_dioseg;
+/**
+   Array of 4KB aligned areas, suitable for direct-IO.
+
+   @invariant size of every but last element of iovec is a multiple of 4KB.
+ */
+struct c2_diovec {
+	uint32_t          div_nr;
+	struct c2_dioseg *div_seg;
+};
+
+struct c2_dio_cookie;
+struct c2_dio_engine;
+
+struct c2_dioseg {
+	void                 *die_buf;
+	c2_bcount_t           die_nob;
+	struct c2_dio_cookie *die_cookie;
+};
+
+enum {
+	C2_DIOVEC_SHIFT = 12,
+	C2_DIOVEC_ALIGN = (1 << C2_DIOVEC_SHIFT),
+	C2_DIOVEC_MASK  = ~(c2_bcount_t)(C2_DIOVEC_ALIGN - 1)
+};
+
+int         c2_diovec_alloc   (struct c2_diovec *vec, 
+			       void *start, c2_bcount_t nob);
+void        c2_diovec_free    (struct c2_diovec *vec);
+int         c2_diovec_register(struct c2_diovec *vec, 
+			       struct c2_dio_engine *eng);
+c2_bcount_t c2_diovec_count   (const struct c2_diovec *vec);
 
 /** @} end of adt group */
 
