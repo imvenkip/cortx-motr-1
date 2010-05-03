17298be1b4c83e46218a2a4158d6cfa9db5bd4ad
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-05-03 18:12:05 +0400

    add preliminary fop field interface for reqh.

--- fop/fop.h
+++ fop/fop.h
@@ -1,7 +1,7 @@
 /* -*- C -*- */
 
-#ifndef __COLIBRI_FOP_H__
-#define __COLIBRI_FOP_H__
+#ifndef __COLIBRI_FOP_FOP_H__
+#define __COLIBRI_FOP_FOP_H__
 
 /**
    @defgroup fop File operation packet
@@ -13,28 +13,87 @@
    @{
 */
 
-typedef uint32_t foptype_code_t;
+typedef uint32_t c2_foptype_code_t;
 
 struct foptype {
-	foptype_code_t  ft_code;
-	const char     *ft_name;
-	c2_list_link    ft_linkage;
+	c2_foptype_code_t  ft_code;
+	const char        *ft_name;
+	c2_list_link       ft_linkage;
 
-	int (*ft_incoming)(struct foptype *ftype, struct fop *fop);
+	int              (*ft_incoming)(struct c2_foptype *ftype, 
+					struct c2_fop *fop);
 };
 
-struct fopdata;
-struct fop {
-	struct foptype *f_type;
-	struct fopdate *f_data;
+struct c2_fopdata;
+struct c2_fop {
+	struct c2_foptype *f_type;
+	struct c2_fopdata *f_data;
 };
 
-int  foptype_register  (struct foptype *ftype, struct rpcmachine *rpcm);
-void foptype_unregister(struct foptype *ftype);
+int  c2_foptype_register  (struct c2_foptype *ftype, 
+			   struct c2_rpcmachine *rpcm);
+void c2_foptype_unregister(struct c2_foptype *ftype);
+
+bool c2_fop_is_update(const struct c2_foptype *type);
+bool c2_fop_is_batch (const struct c2_foptype *type);
+
+enum c2_fop_field_kind {
+	FFK_BUILTIN,
+	FFK_STANDARD,
+	FFK_OTHER,
+
+	FFK_NR
+};
+
+enum c2_fop_field_type {
+	FFT_ZERO,
+	FFT_VOID,
+	FFT_BOOL,
+	FFT_BITMASK,
+	FFT_OBJECT,
+	FFT_NAME,
+	FFT_PATH,
+	FFT_PRINCIPAL,
+	FFT_TIMESTAMP,
+	FFT_EPOCH,
+	FFT_VERSION,
+	FFT_OFFSET,
+	FFT_COUNT,
+	FFT_BUFFER,
+	FFT_RESOURCE,
+	FFT_LOCK,
+	FFT_NODE,
+	FFT_FOP,
+
+	FFT_NR
+};
+
+struct c2_fop_field {
+	enum c2_fop_field_kind ff_kind;
+	enum c2_fop_field_type ff_type;
+	const char            *ff_name;
+};
+
+struct c2_fop_field_iterator {
+	struct c2_fop       *ffi_fop;
+};
+
+struct c2_fop_field_val {
+	struct c2_fop       *ffv_fop;
+	struct c2_fop_field *ffv_field;
+	void                *ffv_val;
+};
+
+void c2_fop_iterator_init(struct c2_fop_field_iterator *it, struct c2_fop *fop);
+void c2_fop_iterator_fini(struct c2_fop_field_iterator *it);
+int  c2_fop_iterator_next(struct c2_fop_field_iterator *it,
+			  struct c2_fop_field_val *val);
+
+
 
 /** @} end of fop group */
 
-/* __COLIBRI_FOP_H__ */
+/* __COLIBRI_FOP_FOP_H__ */
 #endif
 
 /* 
