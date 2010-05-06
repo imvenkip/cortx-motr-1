eadc4a7b889ea6bd5692242b847c65c925a2092b
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-05-06 16:34:47 +0300

    add search. convert db return codes into generic errnp

--- lib/cache.c
+++ lib/cache.c
@@ -1,5 +1,17 @@
 #include "lib/memory.h"
 
+int convert_db_error(int db_error)
+{
+	switch (db_error) {
+	case DB_NOTFOUND:
+		return -ENOENT;
+	case DB_LOCK_DEADLOCK:
+		return -EDEADLK;
+	default:
+		return db_error;
+	}
+}
+
 int c2_cache_init(stuct c2_cache *cache, DB_ENV *env, const char *dbname,
 		  uint32_t flags)
 {
@@ -8,12 +20,12 @@ int c2_cache_init(stuct c2_cache *cache, DB_ENV *env, const char *dbname,
 
 	ret = db_create(&cache->c_db, envp, 0);
 	if (ret != 0)
-		return ret;
+		return convert_db_error(ret);
 
 	if (flags != 0) {
 		ret = cache->c_db->set_flags(&cache->c_db, flags);
 		if (ret != 0)
-			return ret;
+			return convert_db_error(ret);
 	}
 
 	open_flags = DB_CREATE              | /* Allow database creation */
@@ -22,7 +34,7 @@ int c2_cache_init(stuct c2_cache *cache, DB_ENV *env, const char *dbname,
 	ret = dbp->open(cache->c_db, NULL, dbname,
 		    NULL, DB_BTREE, open_flags, 0);
 
-	return ret;
+	return convert_db_error(ret);
 }
 
 void c2_cache_fini(struct c2_cache *cache)
@@ -34,12 +46,22 @@ void c2_cache_fini(struct c2_cache *cache)
 int c2_cache_search(struct c2_cache *cache, void *key,
 		    void **result, int *ressize)
 {
-	void *db_key;
-	int key_size;
+	DBT db_key, db_value;
+	int ret;
 
-	cache->c_pkey_enc(key, &db_key, &key_size);
+	memset(&db_key, 0, sizeof(db_key));
+	ret = cache->c_pkey_enc(key, &db_key.data, &db_key.size);
+	if (ret < 0)
+		return ret;
 
+	memset(db_value, 0, sizeof(db_value));
+	/* ask db to make copy */
+	db_value.flags = DB_DBT_MALLOC;
+	ret = cache->c_db->get(cache->c_db, NULL, db_key,
+			       db_value, DB_READ_UNCOMMITTED);
 	c2_free(db_key, key_size);
+
+	return convert_db_error(ret);
 }
 
 int c2_cache_insert(struct c2_cache *cache, DB_TXN *txn,
@@ -62,7 +84,7 @@ int c2_cache_insert(struct c2_cache *cache, DB_TXN *txn,
 
 	c2_free(db_key, key_size);
 
-	return ret;
+	return convert_db_error(ret);
 }
 
 int c2_cache_delete(struct c2_cache *cache, DB_TXN *txn, void *key)
@@ -72,9 +94,12 @@ int c2_cache_delete(struct c2_cache *cache, DB_TXN *txn, void *key)
 
 	memset(&db_key, 0, sizeof(db_key));
 
-	cache->c_pkey_enc(key, &db_key, &key_size);
+	ret = cache->c_pkey_enc(key, &db_key, &key_size);
+	if (ret < 0)
+		return ret;
 
 	ret = cache->c_db->del(cache->c_db, txn, db_key, 0);
 	c2_free(db_key, key_size);
 
+	return convert_db_error(ret);
 }
