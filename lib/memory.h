64084be5d775b6ae69c6c20c8f2494021aaa5d5a
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-05-06 12:51:48 +0400

    remove explicit size parameter from c2_free(). Add c2_allocated() call.

--- lib/memory.h
+++ lib/memory.h
@@ -2,6 +2,8 @@
 
 #define _COLIBRI_LIB_MEMORY_H_
 
+#include <sys/types.h>
+
 /**
    @defgroup memory memory allocation handling functions
    @{
@@ -26,10 +28,22 @@ void *c2_alloc(size_t size);
  *
  * @return none
  */
-void c2_free(void *data, size_t size);
+void c2_free(void *data);
 
-#define C2_FREE_PTR(ptr)	c2_free((ptr), sizeof *ptr)
+/**
+ * Return amount of memory currently allocated.
+ */
+size_t c2_allocated(void);
 
 /** @} end of memory group */
 
 #endif
+/* 
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
