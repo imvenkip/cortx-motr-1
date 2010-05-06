21b8d7987e6d794300d4a70befb8602e0f2860a7
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-05-06 11:13:27 +0300

    comment server side compound handler

--- rpc/compound_srv.h
+++ rpc/compound_srv.h
@@ -4,7 +4,7 @@
 #define _RPC_COMPOUND_SRV_
 
 #include "net/net_types.h"
-#include "rpc/pcache.h"
+#include "lib/cache.h"
 #include "rpc/compound_types.h"
 
 /**
@@ -12,17 +12,41 @@
  */
 
 /**
- handlers for operations incapsulated in compound request
+ handlers for operations encapsulated in compound request
  */
 struct c2_compound_ops {
-	c2_compound_op	co_op;
-	c2_xdrproc_t	co_arg;
-	c2_xdrproc_t	co_ret;
-	int (*co_handler)(void *arg, void *ret);
-	c2_pc_encode_t	co_db_enc;
-	c2_pc_decode_t	co_db_dec;
+	/**
+	 operation identifier
+	 */
+	c2_compound_op		co_op;
+	/**
+	 convert argument from network to host order
+	 */
+	c2_xdrproc_t		co_arg;
+	/**
+	 convert reply from host to network order
+	 */
+	c2_xdrproc_t		co_ret;
+	/**
+	 operation handler function.
+	 handler must allocate reply buffer itself.
+	
+	 @param arg argument supplied from network packet
+	 @param ret pointer to reply
+	 @param retsize size of reply
+	*/
+	int (*co_handler)(void *arg, void **ret, int *retsize);
+	/**
+	 function to encode reply to store in reply cache
+	 */
+	c2_cache_encode_t	co_db_enc;
+	/**
+	 function to decode reply from reply cache
+	 */
+	c2_cache_decode_t	co_db_dec;
 };
 
 int c2_compound_process(const struct c2_compound_op_arg *op,
-			void *reply);
+			void *arg, void **ret, int *retsize);
+
 #endif
