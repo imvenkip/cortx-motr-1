9058f9182d9ce9cfb983da85561ce58ddf26a853
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-05-07 15:29:14 +0400

    * modify atomic interface to use explicit number of bits;
    * add c2_atomic64_{add,sub}() functions.

--- lib/refs.c
+++ lib/refs.c
@@ -3,18 +3,18 @@
 void c2_ref_init(struct c2_ref *ref, int init_num,
 		void (*release) (struct c2_ref *ref))
 {
-	c2_atomic_set(&ref->ref_cnt, init_num);
+	c2_atomic64_set(&ref->ref_cnt, init_num);
 	ref->release = release;
 }
 
 void c2_ref_get(struct c2_ref *ref)
 {
-	c2_atomic_inc(&ref->ref_cnt);
+	c2_atomic64_inc(&ref->ref_cnt);
 }
 
 void c2_ref_put(struct c2_ref *ref)
 {
-	if (!c2_atomic_dec_and_test(&ref->ref_cnt)){
+	if (!c2_atomic64_dec_and_test(&ref->ref_cnt)){
 		ref->release(ref);
 	}
 }
