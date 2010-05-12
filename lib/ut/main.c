d5a99f2df715f0adbf7c003be7d0dbcf77ac3e4d
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-05-13 01:52:15 +0400

    add new types and unit tests to Makefiles.

--- lib/ut/main.c
+++ lib/ut/main.c
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 
+extern void c2_threads_init(void);
+extern void c2_threads_fini(void);
+
 extern void test_memory(void);
 extern void test_list(void);
 extern void test_bitops(void);
@@ -9,9 +12,12 @@ extern void test_bitmap(void);
 extern void test_refs(void);
 extern void test_queue(void);
 extern void test_vec(void);
+extern void test_thread(void);
 
 int main(int argc, char *argv[])
 {
+	c2_threads_init();
+
 	test_memory();
 	test_list();
 	test_bitops();
@@ -19,5 +25,9 @@ int main(int argc, char *argv[])
 	test_refs();
 	test_queue();
 	test_vec();
+	test_thread();
+
+	c2_threads_fini();
+
 	return 0;
 }
