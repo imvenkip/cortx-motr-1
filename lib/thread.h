fd7718d36ca7a8c0bc4396efa5c61cbf626262a1
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-05-13 01:50:59 +0400

    implement c2_thread on top of pthread_t.

--- lib/thread.h
+++ lib/thread.h
@@ -4,6 +4,7 @@
 #define __COLIBRI_LIB_THREAD_H__
 
 #include <sys/types.h>
+#include <pthread.h>
 
 #include "cdefs.h"
 
@@ -13,14 +14,35 @@
  */
 
 struct c2_thread {
-	int t_x;
+	/** POSIX thread identifier for now. */
+	pthread_t  t_id;
+	void     (*t_func)(void *);
+	void      *t_arg;
 };
+
+#define C2_THREAD_INIT(thread, TYPE, func, arg)		\
+({							\
+	typeof(func) __func = (func);			\
+	typeof(arg)  __arg  = (arg);			\
+	TYPE         __dummy;				\
+	(void)(__func == (void (*)(TYPE))NULL);		\
+	(void)(&__arg  == &__dummy);			\
+	c2_thread_init(thread,				\
+		       (void (*)(void *))__func,	\
+		       (void *)(unsigned long)__arg);	\
+})
+
+#define LAMBDA(T, ...) ({ T __lambda __VA_ARGS__; &__lambda; })
 
 int  c2_thread_init(struct c2_thread *q, void (*func)(void *), void *arg);
 void c2_thread_fini(struct c2_thread *q);
-void c2_thread_join(struct c2_thread *q);
+int  c2_thread_join(struct c2_thread *q);
 
-/** @} end of queue group */
+int  c2_threads_init(void);
+void c2_threads_fini(void);
+
+
+/** @} end of thread group */
 
 /* __COLIBRI_LIB_THREAD_H__ */
 #endif
