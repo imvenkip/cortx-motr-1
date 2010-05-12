5e51ca9b41ff25bae7ad79dbfa28d6bf738d070f
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-05-12 12:30:29 +0300

    code reorder.

--- rpc/rpc_internal.h
+++ rpc/rpc_internal.h
@@ -3,46 +3,25 @@
 
 #define _RPC_INTERNAL_
 /**
+ internal structures and functions
+ */
+
+/**
  find empty slot to send request.
  function scan slot table until first slot without busy flag is found and return
- that slot with busy flag set.
+ that slot with busy flag set, to indicate that slot sequence is busy.
 
  @param sess - full initialized session
 
  @retval NULL failure, e.g., not have free slots to send
  @retval !NULL pointer to client slot info
  */
-struct c2_cli_slot *c2_find_unused_slot(struct c2_cli_session *sess);
+struct c2_cli_slot *c2_slot_find_unused(struct c2_cli_session *sess);
 
 /**
- request ordering enumeration
+ release busy flag after finished processing reply.
  */
-enum c2_request_order {
-	/**
-	 request out of order
-	 */
-	REQ_ORD_BAD,
-	/**
-	 normal request in expected order,
-	 sequence id is greater last used by one.
-	 */
-	REQ_ORD_NORMAL,
-	/**
-	 resend of already handled request.
-	 request sequence is same prevoisly hanlded
-	 */
-	REQ_ORD_RESEND,
-};
-
-/**
- * validate a request via sequnce ordering
- * @param sess - server side session info
- * @param cli_seq - client provided info in sequence operation.
- *
- * @return enum request code to describe request status.
- */
-enum c2_request_order c2_check_request(const struct c2_srv_session *sess,
-					const struct c2_session_sequence_args *cli_seq);
+struct c2_slot_unbusy(struct c2_cli_slot *slot);
 
 #endif
 
