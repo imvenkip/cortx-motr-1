d36b8806bb30de60e7ed6ca3b295160dee21c416
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-05-18 13:52:57 +0300

    git simple UT

--- rpc/ut/rpc-client.c
+++ rpc/ut/rpc-client.c
@@ -1,18 +1,23 @@
 #include "net/net.h"
+#include "net/net_types.h"
 
 #include "rpc/rpclib.h"
 
 int main(int argc, char *argv[])
 {
-	const c2_service_id srv_id = { .uuid = "srv-1"; }
+	struct c2_service_id srv_id = { .uuid = "srv-1" };
+	struct c2_rpc_client *cli;
+	int rc;
 
 	c2_rpclib_init();
 
 	/* in config*/
 	rc = c2_net_conn_create(&srv_id, C2_SESSION_PROGRAM, "localhost");
-	CU_ASSERT(rc);
 
-	cli = c2_rpc_client_init(&srv_id);
+	cli = c2_rpc_client_create(&srv_id);
+
 
 	c2_rpc_client_unlink(cli);
+
+	return 0;
 }
