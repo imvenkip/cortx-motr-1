f62545c11efef10c1ed903c0c18a09650270cf72
Alexey Lyashkov <alexey.lyashkov@clusterstor.com>
2010-05-18 15:59:51 +0300

    add xdr converision for session types.

---
+++ rpc/xdr/session.c
@@ -0,0 +1,66 @@
+#include "lib/cdefs.h"
+
+#include <rpc/types.h>
+#include <rpc/xdr.h>
+
+
+#include "net/net_types.h"
+#include "net/xdr.h"
+#include "rpc/rpc_types.h"
+#include "rpc/session_types.h"
+#include "rpc/xdr/common.h"
+#include "rpc/xdr/session.h"
+
+
+bool c2_xdr_session_create_arg(void *x, struct c2_session_create_arg *objp)
+{
+	 if (!c2_xdr_service_id(x, &objp->sca_client))
+		 return false;
+	 if (!c2_xdr_service_id(x, &objp->sca_server))
+		 return false;
+	 if (!xdr_uint32_t(x, &objp->sca_high_slot_id))
+		 return false;
+	 if (!xdr_uint32_t(x, &objp->sca_max_rpc_size))
+		 return false;
+	return true;
+}
+
+bool c2_xdr_session_create_out (void *x, struct c2_session_create_ret *objp)
+{
+	 if (!xdr_int(x, &objp->error))
+		return false;
+
+	/* request failed */
+	if (objp->error)
+		return true;
+
+	 if (!c2_xdr_session_id(x, &objp->sco_session_id))
+		return false;
+	 if (!xdr_uint32_t(x, &objp->sco_high_slot_id))
+		return false;
+	 if (!xdr_uint32_t(x, &objp->sco_max_rpc_size))
+		return false;
+	return true;
+}
+
+/*
+bool_t
+xdr_session_destroy_arg (XDR *xdrs, session_destroy_arg *objp)
+{
+	register int32_t *buf;
+
+	 if (!xdr_session_id (xdrs, &objp->da_session_id))
+		 return FALSE;
+	return TRUE;
+}
+
+bool_t
+xdr_session_destroy_ret (XDR *xdrs, session_destroy_ret *objp)
+{
+	register int32_t *buf;
+
+	 if (!xdr_int32_t (xdrs, &objp->sda_errno))
+		 return FALSE;
+	return TRUE;
+}
+*/
