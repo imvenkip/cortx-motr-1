548ac4e697f86223802c4afa6d238ea45ed04938
Nikita Danilov <nikita.danilov@clusterstor.com>
2010-05-24 18:04:06 +0400

    add stack backtrace to assertion failure output.

--- lib/assert.c
+++ lib/assert.c
@@ -1,21 +1,40 @@
 /* -*- C -*- */
 
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <stdio.h>  /* fprintf, fflush */
 #include <errno.h>
 #include <stdlib.h> /* abort */
+#include <execinfo.h>
 
-#include "assert.h"
+#include "lib/assert.h"
+#include "lib/cdefs.h"
 
 /**
    @addtogroup assert
    @{
 */
 
-int c2_panic(const char *expr, const char *file, int lineno)
+enum {
+	BACKTRACE_DEPTH_MAX = 256
+};
+
+int c2_panic(const char *expr, const char *func, const char *file, int lineno)
 {
-	fprintf(stderr, "Assertion failure: %s at %s:%i (errno: %i)\n",
-		expr, file, lineno, errno);
+	fprintf(stderr, "Assertion failure: %s at %s() %s:%i (errno: %i)\n",
+		expr, func, file, lineno, errno);
 	fflush(stderr);
+#ifdef HAVE_BACKTRACE
+	{
+		void *trace[BACKTRACE_DEPTH_MAX];
+		int nr;
+
+		nr = backtrace(trace, ARRAY_SIZE(trace));
+		backtrace_symbols_fd(trace, nr, 2);
+	}
+#endif
 	abort();
 	return 0;
 }
