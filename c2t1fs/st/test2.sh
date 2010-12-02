744608b959f935514e058219128d98faf93ced80
Anatoliy Bilenko <anatoliy.bilenko@horizontalscale.com>
2010-12-02 15:30:27 +0200

    Updated test script for testing sns-client read/write

--- c2t1fs/st/test2.sh
+++ c2t1fs/st/test2.sh
@@ -42,32 +42,32 @@ mount -t c2t1fs -o layout-data=3,layout-parity=1,objid=12345,objsize=196608,\
 ds=$IPAddr:$Port0,ds=$IPAddr:$Port1,ds=$IPAddr:$Port2,ds=$IPAddr:$Port3,ds=$IPAddr:$Port4\
  $IPAddr:$Port1 /mnt/c2t1fs
 
-# max_count_=3
-# max_bs_=49152
+max_count_=3
+max_bs_=49152
 
-# dd if=/dev/urandom of=dummy.bin bs=$(($max_bs_*$max_count_)) count=1
+dd if=/dev/urandom of=dummy.bin bs=$(($max_bs_*$max_count_)) count=1
 
-# for count_ in {1,2,$max_count_}
-# do
-#      for bs_ in {12288,24576,$max_bs_}
-#      do
-#         dd if=dummy.bin of=/mnt/c2t1fs/12345 bs=$bs_ count=$count_
-#         left=$(dd if=dummy.bin         bs=$bs_ count=$count_ 2>/dev/null | md5sum)
-# 	sleep 2
-#         right=$(dd if=/mnt/c2t1fs/12345 bs=$bs_ count=$count_ 2>/dev/null | md5sum)
-# 	sleep 2
+for count_ in {1,2,$max_count_}
+do
+     for bs_ in {12288,24576,$max_bs_}
+     do
+        dd if=dummy.bin of=/mnt/c2t1fs/12345 bs=$bs_ count=$count_
+        left=$(dd if=dummy.bin         bs=$bs_ count=$count_ 2>/dev/null | md5sum)
+	sleep 2
+        right=$(dd if=/mnt/c2t1fs/12345 bs=$bs_ count=$count_ 2>/dev/null | md5sum)
+	sleep 2
 	
-# 	if [ "$left" == "$right" ]
-# 	then
-# 	    echo "test {$count_,$bs_} passed."
-# 	else
-# 	    echo "test {$count_,$bs_} failed."
-# 	fi
-#      done
-#  done
-# rm dummy.bin
+	if [ "$left" == "$right" ]
+	then
+	    echo "test {$count_,$bs_} passed."
+	else
+	    echo "test {$count_,$bs_} failed."
+	fi
+     done
+ done
+rm dummy.bin
 
-# umount /mnt/c2t1fs
-# modunload
-# killall lt-server
+umount /mnt/c2t1fs
+modunload
+killall lt-server
 echo ======================done=====================
