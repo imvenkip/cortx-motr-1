07dc60ee15e380da5676d4e6a426cc7e6f7ff96a
Dave Cohrs <dave_cohrs@us.xyratex.com>
2011-02-24 10:14:00 -0800

    coding of bitmap and thread complete

---
+++ lib/bitmap.c
@@ -0,0 +1,75 @@
+/* -*- C -*- */
+
+#include "lib/bitmap.h"
+#include "lib/cdefs.h"
+#include "lib/assert.h"
+#include "lib/errno.h"
+#include "lib/memory.h"
+
+/**
+   @defgroup bitmap Bitmap
+   @{
+ */
+
+/**
+   Number of bits in a word (c2_bitmap.b_words).
+ */
+#define C2_BITMAP_BITS (8 * sizeof ((struct c2_bitmap *)0)->b_words[0])
+
+/**
+   Mask off a single bit within a word.
+   Use C2_BITMAP_WORDS()-1 to select the correct word, then use C2_BITMAP_MASK to
+   access the individual bit within that word.
+   
+   @param idx bit offset into the bitmap
+ */
+#define C2_BITMAP_MASK(idx) (1 << ((idx) % C2_BITMAP_BITS))
+
+int c2_bitmap_init(struct c2_bitmap *map, size_t nr)
+{
+	int ret = 0;
+
+	map->b_nr = nr;
+	C2_ALLOC_ARR(map->b_words, C2_BITMAP_WORDS(nr));
+	if (map->b_words == NULL)
+		ret = -ENOMEM;
+	return ret;
+}
+C2_EXPORTED(c2_bitmap_init);
+
+void c2_bitmap_fini(struct c2_bitmap *map)
+{
+	c2_free(map->b_words);
+}
+C2_EXPORTED(c2_bitmap_fini);
+
+bool c2_bitmap_get(const struct c2_bitmap *map, size_t idx)
+{
+	bool result = false;
+	if (idx < map->b_nr)
+		result = ((map->b_words[C2_BITMAP_WORDS(idx)-1] & C2_BITMAP_MASK(idx)) != 0);
+	return result;
+}
+C2_EXPORTED(c2_bitmap_get);
+
+void c2_bitmap_set(struct c2_bitmap *map, size_t idx, bool val)
+{
+	C2_ASSERT(idx < map->b_nr);
+	if (val)
+		map->b_words[C2_BITMAP_WORDS(idx)-1] |= C2_BITMAP_MASK(idx);
+	else
+		map->b_words[C2_BITMAP_WORDS(idx)-1] &= ~C2_BITMAP_MASK(idx);
+}
+C2_EXPORTED(c2_bitmap_set);
+
+/** @} end of bitmap group */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
---
+++ lib/linux_kernel/thread.c
@@ -0,0 +1,66 @@
+/* -*- C -*- */
+
+#include "lib/misc.h"   /* C2_SET0 */
+#include "lib/thread.h"
+#include "lib/arith.h"
+#include "lib/bitmap.h"
+#include "lib/assert.h"
+
+#include <linux/pid_namespace.h>
+
+/**
+   @addtogroup thread Thread
+
+   Implementation of c2_thread on top of struct task_struct.
+
+   @{
+ */
+
+int c2_thread_confine(struct c2_thread *q, const struct c2_bitmap *processors)
+{
+	int                 result;
+	long                idx;
+	size_t              nr_bits = min64u(processors->b_nr, nr_cpu_ids);
+	cpumask_var_t       cpuset;
+	struct task_struct *p;
+
+	rcu_read_lock();
+	p = pid_task(find_pid_ns(q->t_h.h_id, current->nsproxy->pid_ns), PIDTYPE_PID);
+	if (p == NULL) {
+		rcu_read_unlock();
+		return -ESRCH;
+	}
+
+	/* lock p */
+	get_task_struct(p);
+	rcu_read_unlock();
+
+	if (!alloc_cpumask_var(&cpuset, GFP_KERNEL)) {
+		put_task_struct(p);
+		return -ENOMEM;
+	}
+	cpumask_clear(cpuset);
+
+	for (idx = 0; idx < nr_bits; ++idx) {
+		if (c2_bitmap_get(processors, idx))
+			cpumask_set_cpu(idx, cpuset);
+	}
+
+	result = set_cpus_allowed_ptr(p, cpuset);
+	free_cpumask_var(cpuset);
+	put_task_struct(p);
+	return result;
+}
+C2_EXPORTED(c2_thread_confine);
+
+/** @} end of thread group */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
