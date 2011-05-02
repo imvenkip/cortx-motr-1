618152e0201d0e77683fa591e5a049f7bd6f3760
Nikita Danilov <nikita_danilov@xyratex.com>
2011-05-02 20:36:21 +0400

    a prototype of a generic state machine helper code.

--- sm/sm.c
+++ sm/sm.c
@@ -7,79 +7,70 @@
    @{
 */
 
-int c2_sm_init(struct c2_sm *mach)
+static bool is_subset(uint64_t a, uint64_t b)
 {
-	return 0;
+	return (a & b) == a;
+}
+
+static bool belongs(uint64_t a, uint64_t b)
+{
+	C2_PRE(c2_is_po2(a));
+	return a & b;
+}
+
+static bool c2_sm_invariant(const struct c2_sm *mach)
+{
+	struct c2_sm_conf *c;
+
+	c = mach->sm_conf;
+	return is_subset(c->scf_start | c->scf_failure | c->scf_terminal |
+			 c->scf_final, c->scf_valid) &&
+		c2_is_po2(mach->sm_state) &&
+		belongs(mach->sm_state, c->scf_valid);
+}
+
+void c2_sm_init(struct c2_sm *mach, const struct c2_sm_conf *conf)
+{
+	mach->sm_state = conf->scf_start;
+	mach->sm_conf  = conf;
+	mach->sm_rc    = 0;
+	c2_mutex_init(&mach->sm_lock);
+	c2_chan_init(&mach->sm_chan);
+	C2_POST(c2_sm_invariant(mach));
 }
 
 void c2_sm_fini(struct c2_sm *mach)
 {
+	C2_ASSERT(c2_sm_invariant(mach));
+	C2_ASSERT(is_subset(mach->sm_state, mach->sm_conf.scf_final));
+	c2_chan_fini(&mach->sm_chan);
+	c2_mutex_fini(&mach->sm_lock);
 }
 
-c2_sm_state_t c2_sm_state_get  (struct c2_sm *mach)
+int c2_sm_timedwait(struct c2_sm *mach, uint64_t states,
+		    struct c2_time *deadline)
 {
-	return 0;
-}
+	struct c2_clink wait;
 
-int c2_sm_state_set  (struct c2_sm *mach, c2_sm_state_t state)
-{
-	return 0;
-}
+	c2_clink_init(&wait, NULL);
+	c2_clink_add(&mach->sm_chan, &wait);
 
-int c2_sm_state_wait (struct c2_sm *mach)
-{
-	return 0;
-}
-
-int c2_sm_state_until(struct c2_sm *mach, c2_sm_state_t state)
-{
-	return 0;
-}
-
-int c2_sm_event_init(struct c2_sm_event *ev, struct c2_sm *mach)
-{
-	return 0;
-}
-
-void c2_sm_event_fini(struct c2_sm_event *ev)
-{
-}
-
-enum c2_sm_res c2_sm_event_try(struct c2_sm_event *sm)
-{
-	return SR_DONE;
-}
-
-int c2_sm_event_wait (struct c2_sm_event *sm)
-{
-	return 0;
-}
-
-int c2_sm_event_apply(struct c2_sm_event *sm)
-{
-	return 0;
-}
-
-
-int c2_sm_event_queue(struct c2_sm_event *sm)
-{
-	return 0;
-}
-
-int c2_persistent_sm_register(struct c2_persistent_sm *pmach,
-			      struct c2_dtm *dtm, 
-			      const struct c2_persistent_sm_ops *ops)
-{
-	return 0;
-}
-
-void c2_persistent_sm_unregister(struct c2_persistent_sm *pmach)
-{
+	c2_mutex_lock(&mach->sm_lock);
+	C2_ASSERT(c2_sm_invariant(mach));
+	while (!belongs(mach->sm_state,
+			states | mach->sm_conf.scf_terminal)) {
+		c2_mutex_unlock(&mach->sm_lock);
+		c2_chan_timedwait(&wait, deadline);
+		c2_mutex_lock(&mach->sm_lock);
+		C2_ASSERT(c2_sm_invariant(mach));
+	}
+	c2_mutex_unlock(&mach->sm_lock);
+	return mach->sm_rc;
 }
 
 /** @} end of sm group */
 
-/* 
+/*
  *  Local variables:
  *  c-indentation-style: "K&R"
  *  c-basic-offset: 8
--- sm/sm.h
+++ sm/sm.h
@@ -3,17 +3,17 @@
 #ifndef __COLIBRI_SM_H__
 #define __COLIBRI_SM_H__
 
-#include "lib/list.h"
-#include "lib/queue.h"
-#include "dtm/dtm.h"
+#include "lib/mutex.h"
+#include "lib/chan.h"
 
 /**
    @defgroup sm State machine
    @{
 */
 
-/** Macroscopic state machine state. */
-typedef uint64_t c2_sm_state_t;
+/* import */
+struct c2_time;
+
 
 enum c2_sm_res {
 	SR_DONE,
@@ -22,12 +22,13 @@ enum c2_sm_res {
 };
 
 struct c2_sm;
-struct c2_sm_event;
 
-struct c2_sm_ops {
-	c2_sm_state_t  (*so_state_get)(struct c2_sm *sm);
-	enum c2_sm_res (*so_event_try)(struct c2_sm *sm, 
-				       struct c2_sm_event *ev);
+struct c2_sm_conf {
+	uint64_t scf_start;
+	uint64_t scf_failure;
+	uint64_t scf_terminal;
+	uint64_t scf_final;
+	uint64_t scf_valid;
 };
 
 /**
@@ -36,59 +37,25 @@ struct c2_sm_ops {
    Abstract state machine. Possibly persistent, possibly replicated.
 */
 struct c2_sm {
-	struct c2_queue sm_incoming;
+	uint64_t                 sm_state;
+	const struct c2_sm_conf *sm_conf;
+	struct c2_mutex          sm_lock;
+	struct c2_chan           sm_chan;
+	int32_t                  sm_rc;
 };
 
-int  c2_sm_init(struct c2_sm *mach);
+void c2_sm_init(struct c2_sm *mach, const struct c2_sm_conf *conf);
 void c2_sm_fini(struct c2_sm *mach);
 
-c2_sm_state_t c2_sm_state_get  (struct c2_sm *mach);
-int           c2_sm_state_set  (struct c2_sm *mach, c2_sm_state_t state);
-int           c2_sm_state_wait (struct c2_sm *mach);
-int           c2_sm_state_until(struct c2_sm *mach, c2_sm_state_t state);
-
-/**
-   an event causing state transition.
-*/
-struct c2_sm_event {
-	struct c2_sm        *se_mach;
-	/*struct c2_chan_link  se_wait;*/
-	struct c2_list_link  se_wait;
-	struct c2_queue_link se_linkage;
-};
-
-int  c2_sm_event_init(struct c2_sm_event *ev, struct c2_sm *mach);
-void c2_sm_event_fini(struct c2_sm_event *ev);
-
-enum c2_sm_res c2_sm_event_try  (struct c2_sm_event *sm);
-int            c2_sm_event_wait (struct c2_sm_event *sm);
-int            c2_sm_event_apply(struct c2_sm_event *sm);
-int            c2_sm_event_queue(struct c2_sm_event *sm);
-
-struct c2_persistent_sm_ops;
-
-/** persistent state machine */
-struct c2_persistent_sm {
-	struct c2_sm                       ps_sm;
-	const struct c2_persistent_sm_ops *ps_ops;
-	struct c2_dtm                     *ps_dtm;
-};
-
-struct c2_persistent_sm_ops {
-	int (*pso_recover)(struct c2_persistent_sm *pmach);
-};
-
-int  c2_persistent_sm_register  (struct c2_persistent_sm *pmach,
-				 struct c2_dtm *dtm, 
-				 const struct c2_persistent_sm_ops *ops);
-void c2_persistent_sm_unregister(struct c2_persistent_sm *pmach);
+int c2_sm_timedwait(struct c2_sm *mach, uint64_t states,
+		    struct c2_time *deadline);
 
 /** @} end of sm group */
 
 /* __COLIBRI_SM_H__ */
 #endif
 
-/* 
+/*
  *  Local variables:
  *  c-indentation-style: "K&R"
  *  c-basic-offset: 8
