590f7d0201969fb3c663caef51940c248fe5d071
Nikita Danilov <nikita_danilov@xyratex.com>
2011-05-03 21:27:50 +0400

    Latest gcc versions complain about C2_BASSERT() macro:
            error: variably modified 'VARIABLE' at file scope
    
    Replace the implementation and coalesce identical user space and kernel assertion macros.

--- lib/linux_kernel/assert.h
+++ lib/linux_kernel/assert.h
@@ -4,7 +4,7 @@
 #define __COLIBRI_LIB_LINUX_KERNEL_ASSERT_H__
 
 /**
-   @addtogroup assert 
+   @addtogroup assert
 
    <b>Linux kernel assertion mechanism.</b>
 
@@ -14,18 +14,13 @@
 */
 
 #define C2_ASSERT(cond) BUG_ON(!(cond))
-#define C2_PRE(cond) C2_ASSERT(cond)
-#define C2_POST(cond) C2_ASSERT(cond)
-#define C2_CASSERT(cond) do { switch (1) {case 0: case !!(cond): ;} } while (0)
-#define C2_BASSERT(cond) extern char __build_assertion_failure[!!(cond) - 1]
-#define C2_IMPOSSIBLE(msg) C2_ASSERT(msg == NULL)
 
 /** @} end of assert group */
 
 /* __COLIBRI_LIB_LINUX_KERNEL_ASSERT_H__ */
 #endif
 
-/* 
+/*
  *  Local variables:
  *  c-indentation-style: "K&R"
  *  c-basic-offset: 8
--- lib/user_space/assert.h
+++ lib/user_space/assert.h
@@ -4,14 +4,14 @@
 #define __COLIBRI_LIB_USER_SPACE_ASSERT_H__
 
 /**
-   @addtogroup assert 
+   @addtogroup assert
 
    <b>User space assertions based on c2_panic() function.</b>
 
    @{
 */
 
-void c2_panic(const char *expr, const char *func, const char *file, int lineno) 
+void c2_panic(const char *expr, const char *func, const char *file, int lineno)
 	__attribute__((noreturn));
 
 /**
@@ -22,51 +22,13 @@ void c2_panic(const char *expr, const char *func, const char *file, int lineno)
 #define C2_ASSERT(cond) \
         ((cond) ? (void)0 : c2_panic(#cond, __func__, __FILE__, __LINE__))
 
-/**
-   A macro to check that a function pre-condition holds. C2_PRE() is
-   functionally equivalent to C2_ASSERT().
-
-   @see C2_POST()
- */
-#define C2_PRE(cond) C2_ASSERT(cond)
-
-/**
-   A macro to check that a function post-condition holds. C2_POST() is
-   functionally equivalent to C2_ASSERT().
-
-   @see C2_PRE()
- */
-#define C2_POST(cond) C2_ASSERT(cond)
-
-/**
-   A macro to assert that compile-time condition is true. Condition must be a
-   constant expression (as defined in the section 6.6 of ISO/IEC
-   9899). C2_CASSERT() can be used anywhere where a statement can be.
-
-   @see C2_BASSERT()
- */
-#define C2_CASSERT(cond) do { switch (1) {case 0: case !!(cond): ;} } while (0)
-
-/**
-   A macro to assert that compile-time condition is true. Condition must be a
-   constant expression. C2_BASSERT() can be used anywhere where a declaration
-   can be.
-
-   @see C2_CASSERT()
- */
-#define C2_BASSERT(cond) extern char __build_assertion_failure[!!(cond) - 1]
-
-/**
-   A macro indicating that computation reached an invalid state.
- */
-#define C2_IMPOSSIBLE(msg) C2_ASSERT("Impossible: " msg == NULL)
 
 /** @} end of assert group */
 
 /* __COLIBRI_LIB_USER_SPACE_ASSERT_H__ */
 #endif
 
-/* 
+/*
  *  Local variables:
  *  c-indentation-style: "K&R"
  *  c-basic-offset: 8
