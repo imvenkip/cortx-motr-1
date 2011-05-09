ec32250dbb35295adf54ab064f55cce336140d6c
Nikita Danilov <nikita_danilov@xyratex.com>
2011-05-09 12:49:29 +0400

    update on state machine helpers.

--- sm/sm.c
+++ sm/sm.c
@@ -8,65 +8,104 @@
    @{
 */
 
-static bool is_subset(uint64_t a, uint64_t b)
+static struct c2_sm_state_descr *mach_state(const struct c2_sm *mach)
 {
-	return (a & b) == a;
+	return &mach->sm_conf->scf_state[mach->sm_state];
 }
 
-static bool belongs(uint64_t a, uint64_t b)
+bool c2_sm_invariant(const struct c2_sm *mach)
 {
-	C2_PRE(c2_is_po2(a));
-	return a & b;
-}
+	const struct c2_sm_state_descr *d;
 
-static bool c2_sm_invariant(const struct c2_sm *mach)
-{
-	const struct c2_sm_conf *c;
+	d = mach_state(mach);
 
-	c = mach->sm_conf;
-	return is_subset(c->scf_start | c->scf_failure | c->scf_terminal |
-			 c->scf_final, c->scf_valid) &&
-		c2_is_po2(mach->sm_state) &&
-		belongs(mach->sm_state, c->scf_valid);
+	return
+		mach->sm_state < mach->sm_conf->scf_nr_states &&
+		ergo(d->sd_invariant != NULL, d->sd_invariant(mach));
 }
 
 void c2_sm_init(struct c2_sm *mach, const struct c2_sm_conf *conf)
 {
-	mach->sm_state = conf->scf_start;
+	C2_PRE(conf->scf_nr_states > 0);
+
 	mach->sm_conf  = conf;
 	mach->sm_rc    = 0;
+	mach->sm_state = 0;
 	c2_mutex_init(&mach->sm_lock);
 	c2_chan_init(&mach->sm_chan);
+
 	C2_POST(c2_sm_invariant(mach));
 }
 
 void c2_sm_fini(struct c2_sm *mach)
 {
 	C2_ASSERT(c2_sm_invariant(mach));
-	C2_ASSERT(is_subset(mach->sm_state, mach->sm_conf->scf_final));
+	C2_ASSERT(mach_state(mach)->sd_flags & SDF_FINAL);
+
 	c2_chan_fini(&mach->sm_chan);
 	c2_mutex_fini(&mach->sm_lock);
+}
+
+struct waitinfo {
+	struct c2_clink  w_clink;
+	struct c2_sm    *w_mach;
+	uint64_t         w_states;
+};
+
+static bool reached(const struct c2_sm *mach, uint64_t states)
+{
+	return (1ULL << mach->sm_state) & states;
+}
+
+static bool callback(struct c2_clink *clink)
+{
+	struct waitinfo *wi;
+
+	wi = container_of(clink, struct waitinfo, w_clink);
+
+	C2_ASSERT(c2_mutex_is_locked(&wi->w_mach->sm_lock));
+	return !reached(wi->w_mach, wi->w_states);
 }
 
 int c2_sm_timedwait(struct c2_sm *mach, uint64_t states,
 		    struct c2_time *deadline)
 {
-	struct c2_clink wait;
+	struct waitinfo  wi;
+	struct c2_clink *clink;
 
-	c2_clink_init(&wait, NULL);
-	c2_clink_add(&mach->sm_chan, &wait);
+	clink = &wi.w_clink;
+	c2_clink_init(clink, &callback);
 
 	c2_mutex_lock(&mach->sm_lock);
 	C2_ASSERT(c2_sm_invariant(mach));
-	while (!belongs(mach->sm_state,
-			states | mach->sm_conf->scf_terminal)) {
+
+	c2_clink_add(&mach->sm_chan, clink);
+	while (!reached(mach, states)) {
 		c2_mutex_unlock(&mach->sm_lock);
 		c2_chan_timedwait(&wait, deadline);
 		c2_mutex_lock(&mach->sm_lock);
 		C2_ASSERT(c2_sm_invariant(mach));
 	}
+	c2_clink_del(clink);
 	c2_mutex_unlock(&mach->sm_lock);
+	c2_clink_fini(clink);
 	return mach->sm_rc;
+}
+
+void c2_sm_state_set(struct c2_sm *mach, uint32_t state)
+{
+	c2_mutex_lock(&mach->sm_lock);
+	C2_ASSERT(c2_sm_invariant(mach));
+	C2_PRE(state < mach->sm_conf->scf_nr_states);
+
+	C2_ASSERT(c2_bitmap_get(&mach_state(mach)->sd_allowed, state));
+
+	mach->sm_state = state;
+	C2_ASSERT(c2_sm_invariant(mach));
+
+	c2_signal_broadcast(&mach->sm_chan);
+
+	c2_mutex_unlock(&mach->sm_lock);
 }
 
 /** @} end of sm group */
--- sm/sm.h
+++ sm/sm.h
@@ -3,7 +3,9 @@
 #ifndef __COLIBRI_SM_H__
 #define __COLIBRI_SM_H__
 
+#include "lib/time.h"
 #include "lib/mutex.h"
+#include "lib/bitmap.h"
 #include "lib/chan.h"
 
 /**
@@ -22,14 +24,8 @@ enum c2_sm_res {
 };
 
 struct c2_sm;
-
-struct c2_sm_conf {
-	uint64_t scf_start;
-	uint64_t scf_failure;
-	uint64_t scf_terminal;
-	uint64_t scf_final;
-	uint64_t scf_valid;
-};
+struct c2_sm_state_descr;
+struct c2_sm_conf;
 
 /**
    state machine
@@ -37,18 +33,36 @@ struct c2_sm_conf {
    Abstract state machine. Possibly persistent, possibly replicated.
 */
 struct c2_sm {
-	uint64_t                 sm_state;
+	uint32_t                 sm_state;
 	const struct c2_sm_conf *sm_conf;
 	struct c2_mutex          sm_lock;
 	struct c2_chan           sm_chan;
 	int32_t                  sm_rc;
 };
 
+struct c2_sm_conf {
+	uint32_t            scf_nr_states;
+	struct c2_sm_state *scf_state;
+};
+
+struct c2_sm_state_descr {
+	uint32_t         sd_flags;
+	const char      *sd_name;
+	bool           (*sd_invariant)(const struct c2_sm *mach);
+	struct c2_bitmap sd_allowed;
+};
+
+enum c2_sm_state_descr_flags {
+	SDF_FAILURE  = 1 << 0,
+	SDF_TERMINAL = 1 << 1,
+	SDF_FINAL    = 1 << 2
+};
+
 void c2_sm_init(struct c2_sm *mach, const struct c2_sm_conf *conf);
 void c2_sm_fini(struct c2_sm *mach);
 
 int c2_sm_timedwait(struct c2_sm *mach, uint64_t states,
-		    struct c2_time *deadline);
+		    c2_time_t deadline);
 
 /** @} end of sm group */
 
