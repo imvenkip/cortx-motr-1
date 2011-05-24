6b9922a8ff94dd02d9ba0e348cc1624903f340e0
anand.vidwansa <anand_vidwansa@xyratex.com>
2011-05-24 11:26:20 +0530

    Added UT code for RPC formation component.

---
+++ rpc/ut/formation-ut.c
@@ -0,0 +1,31 @@
+#include "rpc/formation.h"
+
+/*
+   *** Current scenario ***
+   1. The rpc core component is not ready completely. So this UT tries to
+      simulate the things which are not available.
+   2. The RPC Formation component 
+
+   *** Requirements for UT of formation component. ***
+   1. Simulate the end user access pattern.[C2_RPC_FORM_ACCESS_PATERN]
+   2. Simulate minimalistic meta data traffic.[C2_RPC_FORM_MD_TRAFFIC]
+   3. Simulate sufficient IO traffic to stress all corner cases.
+      Ensure multiple requests on same files, multiple IO requests
+      on same/different files in same groups so as to stress all
+      corner cases. [C2_RPC_FORM_IO_TRAFFIC]
+   4. Handle all memory management.[C2_RPC_FORM_MEM_MGMT]
+   5. Write maximum asserts to check behavior of component.[C2_RPC_FORM_ASSERTS]
+   6. Display statistics on rpc formation.[C2_RPC_FORM_STATS]
+
+   *** A concise design for UT of RPC Formation. ***
+   1. Consider a flat hierarchy of a number of files.
+   2. These files will be created first.
+   3. A certain set of files will be picked up for IO.
+   4. All files will be put in some group and there will be sufficient
+      multiple IO requests on same files.
+   5. Create FOPs for these requests (metadata/IO), assign
+ */
+int main()
+{
+}
+
