3c7cb8774b6f1e2fb8862cf4364e1c10fb78d5be
Dave Cohrs <dave_cohrs@us.xyratex.com>
2011-05-25 08:29:59 -0700

    - style: sizeof needs to parentheses - more specific thread name in trace UT - use c2_vec_count for testing internal bufvec vec size

--- net/buf.c
+++ net/buf.c
@@ -30,8 +30,7 @@ bool c2_net__buffer_invariant(const struct c2_net_buffer *buf)
 
 	/* bufvec must be set */
 	if (buf->nb_buffer.ov_buf == NULL ||
-	    buf->nb_buffer.ov_vec.v_nr == 0 ||
-	    buf->nb_buffer.ov_vec.v_count == NULL)
+	    c2_vec_count(&buf->nb_buffer.ov_vec) == 0)
 		return false;
 
 	/* optional callbacks, but if provided then ntc_event_cb required */
@@ -77,8 +76,7 @@ int c2_net_buffer_register(struct c2_net_buffer *buf,
 	       buf->nb_dom == NULL &&
 	       buf->nb_flags == 0 &&
 	       buf->nb_buffer.ov_buf != NULL &&
-	       buf->nb_buffer.ov_vec.v_nr > 0 &&
-	       buf->nb_buffer.ov_vec.v_count != NULL);
+	       c2_vec_count(&buf->nb_buffer.ov_vec) > 0);
 
 	buf->nb_dom = dom;
 	buf->nb_xprt_private = NULL;
