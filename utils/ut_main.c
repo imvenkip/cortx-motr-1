916fdb9f5896f424db6e4727247a733c4414bfa0
Subhash Arya <admin@Subhash-Aryas-MacBook-Pro.local>
2011-06-02 19:49:14 +0530

    Added ut and ub for xdr little endian library

--- utils/ut_main.c
+++ utils/ut_main.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 
 	if (unit_start(UT_SANDBOX) == 0) {
 		/* sort test suites in alphabetic order */
-		/*c2_ut_add(&libc2_ut);
+		c2_ut_add(&libc2_ut);
 		c2_ut_add(&adieu_ut);
 		c2_ut_add(&ad_ut);
 		c2_ut_add(&capa_ut);
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 		c2_ut_add(&net_client_ut);
 		c2_ut_add(&parity_math_ut);
 		c2_ut_add(&stobio_ut);
-		c2_ut_add(&udb_ut);*/
+		c2_ut_add(&udb_ut);
 		c2_ut_add(&xdr_ut);
 		c2_ut_run("c2ut.log");
 		if (!keep)
