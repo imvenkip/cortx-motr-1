e109b6c12d7809eb99ee3ee9bd0d8cbe328f9942
Dave Cohrs <dave_cohrs@us.xyratex.com>
2011-06-10 14:15:22 -0700

    ensure fill UT output gets displayed, even if extra kern messages after UT

--- utils/linux_kernel/ut.sh
+++ utils/linux_kernel/ut.sh
@@ -2,37 +2,25 @@
 
 # Small wrapper to run kernel UT
 
-case "`id -u`" in
-0) ;;
-*) echo "Must be run as root"
-   exit 1
-   ;;
-esac
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Must be run as root"
+    exit 1
+fi
 
 d="`git rev-parse --show-cdup`"
-case "x$d" in
-x) ;;
-*) cd "$d"
-   ;;
-esac
+if [ -n "$d" ]; then
+    cd "$d"
+fi
 
 . c2t1fs/st/common.sh
 
 MODLIST="lib/linux_kernel/klibc2.ko \
          utils/linux_kernel/kutc2.ko"
 
-sorig=`stat -c %s /var/log/kern`
+tailseek=$(( $(stat -c %s /var/log/kern) + 1 ))
 
 # currently, kernel UT runs as part of loading kutc2 module
 modload
-
-sdone=`stat -c %s /var/log/kern`
-stail=$((sdone - sorig))
-case "$stail" in
-0)  echo "No output found in /var/log/kern"
-    ;;
-*)  tail -c$stail /var/log/kern
-    ;;
-esac
-
 modunload
+
+tail -c+$tailseek /var/log/kern
