c81b1f36141f73e47248a0406877d49a81867549
Anup_Barve <anup_barve@xyratex.com>
2011-08-11 17:42:25 +0530

    Fixed comments from Dave

--- rpc/formation.c
+++ rpc/formation.c
@@ -15,7 +15,7 @@
  * http://www.xyratex.com/contact
  *
  * Original author: Anand Vidwansa <Anand_Vidwansa@xyratex.com>
- * Original author: Anup Barve <Anup_Barve@xyratex.com>
+ * 		    Anup Barve <Anup_Barve@xyratex.com>
  * Original creation date: 04/28/2011
  */
 
