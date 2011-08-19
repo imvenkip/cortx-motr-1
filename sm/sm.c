dcac1ea83d3c56a73402a49ff792f163135b4d2c
nikita <nikita.danilov@horizontalscale.com>
2011-08-19 14:20:35 +0400

    implement AST mechanism and state machine groups.

--- sm/sm.c
+++ sm/sm.c
@@ -31,6 +31,101 @@
    @{
 */
 
+void c2_sm_group_init(struct c2_sm_group *grp)
+{
+	int i;
+
+	C2_SET0(grp);
+	c2_mutex_init(&grp->s_lock);
+	c2_semaphore_init(&grp->s_sem, 0);
+	c2_list_init(&grp->s_cb_free);
+	for (i = 0; i < ARRAY_SIZE(grp->s_ast); ++i)
+		c2_list_add_tail(&grp->s_ast_free, &grp->s_ast[i].sa_free);
+}
+C2_EXPORTED(c2_sm_group_init);
+
+void c2_sm_group_fini(struct c2_sm_group *grp)
+{
+	c2_semaphore_fini(&grp->s_sem);
+	c2_mutex_fini(&grp->s_lock);
+}
+C2_EXPORTED(c2_sm_group_fini);
+
+static void grp_lock(struct c2_sm_group *grp)
+{
+	C2_PRE(c2_atomic64_get(&grp->s_spl, 0));
+	c2_mutex_lock(&grp->s_lock);
+	c2_sm_asts_run(grp);
+}
+
+static void grp_unlock(struct c2_sm_group *grp)
+{
+	c2_sm_asts_run(grp);
+	c2_mutex_unlock(&grp->s_lock);
+}
+
+static bool grp_is_locked(const struct c2_sm_group *grp)
+{
+	return c2_mutex_is_locked(&grp->s_lock);
+}
+
+static bool ast_cas(struct c2_sm_group *grp,
+		    struct c2_sm_ast *old, struct c2_sm_ast *new)
+{
+	return c2_atomic64_cas(&grp->sa_forkq, (int64_t)old, (int64_t)new);
+}
+
+void c2_sm_ast_post(struct c2_sm_group *grp, struct c2_sm_ast *ast)
+{
+	do
+		ast->sa_next = grp->s_forkq;
+	while (!ast_cas(grp, ast->sa_next, ast));
+	c2_semaphore_up(&grp->s_sem);
+}
+C2_EXPORTED(c2_sm_ast_post);
+
+void c2_sm_asts_run(struct c2_sm_group *grp)
+{
+	struct c2_sm_ast *ast;
+
+	C2_PRE(grp_is_locked(grp));
+
+	while (1) {
+		do
+			ast = grp->s_forkq;
+		while (ast != NULL && !ast_cas(&grp, ast, ast->sa_next));
+
+		if (ast != NULL) {
+			ast->sa_cb(ast);
+			if (&grp->s_ast[0] <= ast &&
+			    ast < &grp->s_ast[C2_SM_GROUP_AST_MAX]) {
+				C2_ASSERT(!c2_list_link_is_in(&ast->sa_free));
+				c2_list_add_tail(&grp->s_ast_free,
+						 &ast->sa_free);
+			}
+
+			c2_semaphore_trydown(&grp->s_sem);
+		} else
+			break;
+	}
+}
+C2_EXPORTED(c2_sm_asts_run);
+
+static void sm_lock(struct c2_sm *mach)
+{
+	grp_lock(mach->sm_grp);
+}
+
+static void sm_unlock(struct c2_sm *mach)
+{
+	grp_unlock(mach->sm_grp);
+}
+
+static bool sm_is_locked(const struct c2_sm *mach)
+{
+	return c2_mutex_is_locked(&mach->sm_grp->s_lock);
+}
+
 static bool state_is_valid(const struct c2_sm_conf *conf, uint32_t state)
 {
 	return
@@ -55,7 +150,7 @@ bool c2_sm_invariant(const struct c2_sm *mach)
 	struct c2_sm_state_descr *sd = sm_state(mach);
 
 	return
-		c2_mutex_is_locked(mach->sm_lock) &&
+		sm_is_locked(mach) &&
 		ergo(sd->sd_invariant != NULL, sd->sd_invariant(mach));
 }
 
@@ -89,16 +184,24 @@ static bool conf_invariant(const struct c2_sm_conf *conf)
 	return true;
 }
 
+static unsigned long impossible(unsigned long data)
+{
+	C2_IMPOSSIBLE("C2_TIME_NEVER happened.");
+	return 0;
+}
+
 void c2_sm_init(struct c2_sm *mach, const struct c2_sm_conf *conf,
-		uint32_t state, struct c2_mutex *lock, struct c2_addb_ctx *ctx)
+		uint32_t state, struct c2_sm_group *grp,
+		struct c2_addb_ctx *ctx)
 {
 	C2_PRE(conf_invariant(conf));
 
 	mach->sm_state = state;
 	mach->sm_conf  = conf;
-	mach->sm_lock  = lock;
+	mach->sm_grp   = grp;
 	mach->sm_addb  = ctx;
 	mach->sm_rc    = 0;
+	c2_list_add(&grp->s_mach, &mach->sm_linkage);
 	c2_chan_init(&mach->sm_chan);
 	C2_POST(c2_sm_invariant(mach));
 }
@@ -108,6 +211,7 @@ void c2_sm_fini(struct c2_sm *mach)
 {
 	C2_ASSERT(c2_sm_invariant(mach));
 	C2_PRE(sm_state(mach)->sd_flags & SDF_TERMINAL);
+	c2_list_del(&mach->sm_linkage);
 	c2_chan_fini(&mach->sm_chan);
 }
 C2_EXPORTED(c2_sm_fini);
@@ -121,39 +225,46 @@ struct wait_state {
 static bool wait_filter(struct c2_clink *clink)
 {
 	struct wait_state *ws;
+	struct c2_sm      *mach;
 
 	ws = container_of(clink, struct wait_state, ws_waiter);
-	C2_ASSERT(c2_sm_invariant(ws->ws_mach));
+	mach = ws->ws_mach;
+	C2_ASSERT(c2_sm_invariant(mach));
 
-	return ((1 << ws->ws_mach->sm_state) & ws->ws_wake) == 0;
+	return ((1 << mach->sm_state) & ws->ws_wake) == 0 &&
+		!(sn_state(mach)->sd_flags & SDF_TERMINAL);
 }
 
-int c2_sm_timedwait(struct c2_sm *mach, uint64_t states,
-		    c2_time_t deadline)
+int c2_sm_timedwait(struct c2_sm *mach, uint64_t states, c2_time_t deadline)
 {
 	struct c2_clink waiter;
+	int             result;
 
-	C2_PRE(c2_mutex_is_locked(mach->sm_lock));
+	C2_ASSERT(c2_sm_invariant(mach));
 
+	result = 0;
 	c2_clink_init(&waiter, wait_filter);
 	c2_clink_add(&mach->sm_chan, &waiter);
-	while (((1 << mach->sm_state) & states) == 0) {
+	while (result == 0 && ((1 << mach->sm_state) & states) == 0) {
 		C2_ASSERT(c2_sm_invariant(mach));
 		if (sm_state(mach)->sd_flags & SDF_TERMINAL)
-			return -ESRCH;
-		c2_mutex_unlock(mach->sm_lock);
-		c2_chan_wait(&waiter);
-		c2_mutex_lock(mach->sm_lock);
+			result = -ESRCH;
+		else {
+			sm_unlock(mach);
+			if (!c2_chan_timedwait(&waiter, deadline))
+				result = -ETIMEDOUT;
+			sm_lock(mach);
+		}
 	}
 	C2_ASSERT(c2_sm_invariant(mach));
-	return 0;
+	return result;
 }
 C2_EXPORTED(c2_sm_timedwait);
 
 void c2_sm_fail(struct c2_sm *mach, int fail_state, int32_t rc)
 {
 	C2_PRE(rc != 0);
-	C2_PRE(c2_mutex_is_locked(mach->sm_lock));
+	C2_PRE(c2_sm_invariant(mach));
 	C2_PRE(mach->sm_rc == 0);
 	C2_PRE(state_get(mach, fail_state)->sd_flags & SDF_FAILURE);
 
@@ -166,7 +277,6 @@ void c2_sm_state_set(struct c2_sm *mach, int state)
 {
 	struct c2_sm_state_descr *sd;
 
-	C2_PRE(c2_mutex_is_locked(mach->sm_lock));
 	C2_PRE(c2_sm_invariant(mach));
 
 	sd = sm_state(mach);
@@ -174,28 +284,94 @@ void c2_sm_state_set(struct c2_sm *mach, int state)
 
 	if (sd->sd_ex != NULL)
 		sd->sd_ex(mach);
+	c2_sm_asts_run(mach->sm_grp);
 	mach->sm_state = state;
 	sd = sm_state(mach);
 	if (sd->sd_in != NULL)
 		sd->sd_in(mach);
 	c2_chan_broadcast(&mach->sm_chan);
 	C2_POST(c2_sm_invariant(mach));
-	C2_POST(c2_mutex_is_locked(mach->sm_lock));
 }
 C2_EXPORTED(c2_sm_state_set);
 
-unsigned long sm_timeout_callback(unsigned long data)
+static void sm_timeout_bottom(struct c2_sm *mach, void *datum)
 {
+	struct c2_sm_timeout *to   = datum;
+	struct c2_sm         *mach = to->st_ast.sa_mach;
+
+	C2_ASSERT(c2_sm_invariant(mach));
+
+	if (to->st_active) {
+		to->st_active = false;
+		c2_sm_state_set(mach, to->st_state);
+	}
 }
 
-int c2_sm_timeout(struct c2_sm *mach, struct c2_timer *timer,
+static unsigned long sm_timeout_top(unsigned long data)
+{
+	struct c2_sm_timeout *to = (void *)data;
+
+	c2_sm_ast_post(to->st_ast.sa_mach->sm_grp, &to->st_ast);
+	return 0;
+}
+
+static bool sm_timeout_cancel(struct c2_clink *link)
+{
+	struct c2_sm_timeout *to = container_of(link, struct c2_sm_timeout,
+						st_clink);
+
+	C2_ASSERT(c2_sm_invariant(to->st_ast.sa_mach));
+
+	to->st_active = false;
+	c2_timer_stop(&to->st_timer);
+}
+
+int c2_sm_timeout(struct c2_sm *mach, struct c2_sm_timeout *to,
 		  c2_time_t timeout, int state)
 {
-	int result;
+	int              result;
+	struct c2_timer *tm = &to->st_timer;
 
-	result = c2_timer_init(timer, C2_TIMER_SOFT, timeout, 1);
+	C2_PRE(c2_sm_invariant(mach));
+	C2_PRE(!(sm_state(mach)->sd_flags & SDF_TERMINAL));
+	C2_PRE(!(state_get(mach, state)->sd_flags & SDF_TERMINAL));
+
+	to->st_active      = true;
+	to->st_state       = state;
+	to->st_ast.sa_cb   = sm_timeout_bottom;
+	to->st_ast.sa_mach = mach;
+	c2_clink_init(&to->st_clink, sm_timeout_cancel);
+	c2_clink_add(&mach->sm_chan, &to->st_clink);
+	c2_timer_init(tm, C2_TIMER_SOFT, timeout, 1, sm_timeout_top,
+		      (unsigned long)to);
+	result = c2_timer_start(tm);
+	if (result != 0)
+		c2_sm_timeout_fini(to);
+	return result;
 }
 C2_EXPORTED(c2_sm_timeout);
+
+void c2_sm_timeout_fini(struct c2_sm_timeout *to)
+{
+	C2_PRE(to->st_ast.sa_next == NULL);
+
+	if (c2_timer_is_started(&to->st_timer))
+		c2_timer_stop(&to->st_timer);
+	c2_timer_fini(&to->st_timer);
+	if (c2_clink_is_armed(&to->st_clink))
+		c2_clink_del(&to->st_clink);
+	c2_clink_fini(&to->st_clink);
+}
+C2_EXPORTED(c2_sm_timeout_fini);
+
+struct c2_sm_ast *c2_sm_ast_get(struct c2_sm_group *grp)
+{
+	C2_PRE(grp_is_locked(grp));
+	C2_PRE(!c2_list_is_empty(&grp->s_ast_free));
+
+	return container_of(grp->s_ast_free.l_head, struct c2_sm_ast, sa_free);
+}
+C2_EXPORTED(c2_sm_cb_post);
 
 /** @} end of sm group */
 
--- sm/sm.h
+++ sm/sm.h
@@ -22,14 +22,160 @@
 #define __COLIBRI_SM_SM_H__
 
 #include "lib/types.h"               /* int32_t, uint64_t */
+#include "lib/atomic.h"
 #include "lib/time.h"                /* c2_time_t */
+#include "lib/semaphore.h"
 #include "lib/chan.h"
+#include "lib/mutex.h"
+#include "lib/list.h"
 
 /**
    @defgroup sm State machine
 
    This modules defines interfaces to functionality common to typical
    non-blocking state machines extensively used by Colibri.
+
+   The main difference between "state machine" (non-blocking) code and
+   "threaded" (blocking) code is that the latter blocks waiting for some events
+   while having some computational state stored in the "native" C language stack
+   (in the form of automatic variables). Because of this the thread must remain
+   dedicated to the same threaded activity not only during actual "computation",
+   when processor is actively used, but also for the duration of wait. In many
+   circumstances this is too expensive, because threads are heavy objects.
+
+   Non-blocking code, on the other hand, packs all its state into a special
+   data-structures before some potential blocking points and unpacks it after
+   the event of interest occurs. This allows the same thread to be re-used for
+   multiple non-blocking computations.
+
+   Which blocking points deserve packing-unpacking depends on
+   circumstances. Long-term waits for network or storage communication are prime
+   candidates for non-blocking handling. Memory accesses, which can incur
+   blocking page faults in a user space process are probably too ubiquitous for
+   this. Memory allocations and data-structure locks fall into an intermediate
+   group.
+
+   This module defines data-structures and interfaces to handle common
+   non-blocking state-machine functionality:
+
+       - state tracking and state transitions;
+
+       - concurrency;
+
+       - interaction between a state machine and external world (both
+         non-blocking and threaded);
+
+       - accounting and statistics collection.
+
+   <b>State and transitions.</b>
+
+   State machine state is recorded in c2_sm::sm_state. This is supposed to be a
+   relatively coarse-grained high level state, broadly determining state machine
+   behaviour. An instance of c2_sm will typically be embedded into a larger
+   structure containing fields fully determining state machine behaviour. Each
+   state comes with a description. All descriptions for a particular state
+   machine are packed into a c2_sm_conf::scf_state[] array.
+
+   State machine transferred from one state to another by a call to
+   c2_sm_state_set() (or its variant c2_sm_fail()).
+
+   <b>Concurrency.</b>
+
+   State machine is a part of a state machine group (c2_sm_group). All state
+   machines in a group use group's mutex to serialise their state
+   transitions. One possible scenario is to have a group for all state machines
+   associated with a given locality (c2_fom_locality). Alternatively a
+   group-per-machine can be used.
+
+   <b>Interaction.</b>
+
+   The only "output" event that a state machine communicates to the external
+   world is (from this module's point of view) its state transition. State
+   transitions are announces on a per-machine channel (c2_sm::sm_chan). This
+   mechanism works both for threaded and non-blocking event consumers. The
+   formers use c2_sm_timedwait() to wait until the state machine reaches
+   desirable state, the latter register a clink with c2_sm::sm_chan.
+
+   "Input" events cause state transitions. Typical examples of such events are:
+   completion of a network of storage communication, timeout or a state
+   transition in a different state machine. Such events often happen in
+   "awkward" context: signal and interrupt handlers, timer call-backs and
+   similar. Acquiring the group's mutex, necessary for state transition in such
+   places is undesirable for multiple reasons:
+
+       - to avoid self-deadlock in a case where an interrupt of signal is
+         services by a thread that already holds the mutex, the latter must be
+         made "async-safe", which is quite expensive;
+
+       - implementation of a module that provides a call-back must take into
+         account the possibility of the call-back blocking waiting for a
+         mutex. This is also quite expensive;
+
+       - locking order dependencies arise between otherwise unrelated
+         components;
+
+       - all these issues are exasperated in a situation where state transition
+         must take additional locks, which it often does.
+
+   The solution to these problems comes from operating system kernels design,
+   see the AST section below.
+
+   <b>Accounting and statistics.<b>
+
+   This module accumulates statistics about state transitions and time spent in
+   particular states. For additional flexibility these data are reported through
+   an addb context supplied during state machine initialisation so that the same
+   context can be used for other purposes too.
+
+   @todo accounting and statistics is not currently implemented.
+
+   <b>AST.</b>
+
+   Asynchronous System Trap (AST) is a mechanism that allows a code running in
+   an "awkward context" (see above) to post a call-back to be executed at the
+   "base level" under a group mutex. UNIX kernels traditionally used a similar
+   mechanism, where an interrupt handler does little more than setting a flag
+   and returning. This flag is checked when the kernel is just about to return
+   to the user space. If the flag is set, the rest of interrupt processing
+   happens. In Linux similar mechanism is called a "top-half" and "bottom-half"
+   of interrupt processing. In Windows it is a DPC
+   (http://en.wikipedia.org/wiki/Deferred_Procedure_Call) mechanism, in older
+   DEC kernels it was called a "fork queue".
+
+   c2_sm_ast structure represents a call-back to be invoked under group
+   mutex. An ast is "posted" to a state machine group by a call to
+   c2_sm_ast_post(), which can be done in any context, in the sense that it
+   doesn't take any locks. Posted asts are executed
+
+       - just after group mutex is taken;
+
+       - just before group mutex is released;
+
+       - whenever a state machine in the group performs a state transition;
+
+       - whenever c2_sm_asts_run() is called.
+
+   Ast mechanism solves the problems with input events mentioned above at the
+   expense of
+
+       - an increased latency: the call-back is not executed immediately;
+
+       - an additional burden of ast-related book-keeping: it is up to the ast
+         user to free ast structure when it is safe to do so (i.e., after the
+         ast completed execution).
+
+   To deal with the second problem, each group comes with a pool of
+   pre-allocated asts (c2_sm_group::s_ast[]) from where asts can be allocated by
+   a call to c2_sm_ast_get(). Asts from the pool are returned back to the pool
+   automatically once they complete their execution.
+
+   To deal with the latency problem, a user must arrange c2_sm_asts_run() to be
+   called during long state transitions (typically within loops).
+
+   If an ast is posted and group mutex is not held (i.e., the group is "idle") a
+   c2_sm_group::s_sem semaphore is signalled. It is expected that some threads,
+   assigned to handling state transitions of machines from the group, are
+   waiting on this semaphore.
 
    @{
 */
@@ -38,6 +184,8 @@
 struct c2_sm;
 struct c2_sm_state_descr;
 struct c2_sm_conf;
+struct c2_sm_group;
+struct c2_sm_ast;
 
 /* import */
 struct c2_addb_ctx;
@@ -77,12 +225,7 @@ struct c2_sm {
 	   book-keeping, including addb-based accounting.
 	 */
 	const struct c2_sm_conf *sm_conf;
-	/**
-	   Lock under which state transitions happen. This lock is supplied
-	   during c2_sm creation so that multiple state machines can share the
-	   same lock if necessary.
-	 */
-	struct c2_mutex         *sm_lock;
+	struct c2_sm_group      *sm_grp;
 	/**
 	   addb context in which state machine related events and statistics are
 	   reported. This is included by reference for flexibility.
@@ -107,6 +250,7 @@ struct c2_sm {
    @invariant c2_sm_desc_invariant()
  */
 struct c2_sm_conf {
+	const char               *scf_name;
 	/** Number of states in this state machine. */
 	uint32_t                  scf_nr_states;
 	/** Array of state descriptions. */
@@ -174,17 +318,58 @@ enum c2_sm_state_descr_flags {
 	   A state marked with this flag is a terminal state. No transitions out
 	   of this state are allowed (checked by c2_sm_conf_invariant()) and an
 	   attempt to wait for a state transition, while the state machine is in
-	   a terminal state, immediately returns ESRCH.
+	   a terminal state, immediately returns -ESRCH.
+
+	   @see c2_sm_timedwait()
 	 */
 	SDF_TERMINAL = 1 << 1
 };
 
+struct c2_sm_ast {
+	void              (*sa_cb)(struct c2_sm_group *grp, struct c2_sm_ast *);
+	struct c2_sm_ast   *sa_next;
+	struct c2_sm       *sa_mach;
+	void               *sa_datum;
+	struct c2_list_link sa_freelist;
+};
+
+enum {
+	C2_SM_GROUP_AST_MAX = 256;
+};
+
+struct c2_sm_group {
+	struct c2_mutex      s_lock;
+	struct c2_semaphore  s_sem;
+	struct c2_sm_ast    *s_forkq;
+	struct c2_list       s_ast_free;
+	struct c2_sm_ast     s_ast[C2_SM_GROUP_AST_MAX];
+};
+
 void c2_sm_init(struct c2_sm *mach, const struct c2_sm_conf *conf,
-		uint32_t state, struct c2_mutex *lock, struct c2_addb_ctx *ctx);
+		uint32_t state, struct c2_sm_grp *grp, struct c2_addb_ctx *ctx);
 void c2_sm_fini(struct c2_sm *mach);
 
-int c2_sm_timedwait(struct c2_sm *mach, uint64_t states,
-		    c2_time_t deadline);
+void c2_sm_group_init(struct c2_sm_group *grp);
+void c2_sm_group_fini(struct c2_sm_group *grp);
+
+/**
+   Waits until a given state machine enters any of states enumerated by a given
+   bit-mask.
+
+   @retval 0          - one of the states reached
+
+   @retval -ESRCH     - terminal state reached,
+                        see c2_sm_state_descr_flags::SDF_TERMINAL
+
+   @retval -ETIMEDOUT - deadline passed
+
+   In case where multiple wait termination conditions hold simultaneously (e.g.,
+   @states includes a terminal state), the result is implementation dependent.
+
+   @note this interface assumes that states are numbered by numbers less than
+   64.
+ */
+int c2_sm_timedwait(struct c2_sm *mach, uint64_t states, c2_time_t deadline);
 
 /**
    Moves a state machine into fail_state state atomically with setting rc code.
@@ -202,11 +387,22 @@ void c2_sm_fail(struct c2_sm *mach, int fail_state, int32_t rc);
 /**
    Transits a state machine into the indicated state.
 
+   Calls ex- and in- methods of the corresponding states (even if the state
+   doesn't change after all).
+
    @pre c2_mutex_is_locked(mach->sm_lock)
    @post mach->sm_state == state
    @post c2_mutex_is_locked(mach->sm_lock)
  */
 void c2_sm_state_set(struct c2_sm *mach, int state);
+
+struct c2_sm_timeout {
+	struct c2_timer  st_timer;
+	struct c2_clink  st_clink;
+	struct c2_sm_ast st_ast;
+	int              st_state;
+	bool             st_active;
+};
 
 /**
    Arms a timer to move a machine into a given state after a given timeout.
@@ -214,7 +410,14 @@ void c2_sm_state_set(struct c2_sm *mach, int state);
    @pre c2_mutex_is_locked(mach->sm_lock)
    @post c2_mutex_is_locked(mach->sm_lock)
  */
-int c2_sm_timeout(struct c2_sm *mach, c2_time_t timeout, int state);
+int c2_sm_timeout(struct c2_sm *mach, struct c2_sm_timeout *to,
+		  c2_time_t timeout, int state);
+void c2_sm_timeout_fini(struct c2_sm_timeout *to);
+
+void c2_sm_ast_post(struct c2_sm_group *grp, struct c2_sm_ast *ast);
+void c2_sm_asts_run(struct c2_sm_group *grp);
+
+struct c2_sm_ast *c2_sm_ast_get(struct c2_sm_group *grp);
 
 /** @} end of sm group */
 
