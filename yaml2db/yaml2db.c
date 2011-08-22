123705e6dff4edad2663b8e5e2cea64b043c205d
Anup_Barve <anup_barve@xyratex.com>
2011-08-22 19:26:25 +0530

    Convert stream based parsing to document based parsing

--- yaml2db/yaml2db.c
+++ yaml2db/yaml2db.c
@@ -39,8 +39,8 @@
 
 /* Constant names and paths */
 static const char *D_PATH = "__config_db";
-//static const char *disk_str = "disks";
-//static const char *disk_table = "disk_table";
+static const char *disk_table = "disk_table";
+static char *disk_str = "disks";
 
 /* DB Table ops */
 static int test_key_cmp(struct c2_table *table,
@@ -177,87 +177,6 @@ static void yaml2db_fini(struct c2_yaml2db_ctx *yctx)
         c2_addb_ctx_fini(&yctx->yc_addb);
 }
 
-#if 0
-/**
-  Parsing and setting the disk configuration
-  @param yctx - yaml2db context
- */
-static void yaml2db_disk_conf(struct c2_yaml2db_ctx *yctx)
-{
-	int			rc;
-	struct			c2_table table;
-	struct			c2_db_tx tx;
-	uint64_t		key = 0;
-	struct c2_db_pair	db_pair;
-
-	C2_PRE(yctx != NULL);
-
-	/* Initialize the table */
-	rc = c2_table_init(&table, &yctx->yc_db, disk_table,
-			0, &disk_table_ops);
-	if (rc != 0)
-		return;
-
-	/* Initialize the database transaction */
-	rc = c2_db_tx_init(&tx, &yctx->yc_db, 0);
-	if (rc != 0) {
-		c2_table_fini(&table);
-		return;
-	}
-
-	do{
-		yaml_parser_parse(&yctx->yc_parser, &yctx->yc_event);
-		if (yctx->yc_event.type == YAML_SCALAR_EVENT) {
-			yaml_parser_parse(&yctx->yc_parser, &yctx->yc_event);
-			/* Key to be initialized per disk info */
-			key += 100;
-			c2_db_pair_setup(&db_pair, &table, &key, sizeof key,
-				 yctx->yc_event.data.scalar.value,
-				 sizeof(yctx->yc_event.data.scalar.value));
-			rc = c2_table_insert(&tx, &db_pair);
-			if (rc != 0)
-				return;
-			/* Parse the status keyword */
-			yaml_parser_parse(&yctx->yc_parser, &yctx->yc_event);
-			yaml_parser_parse(&yctx->yc_parser, &yctx->yc_event);
-			key++;
-			c2_db_pair_setup(&db_pair, &table, &key, sizeof key,
-				 yctx->yc_event.data.scalar.value,
-				 sizeof(yctx->yc_event.data.scalar.value));
-			rc = c2_table_insert(&tx, &db_pair);
-			if (rc != 0)
-				return;
-			/* Parse the setting keyword */
-			yaml_parser_parse(&yctx->yc_parser, &yctx->yc_event);
-			yaml_parser_parse(&yctx->yc_parser, &yctx->yc_event);
-			key++;
-			c2_db_pair_setup(&db_pair, &table, &key, sizeof key,
-				 yctx->yc_event.data.scalar.value,
-				 sizeof(yctx->yc_event.data.scalar.value));
-			rc = c2_table_insert(&tx, &db_pair);
-			if (rc != 0)
-				return;
-		}
-	} while(yctx->yc_event.type != YAML_SEQUENCE_END_EVENT);
-
-	c2_db_tx_commit(&tx);
-}
-
-/**
-  Finds whether the event has disks info that can be parsed
-  @param event - yaml event
-  @retval true if disk info present, false otherwise
- */
-static bool event_has_disks(const yaml_event_t *event)
-{
-	C2_PRE(event != NULL);
-
-	if (strcmp((char *)event->data.scalar.value, (char *)disk_str) == 0)
-		return true;
-	return false;
-}
-#endif
-
 /**
   Function to detect and print parsing errors
   @param parser - yaml_parser structure
@@ -319,11 +238,11 @@ static void yaml_parser_error_detect(const yaml_parser_t *parser)
 }
 
 /**
-  Parsing function
+  Function to load the yaml document
   @param yctx - yaml2db context
   @retval 0 if successful, -errno otherwise
  */
-static int yaml2db_parse(struct c2_yaml2db_ctx *yctx)
+static int yaml2db_doc_load(struct c2_yaml2db_ctx *yctx)
 {
 	int		 rc;
 	yaml_node_t	*root_node;
@@ -350,6 +269,160 @@ static int yaml2db_parse(struct c2_yaml2db_ctx *yctx)
 parser_error:
 	yaml_parser_error_detect(&yctx->yc_parser);
 	return -EINVAL;
+}
+
+/**
+  Function to read mapping
+  @param yctx - yaml2db context
+  @param table - database table in which disk structure has to be stored
+  @param tx - database transaction
+  @param node - yaml node used as an anchor from which sequence has to be read
+  @retval 0 if success, -errno otherwise
+*/
+static int read_and_store_disk_mapping(struct c2_yaml2db_ctx *yctx,
+		struct c2_table *table, struct c2_db_tx *tx, yaml_node_t *node)
+{
+	yaml_node_pair_t	*pair;
+	yaml_node_t		*mnode_key;
+	yaml_node_t		*mnode_value;
+        struct c2_db_pair	 db_pair;
+	int			 rc;
+	int			 key = 0;
+
+	C2_PRE(node != NULL);
+	C2_PRE(yctx != NULL);
+
+	for (pair = node->data.mapping.pairs.start;
+			pair < node->data.mapping.pairs.top; pair++) {
+		mnode_key = yaml_document_get_node(&yctx->yc_document,
+				pair->key);
+		mnode_value = yaml_document_get_node(&yctx->yc_document,
+				pair->value);
+		if (pair->key != 0 && pair->value != 0)
+			key++;
+			c2_db_pair_setup(&db_pair, table, &key, sizeof key,
+					mnode_value->data.scalar.value,
+					sizeof(mnode_value->data.scalar.value));
+			rc = c2_table_insert(tx, &db_pair);
+			if (rc != 0) {
+				C2_ADDB_ADD(&yctx->yc_addb, &yaml2db_addb_loc,
+						yaml2db_func_fail,
+						"c2_table_insert", 0);
+				c2_db_pair_release(&db_pair);
+				c2_db_pair_fini(&db_pair);
+				return -EINVAL;
+			}
+			c2_db_pair_release(&db_pair);
+			c2_db_pair_fini(&db_pair);
+	}
+	return 0;
+}
+
+/**
+  Function to read sequence
+  @param yctx- YAML context structure
+  @param node - yaml node used as an anchor from which sequence has to be read
+  @retval 0 if success, -errno otherwise
+*/
+static int read_sequence(struct c2_yaml2db_ctx *yctx, yaml_node_t *node)
+{
+	yaml_node_item_t	*item;
+	yaml_node_t		*snode;
+        struct c2_table 	 table;
+        struct c2_db_tx		 tx;
+	int			 rc;
+
+	C2_PRE(yctx != NULL);
+	C2_PRE(node != NULL);
+
+        /* Initialize the table */
+        rc = c2_table_init(&table, &yctx->yc_db, disk_table,
+                        0, &disk_table_ops);
+        if (rc != 0) {
+                C2_ADDB_ADD(&yctx->yc_addb, &yaml2db_addb_loc,
+				yaml2db_func_fail, "c2_table_init", 0);
+                return -EINVAL;
+	}
+
+        /* Initialize the database transaction */
+        rc = c2_db_tx_init(&tx, &yctx->yc_db, 0);
+        if (rc != 0) {
+                C2_ADDB_ADD(&yctx->yc_addb, &yaml2db_addb_loc,
+				yaml2db_func_fail, "c2_db_tx_init", 0);
+                c2_table_fini(&table);
+                return -EINVAL;
+        }
+
+	for (item = node->data.sequence.items.start;
+			item < node->data.sequence.items.top; item++) {
+		snode = yaml_document_get_node(&yctx->yc_document, *item);
+		if (snode == NULL)
+			return -EINVAL;
+		rc = read_and_store_disk_mapping(yctx, &table, &tx, snode);
+		if (rc != 0) {
+			C2_ADDB_ADD(&yctx->yc_addb, &yaml2db_addb_loc,
+					yaml2db_func_fail,
+					"read_and_store_disk_mapping", 0);
+			return -EINVAL;
+		}
+	}
+	c2_db_tx_commit(&tx);
+	c2_table_fini(&table);
+	return 0;
+}
+
+/**
+  Function to return the yaml_node as an anchor, given a scalar value
+  @param yctx - yaml2db context
+  @param value - scalar value to be checked in the document
+  @retval yaml_node_t pointer if successful, NULL otherwise
+ */
+static yaml_node_t *yaml2db_scalar_locate(struct c2_yaml2db_ctx *yctx,
+		char *value)
+{
+	yaml_node_t	*node;
+	bool		 scalar_found = false;
+
+	C2_PRE(yctx != NULL);
+
+	for (node = yctx->yc_document.nodes.start;
+			node < yctx->yc_document.nodes.top; node++) {
+		if(node->type == YAML_SCALAR_NODE) {
+			if (strcmp((char *)node->data.scalar.value,
+					(char *) value) == 0){
+				scalar_found = true;
+				break;
+			}
+		}
+	}
+
+	if (scalar_found)
+		return node;
+	else
+		return NULL;
+}
+/**
+  Function to parse the yaml document
+  @param yctx - yaml2db context
+  @retval 0 if successful, -errno otherwise
+ */
+static int yaml2db_load_disk_conf(struct c2_yaml2db_ctx *yctx)
+{
+        int                      rc;
+	yaml_node_t		*node;
+
+        C2_PRE(yctx != NULL);
+
+	node = yaml2db_scalar_locate(yctx, disk_str);
+	if (node == NULL) {
+                C2_ADDB_ADD(&yctx->yc_addb, &yaml2db_addb_loc,
+				yaml2db_func_fail, "yaml2db_scalar_locate", 0);
+		return -EINVAL;
+	}
+
+	node++;
+	rc = read_sequence(yctx, node);
+	return rc;
 }
 
 /**
@@ -400,14 +473,22 @@ int main(int argc, char *argv[])
 		return rc;
 	}
 
-	/* Parse and store the configuration contents in the database */
-	rc = yaml2db_parse(&yctx);
+	/* Load the information from yaml file to yaml_document, and check for
+	   parsing errors internally */
+	rc = yaml2db_doc_load(&yctx);
 	if (rc != 0){
                 C2_ADDB_ADD(&yctx.yc_addb, &yaml2db_addb_loc, yaml2db_func_fail,
-                                "yaml2db_parse", 0);
+                                "yaml2db_doc_load", 0);
 		return rc;
 	}
 
+	/* Parse the disk configuration which is loaded in the context */
+	rc = yaml2db_load_disk_conf(&yctx);
+	if (rc != 0){
+                C2_ADDB_ADD(&yctx.yc_addb, &yaml2db_addb_loc, yaml2db_func_fail,
+                                "yaml2db_parse_disk_conf", 0);
+		return rc;
+	}
 
 	/* Finalize the parser and database environment */
 	yaml2db_fini(&yctx);
