82a196874da6fe7e8fd47ea11ede946da872a30c
Madhavrao Vemuri <madhav_vemuri@xyratex.com>
2011-10-18 19:43:10 +0530

    Licences added , Conditions are checked and proper indentation is done

--- ioservice/io_buf_pool.c
+++ ioservice/io_buf_pool.c
@@ -1,31 +1,96 @@
+/* -*- C -*- */
+/*
+ * COPYRIGHT 2011 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Madhavrao Vemuri <madhav_vemuri@xyratex.com>
+ * Original creation date: 12/10/2011
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include "lib/tlist.h"
+#include "lib/memory.h"/* C2_ALLOC_PTR */
+#include "lib/thread.h"/* C2_THREAD_INIT */
+#include "lib/errno.h" /* ENOMEM */
+#include "lib/misc.h"  /* C2_SET0 */
+#include "net/net.h"   /* C2_NET_BUFFER*/
 #include "net/bulk_sunrpc.h"
+#include <unistd.h>
+#include <stdio.h>
 #include "io_buf_pool.h"
+
+/**
+   @addtogroup io_buf_prealloc Buffer pool
+   @{
+ */
+
+/**
+   Initializes a buffer pool.
+   Parameters are cap:capacity of the pool
+		  buf_size:size of each buffer
+		  seg_size:size of each segment in a buffer
+ */
 int c2_buf_pool_init(struct c2_buf_pool *pool, int cap, int buf_size, int seg_size)
 {
-	int nr = 0, NR = 0, i=0;
+	int nr = 0;/* Number of segments in each buffer. */
 	int rc = 0;
+
 	struct c2_net_buffer *nb = NULL;
-	pool->bp_threshold = 1; 
+
+	C2_PRE(pool != NULL);
+	pool->bp_threshold = BUF_POOL_THRESHOLD;
+
 	c2_tlist_init(&buf_pool_descr, &pool->bp_head);
-	NR = cap / buf_size ;
+	
 	do {
 		nr = buf_size / seg_size;
 		cap -= (nr * seg_size);
-		C2_ALLOC_PTR(nb);	
-		C2_ALLOC_PTR(pool->bp_list);	
+	
+		C2_ALLOC_PTR(nb);
+		if (nb == NULL)
+			return -ENOMEM;
+		C2_ALLOC_PTR(pool->bp_list);
+		if (pool->bp_list == NULL)
+			return -ENOMEM;
 		pool->bp_list->bl_nb = nb;
+		
 		rc = c2_bufvec_alloc(&nb->nb_buffer, nr, seg_size);
+		if (rc != 0)
+			goto clean;
+
 		printf("%lu %lu \n",(unsigned long)nb,(unsigned long)&nb->nb_buffer);
+
 		rc = c2_net_buffer_register(nb, &pool->ndom);
+		if (rc != 0)
+			goto clean;
+
 		printf("nr %d cap %d \n",nr,cap);
+
 		c2_tlink_init(&buf_pool_descr, pool->bp_list);
 		C2_ASSERT(!c2_tlink_is_in(&buf_pool_descr, pool->bp_list));
-		printf("tlist %d\n",++i);
 		c2_tlist_add_tail(&buf_pool_descr, &pool->bp_head, pool->bp_list);
-		pool->bp_free++;	
+		pool->bp_free++;
 	} while (cap > 0);
-	return 0;	
+
+	return rc;
+clean :
+	c2_buf_pool_fini(pool);
+	return rc;
 }
+
 /**
    Finalizes a buffer pool.
  */
@@ -33,6 +98,7 @@ void c2_buf_pool_fini(struct c2_buf_pool *pool)
 {
 	struct c2_net_buffer *nb = NULL;
 	int i = 0;
+	C2_PRE(pool != NULL);
 	c2_tlist_for(&buf_pool_descr, &pool->bp_head, pool->bp_list) {
 		nb = pool->bp_list->bl_nb;
 		c2_net_buffer_deregister(nb, &pool->ndom);
@@ -42,20 +108,24 @@ void c2_buf_pool_fini(struct c2_buf_pool *pool)
 		c2_free(nb);
 		c2_free(pool->bp_list);
 		printf("%d %lu \n",++i,(unsigned long)nb);
-		pool->bp_free--;	
+		pool->bp_free--;
 	}
 	c2_tlist_endfor;
 	c2_tlist_fini(&buf_pool_descr, &pool->bp_head);
 }
 
+/* Acquires the lock on buffer pool. */
 void c2_buf_pool_lock(struct c2_buf_pool *pool)
 {
 	c2_mutex_lock(&pool->bp_lock);
 }
+
+/* Releases the lock on buffer pool */
 void c2_buf_pool_unlock(struct c2_buf_pool *pool)
 {
 	c2_mutex_unlock(&pool->bp_lock);
 }
+
 /**
    Returns a network buffer the pool.
    Returns NULL when the pool is empty
@@ -64,35 +134,44 @@ void c2_buf_pool_unlock(struct c2_buf_pool *pool)
 struct c2_net_buffer * c2_buf_pool_get(struct c2_buf_pool *pool)
 {
 	struct c2_net_buffer *nb = NULL;
+
+	C2_PRE(pool != NULL);
 	if(pool->bp_free < pool->bp_threshold) {
-		pool->bp_ops->low(pool);	
-	return NULL;
+		pool->bp_ops->low(pool);
+		return NULL;
 	}
+
 	pool->bp_list = c2_tlist_head(&buf_pool_descr, &pool->bp_head);
 	c2_tlist_del(&buf_pool_descr, pool->bp_list);
 	c2_tlink_fini(&buf_pool_descr, pool->bp_list);
-	nb = pool->bp_list->bl_nb;	
+	nb = pool->bp_list->bl_nb;
 	c2_free(pool->bp_list);
 	printf("GET %lu \n",(unsigned long)nb);
-	pool->bp_free--;	
+	pool->bp_free--;
 
 	return nb;
 }
 
 /**
-   Returns the back to the pool.
+   Returns the buffer back to the pool.
  */
 void c2_buf_pool_put(struct c2_buf_pool *pool, struct c2_net_buffer *buf)
 {
-	C2_ALLOC_PTR(pool->bp_list);	
+	C2_PRE(pool != NULL);
+	C2_PRE(buf != NULL);
+	C2_ALLOC_PTR(pool->bp_list);
+	C2_ASSERT(pool->bp_list != NULL);
+
 	pool->bp_list->bl_nb = buf;
 	printf("PUT %lu \n",(unsigned long)buf);
+
 	c2_tlink_init(&buf_pool_descr, pool->bp_list);
 	C2_ASSERT(!c2_tlink_is_in(&buf_pool_descr, pool->bp_list));
 	c2_tlist_add_tail(&buf_pool_descr, &pool->bp_head, pool->bp_list);
+
 	pool->bp_free++;
-	if(pool->bp_free > pool->bp_threshold) 
-		pool->bp_ops->notEmpty(pool);	
+	if(pool->bp_free > pool->bp_threshold)
+		pool->bp_ops->notEmpty(pool);
 }
 
 /**
@@ -101,7 +180,10 @@ void c2_buf_pool_put(struct c2_buf_pool *pool, struct c2_net_buffer *buf)
 void c2_buf_pool_add(struct c2_buf_pool *pool,struct c2_net_buffer *buf)
 {
 	int rc = 0;
+	C2_PRE(buf != NULL);
+	printf("ADD %lu \n",(unsigned long)buf);
 	rc = c2_net_buffer_register(buf, &pool->ndom);
+	C2_ASSERT(rc == 0);
 	c2_buf_pool_put(pool, buf);
 
 }
@@ -120,28 +202,31 @@ int main()
 
 	int rc;
 	struct c2_thread        *client_thread;
-	int			 nr_client_threads = 512;
+	int			 nr_client_threads = 25;
 	int i;
-	struct c2_net_buffer *nb = NULL;	
+	struct c2_net_buffer *nb = NULL;
 	struct c2_net_xprt *xprt;
 	c2_chan_init(&buf_chan);
 	xprt = &c2_net_bulk_sunrpc_xprt;
 	c2_net_xprt_init(xprt);
 	rc = c2_net_domain_init(&bp.ndom, xprt);
-	rc = c2_buf_pool_init(&bp, 65536, 128, 64);
-	bp.bp_ops = &b_ops;
 	C2_ASSERT(rc == 0);
+	rc = c2_buf_pool_init(&bp, 16384, 512, 256);
+	C2_ASSERT(rc == 0);
+	bp.bp_ops = &b_ops;
 	nb = c2_buf_pool_get(&bp);
 	sleep(1);
 	c2_buf_pool_put(&bp, nb);
-		C2_ALLOC_PTR(nb);	
-		rc = c2_bufvec_alloc(&nb->nb_buffer, 10, 1024);
-	//rc = c2_net_buffer_register(nb, &bp.ndom);
+	C2_ALLOC_PTR(nb);
+	if (nb == NULL)
+		return -ENOMEM;
+	rc = c2_bufvec_alloc(&nb->nb_buffer, 10, 1024);
+	C2_ASSERT(rc == 0);
 	c2_buf_pool_add(&bp, nb);
-	
-	//c2_net_domain_fini(&bp.ndom);
-	//c2_net_xprt_fini(xprt);
+
 	C2_ALLOC_ARR(client_thread, nr_client_threads);
+	if (client_thread == NULL)
+		return -ENOMEM;
 	for (i = 0; i < nr_client_threads; i++) {
 		C2_SET0(&client_thread[i]);
 		rc = C2_THREAD_INIT(&client_thread[i], int,
@@ -162,7 +247,7 @@ int main()
 
 void buffers_get_put(int rc)
 {
-	struct c2_net_buffer *nb = NULL;	
+	struct c2_net_buffer *nb = NULL;
 	struct c2_clink buf_link;
 	c2_clink_init(&buf_link, NULL);
 	c2_clink_add(&buf_chan, &buf_link);
@@ -183,10 +268,22 @@ void buffers_get_put(int rc)
 }
 void NotEmpty(struct c2_buf_pool *bp)
 {
-	//printf("Buffer pool is Not empty \n");
 	c2_chan_signal(&buf_chan);
 }
 void Low(struct c2_buf_pool *bp)
 {
 	printf("Buffer pool is LOW \n");
 }
+
+/** @} end of io_buf_prealloc */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
+
--- ioservice/io_buf_pool.h
+++ ioservice/io_buf_pool.h
@@ -1,112 +1,96 @@
-#include "lib/tlist.h"
-#include "lib/memory.h"
-#include "lib/thread.h"
-#include "lib/misc.h" /* C2_SET0 */
-#include "net/net.h"  /* C2_NET_BUFFER*/
-#include <unistd.h>
-#include <stdio.h>
+/*
+ * COPYRIGHT 2011 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Madhavrao Vemuri <madhav_vemuri@xyratex.com>
+ * Original creation date: 12/10/2011
+ */
+
+#ifndef __COLIBRI_IO_BUF_POOL_H__
+#define __COLIBRI_IO_BUF_POOL_H__
+
 enum {
-	BUF_SIZE = 64
+	/* Size of each segment in a network buffer. */
+	SEG_SIZE           = 64,
+	/* 
+	  Minimum number of buffers below which low memory condition 
+	  occurs.
+	 */
+	BUF_POOL_THRESHOLD = 1
 };
+
+/* A pool of buffers and its context.*/
 struct c2_buf_pool;
+/* A list of buffers. */
 struct c2_buf_pool_list;
-/**
-   Initializes a buffer pool.
-   Parameters are cap:capacity of the pool
-		  buf_size:size of each network buffer
-		  seg_size:size of each segment in a network buffer
- */
+
 int c2_buf_pool_init(struct c2_buf_pool *pool, int cap, int buf_size, int seg_size);
-
-/**
-   Finalizes a buffer pool.
- */
 void c2_buf_pool_fini(struct c2_buf_pool *pool);
-
 void c2_buf_pool_lock(struct c2_buf_pool *pool);
 void c2_buf_pool_unlock(struct c2_buf_pool *pool);
-
-/**
-   Returns a network buffer the pool.
-   Returns NULL when the pool is empty
-   @pre pool is locked
- */
 struct c2_net_buffer * c2_buf_pool_get(struct c2_buf_pool *pool);
-
-/**
-   Returns the back to the pool.
- */
 void c2_buf_pool_put(struct c2_buf_pool *pool, struct c2_net_buffer *buf);
-
-/**
-   Adds a new buffer to the pool to increase the capacity.
- */
 void c2_buf_pool_add(struct c2_buf_pool *pool,struct c2_net_buffer *buf);
 
-
+/* Call backs that buffer pool can trigger on different memory conditions.*/
 struct c2_buf_pool_ops {
+	/* Buffer pool is not empty. */
 	void (*notEmpty)(struct c2_buf_pool *);
+	/* Buffers in memory are lower than threshold. */
 	void (*low)(struct c2_buf_pool *);
 };
 
+/* Buffer list contains list of buffers. */
 struct c2_buf_list {
-struct c2_tlink bl_link;
-uint64_t	bl_magic;
-struct c2_net_buffer *bl_nb;
+	/* Link for tlist. */
+	struct c2_tlink	      bl_link;
+	/* Magic for tlist. */
+	uint64_t	      bl_magic;
+	/* List of buffers to be stored in tlist. */
+	struct c2_net_buffer *bl_nb;
 };
 
+/* Buffer pool context. */
 struct c2_buf_pool {
-uint32_t bp_free;
-uint32_t bp_threshold;
-struct c2_addb_ctx bp_addb;
-
-struct c2_mutex bp_lock;
-struct c2_buf_list *bp_list;
-struct c2_buf_pool_ops *bp_ops;
-struct c2_tl bp_head;
-struct c2_net_domain ndom;
+	/* Number of free buffers in the pool. */
+	uint32_t bp_free;
+	/* Number of buffer below which low memory condtion occurs. */
+	uint32_t bp_threshold;
+	/* Addb context for buffer pool. */
+	struct c2_addb_ctx bp_addb;
+	/* Buffer pool lock. */
+	struct c2_mutex bp_lock;
+	/* The list of buffers */
+	struct c2_buf_list *bp_list;
+	/* Call back opeartions can be triggered buffer pool. */
+	struct c2_buf_pool_ops *bp_ops;
+	/* Head of list of buffers in the pool. */
+	struct c2_tl bp_head;
+	/* Network domain to register the buffers. */
+	struct c2_net_domain ndom;
 };
 
 enum {
-	/* Hex ASCII value of "iob_link" */
-	BUF_POOL_LINK_MAGIC = 0x696f625f6c696e6b,
-	/* Hex ASCII value of "iob_head" */
-	BUF_POOL_HEAD_MAGIC = 0x696f625f68656164,
+	/* Hex ASCII value of "bl_link" */
+	BUF_POOL_LINK_MAGIC = 0x626c5f6c696e6b,
+	/* Hex ASCII value of "bl_head" */
+	BUF_POOL_HEAD_MAGIC = 0x626c5f68656164,
 };
 
+/* Descriptor for the tlist of buffers. */
 static const struct c2_tl_descr buf_pool_descr =
 		    C2_TL_DESCR("buf_pool_descr",
 		    struct c2_buf_list, bl_link, bl_magic,
 		    BUF_POOL_LINK_MAGIC, BUF_POOL_HEAD_MAGIC);
-
-/*
-struct c2_buf_pool {
-uint32_t bp_free;
-uint_32t bp_threshold;
-struct c2_mutex list_lock;
-static struct c2_tl buf_head;
-struct c2_addb_ctx bp_addb;
-
-struct c2_buf_pool_list *bp_list;
-
-};
-
-struct c2_buf_pool_list {
-struct c2_tlink bpl_link;
-uint64_t        bpl_magic;
-struct c2_net_buffer nb;
-}
-*/
-
-//enum {
-	/* Hex ASCII value of "bpl_link" */
-//	IO_BUF_LINK_MAGIC = 0x62706c5f6c696e6b,
-	/* Hex ASCII value of "bpl_head" */
-//	IO_BUF_HEAD_MAGIC = 0x62706c5f68656164,
-//};
-
-//static const struct c2_tl_descr bpl_descr =
-//		    C2_TL_DESCR("bpl_descr",
-//		    struct c2_buf_pool_list, bpl_link, bpl_magic,
-//		    IO_BUF_LINK_MAGIC, IO_BUF_HEAD_MAGIC);
-//
+#endif
