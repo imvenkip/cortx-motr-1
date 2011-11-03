2e480d2c407a79087a3d2cab3914726ce2f59628
Anup_Barve <anup_barve@xyratex.com>
2011-11-03 15:00:38 +0530

    Added concurrency test where, N threads independently use the same cobfid map structure to insert data and share a mutex for serialization. At the end, each thread independently enumerates its container/file After the threads join, an enumeration of the entire map shows the data from all the threads in correct order.

--- ioservice/ut/cobfid_map.c
+++ ioservice/ut/cobfid_map.c
@@ -467,6 +467,192 @@ void test_iter_sensitivity(void)
 
 }
 
+/* Number of threads that will concurrently access a cobfid_map */
+enum {
+	CFM_THREAD_NR = 4,
+};
+
+struct c2_mutex		cfm_global_mutex;
+struct c2_dbenv		cfm_global_dbenv;
+struct c2_cobfid_map	cfm_global_map;
+struct c2_addb_ctx	cfm_global_addb_ctx;
+
+/* Function to add and enumerate a container */
+void cfm_op(int nr)
+{
+	int			 i;
+	int			 j;
+	int			 rc;
+	int			 rec_nr;
+	int			 rec_total;
+        uint64_t                 container_id_in;
+        uint64_t                 container_id_out;
+        struct c2_fid           *fid_in;
+        struct c2_fid           *fid_out;
+        struct c2_uint128       *cob_fid_in;
+        struct c2_uint128       *cob_fid_out;
+        struct c2_db_tx          cfm_dbtx;
+        struct c2_cobfid_map_iter cfm_iter;
+
+	rec_total = MULTIPLE_BUF_REC_NR;
+
+	C2_ALLOC_ARR(fid_in, rec_total);
+        C2_UT_ASSERT(fid_in != NULL);
+        
+        C2_ALLOC_ARR(cob_fid_in, rec_total);
+        C2_UT_ASSERT(cob_fid_in != NULL);
+        
+        C2_ALLOC_ARR(fid_out, rec_total);
+        C2_UT_ASSERT(fid_out != NULL);
+        
+        C2_ALLOC_ARR(cob_fid_out, rec_total);
+        C2_UT_ASSERT(cob_fid_out != NULL);
+
+	/* Container id is based on the thread id (value of nr) */
+	container_id_in = 200 + nr;
+	j = rec_total - 1;
+
+        /* Fill in the database with varying fid values in
+           decreasing order of fid key. This is done on purpose to test the
+           ordering property where enumeration should be done in increasing
+           order of the fid key.*/
+        for (i = 0; i < rec_total; i++) {
+                /* Populate some random data */
+                fid_in[i].f_container = 0;
+                fid_in[i].f_key = j;
+                cob_fid_in[i].u_hi = 333;
+                cob_fid_in[i].u_lo = j;
+
+		c2_mutex_lock(&cfm_global_mutex);
+		rc = c2_db_tx_init(&cfm_dbtx, &cfm_global_dbenv, 0);
+		C2_UT_ASSERT(rc == 0);
+
+		rc = c2_cobfid_map_add(&cfm_global_map, container_id_in,
+				       fid_in[i], cob_fid_in[i], &cfm_dbtx);
+		printf("\nADD : TID : %d cid : %lu fid : %lu cob_fid : %lu",
+			nr, container_id_in, fid_in[i].f_key,
+			cob_fid_in[i].u_lo); 
+		c2_db_tx_commit(&cfm_dbtx);
+		c2_mutex_unlock(&cfm_global_mutex);
+                C2_UT_ASSERT(rc == 0);
+                j--;
+        }
+
+        rec_nr = 0;
+
+	c2_mutex_lock(&cfm_global_mutex);
+	rc = c2_db_tx_init(&cfm_dbtx, &cfm_global_dbenv, 0);
+	C2_UT_ASSERT(rc == 0);
+	rc = c2_cobfid_map_container_enum(&cfm_global_map, container_id_in,
+					  &cfm_iter, &cfm_dbtx);
+	printf("\nENI : TID : %d ",nr);
+	C2_UT_ASSERT(rc == 0);
+	while ((rc = c2_cobfid_map_iter_next(&cfm_iter, &container_id_out,
+					     &fid_out[rec_nr],
+					     &cob_fid_out[rec_nr])) == 0) {
+		rec_nr++;
+	}
+	c2_cobfid_map_iter_fini(&cfm_iter);
+	c2_db_tx_commit(&cfm_dbtx);
+	c2_mutex_unlock(&cfm_global_mutex);
+
+        C2_UT_ASSERT(rec_nr == rec_total);
+
+        /* Check if the fid and cob input arrays are exact reverse of their
+           corresponding out counterparts. Also check the cid array in case
+           of map enumeration*/
+        for (i = 0; i < rec_total; i++) {
+                C2_UT_ASSERT(c2_fid_eq(&fid_in[i],
+                                       &fid_out[rec_total - 1 - i]));
+                C2_UT_ASSERT(c2_uint128_eq(&cob_fid_in[i],
+                                           &cob_fid_out[rec_total - 1 - i]));
+        }
+
+        c2_free(fid_in);
+        c2_free(cob_fid_in);
+        c2_free(fid_out);
+        c2_free(cob_fid_out);
+
+	printf("\nnr = %d\n", nr);
+}
+
+/* Test concurrent operation across multiple threads for a cobfid_map */
+void test_cfm_concurrency(void)
+{
+	int			  i;
+	int			  rc;
+	int			  rec_nr;
+	struct c2_thread	 *cfm_thread;
+	struct c2_cobfid_map_iter cfm_iter;
+	struct c2_db_tx		  cfm_dbtx;
+	uint64_t		  cid_out;
+	struct c2_uint128         cob_fid_out;
+	struct c2_fid             fid_out;
+
+	/* Reset any existing database */
+	rc = c2_ut_db_reset(concurrency_test_map);
+	C2_UT_ASSERT(rc == 0);
+
+        /* Initialise the database with given path */
+        rc = c2_dbenv_init(&cfm_global_dbenv, concurrency_test_map, 0);
+        C2_UT_ASSERT(rc == 0);	
+
+	/* Initialize the map */
+	rc = c2_cobfid_map_init(&cfm_global_map, &cfm_global_dbenv,
+				&cfm_global_addb_ctx, "cfm_map_table");
+	C2_UT_ASSERT(rc == 0);
+
+	c2_mutex_init(&cfm_global_mutex);
+
+	C2_ALLOC_ARR(cfm_thread, CFM_THREAD_NR);
+	C2_UT_ASSERT(cfm_thread != NULL);
+	
+	/* Spawn threads which will independently add and enumerate */
+	for (i = 0; i < CFM_THREAD_NR; ++i) {
+	       rc = C2_THREAD_INIT(&cfm_thread[i], int, NULL, &cfm_op, i,
+			           "cfm_thread_%d", i);
+	       C2_UT_ASSERT(rc == 0);
+	}
+
+	for (i = 0; i < CFM_THREAD_NR; ++i) {
+		c2_thread_join(&cfm_thread[i]);
+	}	
+
+	c2_free(cfm_thread);
+	c2_mutex_fini(&cfm_global_mutex);
+
+
+	/* Map enumeration for records inserted by above threads */
+        rc = c2_db_tx_init(&cfm_dbtx, &cfm_global_dbenv, 0);
+        C2_UT_ASSERT(rc == 0);
+
+	rc = c2_cobfid_map_enum(&cfm_global_map, &cfm_iter, &cfm_dbtx);
+	C2_UT_ASSERT(rc == 0);
+	while ((rc = c2_cobfid_map_iter_next(&cfm_iter,
+					&cid_out,
+					&fid_out,
+					&cob_fid_out)) == 0) {
+		printf("\nENUM : cid : %lu fid : %lu cob_fid : %lu",
+			cid_out, fid_out.f_key,
+			cob_fid_out.u_lo); 
+		rec_nr++;
+	}
+	C2_UT_ASSERT(cfm_iter.cfmi_error == -ENOENT);
+	C2_UT_ASSERT(rc == -ENOENT);
+
+        c2_cobfid_map_iter_fini(&cfm_iter);
+
+        /* Check if number of records enumerated is same as number of records
+           inserted */
+        //C2_UT_ASSERT(rec_nr == rec_total);
+
+	c2_cobfid_map_fini(&cfm_global_map);
+	c2_dbenv_fini(&cfm_global_dbenv);
+
+	rc = c2_ut_db_reset(concurrency_test_map);
+	C2_UT_ASSERT(rc == 0);
+}
+
 const struct c2_test_suite cfm_ut = {
 	.ts_name = "cfm-ut",
 	.ts_init = NULL,
@@ -477,6 +663,7 @@ const struct c2_test_suite cfm_ut = {
 		{ "cfm-map-enumerate-single-buffer", me_single_buf },
 		{ "cfm-map-enumerate-multiple-buffers", me_multiple_buf },
 		{ "cfm_iter_sensitivity", test_iter_sensitivity },
+		{ "cfm_concurrency", test_cfm_concurrency },
 		{ NULL, NULL }
 	}
 };
