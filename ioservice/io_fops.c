b47e432a7fbeca8b222faf264025aa6cb613a09e
anand.vidwansa <anand_vidwansa@xyratex.com>
2011-11-14 19:45:14 +0530

    fop2c.in changes - Got rid of the array from fop2c script. - Moved the extern _tfmt declarations after #include files.

--- ioservice/io_fops.c
+++ ioservice/io_fops.c
@@ -31,15 +31,16 @@
 #include "ioservice/io_fops_u.h"
 #endif
 
-extern struct c2_fop_type_format c2_net_buf_desc_tfmt;
-extern struct c2_fop_type_format c2_addb_record_tfmt;
-
 #include "lib/errno.h"
 #include "lib/memory.h"
 #include "xcode/bufvec_xcode.h" /* c2_xcode_fop_size_get() */
 #include "fop/fop_format_def.h"
-#include "ioservice/io_fops.ff"
 #include "lib/vec.h"	/* c2_0vec */
+
+extern struct c2_fop_type_format c2_net_buf_desc_tfmt;
+extern struct c2_fop_type_format c2_addb_record_tfmt;
+
+#include "ioservice/io_fops.ff"
 
 /**
    The IO fops code has been generalized to suit both read and write fops
