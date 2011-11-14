8a6d7a958e19891b3ddb1589301d410afd1b376f
Amit Jambure <Amit_Jambure@xyratex.com>
2011-11-14 17:47:48 +0530

    * Added stub routines to create target component objects.

--- c2t1fs/dir.c
+++ c2t1fs/dir.c
@@ -26,6 +26,9 @@ static int c2t1fs_readdir(struct file *f,
 
 static int c2t1fs_unlink(struct inode *dir, struct dentry *dentry);
 
+static int c2t1fs_create_target_objects(struct c2t1fs_inode *ci);
+int c2t1fs_cob_create(struct c2t1fs_sb *csb, struct c2_fid cob_fid);
+
 struct file_operations c2t1fs_dir_file_operations = {
 	.read    = generic_read_dir,
 	.readdir = c2t1fs_readdir,
@@ -82,15 +85,21 @@ static int c2t1fs_create(struct inode     *dir,
 	insert_inode_hash(inode);
 	mark_inode_dirty(inode);
 
+	rc = c2t1fs_create_target_objects(ci);
+	if (rc != 0)
+		goto out;
+
 	rc = c2t1fs_dir_ent_add(dir, dentry->d_name.name, dentry->d_name.len,
 					ci->ci_fid);
-	if (rc != 0) {
-		inode_dec_link_count(inode);
-		iput(inode);
+	if (rc != 0)
 		goto out;
-	}
+
 	d_instantiate(dentry, inode);
+	END(0);
+	return 0;
 out:
+	inode_dec_link_count(inode);
+	iput(inode);
 	END(rc);
 	return rc;
 }
@@ -336,4 +345,71 @@ static int c2t1fs_unlink(struct inode *dir, struct dentry *dentry)
 out:
 	END(rc);
 	return rc;
+}
+
+struct c2_fid c2t1fs_target_fid(const struct c2_fid gob_fid, int index)
+{
+	struct c2_fid fid;
+
+	START();
+
+	fid.f_container = index;
+	fid.f_key       = gob_fid.f_key;
+
+	TRACE("Out: [%lu:%lu]\n", (unsigned long)fid.f_container,
+				  (unsigned long)fid.f_key);
+	END(0);
+	return fid;
+}
+static int c2t1fs_create_target_objects(struct c2t1fs_inode *ci)
+{
+	struct c2t1fs_sb *csb;
+	struct c2_fid     gob_fid;
+	struct c2_fid     cob_fid;
+	int               nr_containers;
+	int               i;
+	int rc;
+
+	START();
+
+	gob_fid = ci->ci_fid;
+
+	TRACE("Create target objects for [%lu:%lu]\n",
+				(unsigned long)gob_fid.f_container,
+				(unsigned long)gob_fid.f_key);
+
+	csb = C2T1FS_SB(ci->ci_inode.i_sb);
+	nr_containers = csb->csb_nr_containers;
+	C2_ASSERT(nr_containers >= 1);
+
+	for (i = 1; i <= nr_containers; i++) {
+		cob_fid = c2t1fs_target_fid(gob_fid, i);
+		rc = c2t1fs_cob_create(csb, cob_fid);
+		if (rc != 0) {
+			TRACE("Failed: create [%lu:%lu]\n",
+				(unsigned long)cob_fid.f_container,
+				(unsigned long)cob_fid.f_key);
+			goto out;
+		}
+	}
+out:
+	END(rc);
+	return rc;
+}
+
+int c2t1fs_cob_create(struct c2t1fs_sb *csb, struct c2_fid cob_fid)
+{
+	struct c2_rpc_session *session;
+
+	START();
+
+	session = c2t1fs_container_id_to_session(csb, cob_fid.f_container);
+	C2_ASSERT(session != NULL);
+
+	TRACE("Send cob_create [%lu:%lu] to session %lu\n",
+				(unsigned long)cob_fid.f_container,
+				(unsigned long)cob_fid.f_key,
+				(unsigned long)session->s_session_id);
+	END(0);
+	return 0;
 }
