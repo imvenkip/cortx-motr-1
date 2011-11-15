3d960986b5b17eec605c416f17c185c17d246401
Dipak_Dudhabhate <dipak_dudhabhate@xyratex.com>
2011-11-15 22:26:03 +0530

    Removed bugs in console UT

--- console/ut/console.c
+++ console/ut/console.c
@@ -146,13 +146,13 @@ static void check_values(struct c2_fit *it)
 
 static void fop_iterator_test(void)
 {
-        struct c2_cons_fop_test *fop;
+	struct c2_cons_fop_test *fop;
         struct c2_fop           *f;
         struct c2_fit            it;
 
         f = c2_fop_alloc(&c2_cons_fop_test_fopt, NULL);
         C2_UT_ASSERT(f != NULL);
-        fop = c2_fop_data(f);
+	fop = c2_fop_data(f);
 
         c2_fop_all_object_it_init(&it, f);
 	init_test_fop(fop);
@@ -183,7 +183,6 @@ static void yaml_basic_test(void)
 
 static void input_test(void)
 {
-        struct c2_cons_fop_test *fop;
         struct c2_fop           *f;
         struct c2_fit            it;
 	int			 result;
@@ -195,7 +194,6 @@ static void input_test(void)
 
         f = c2_fop_alloc(&c2_cons_fop_test_fopt, NULL);
         C2_UT_ASSERT(f != NULL);
-        fop = c2_fop_data(f);
 
         c2_fop_all_object_it_init(&it, f);
         c2_cons_fop_obj_input(&it);
@@ -232,7 +230,6 @@ static void file_compare(const char *in, const char *out)
 
 static void output_test(void)
 {
-        struct c2_cons_fop_test *fop;
         struct c2_fop           *f;
         struct c2_fit            it;
 	FILE			*fp;
@@ -247,7 +244,6 @@ static void output_test(void)
 
         f = c2_fop_alloc(&c2_cons_fop_test_fopt, NULL);
         C2_UT_ASSERT(f != NULL);
-        fop = c2_fop_data(f);
 
 	/* save fd of stdout */
 	fd = dup(fileno(stdout));
