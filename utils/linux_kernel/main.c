af86d7d615f709925c9d27c6269fe1d1e2e6d0d5
Dave Cohrs <dave_cohrs@us.xyratex.com>
2012-01-04 15:45:03 -0800

    LNet UT framework (only kernel compile for now)

--- utils/linux_kernel/main.c
+++ utils/linux_kernel/main.c
@@ -35,6 +35,7 @@ extern const struct c2_test_suite c2_net_bulk_if_ut;
 extern const struct c2_test_suite c2_net_bulk_mem_ut;
 extern const struct c2_test_suite c2_net_bulk_sunrpc_ut;
 extern const struct c2_test_suite c2_net_ksunrpc_ut;
+extern const struct c2_test_suite c2_net_lnet_ut;
 extern const struct c2_test_suite buffer_pool_ut;
 
 static struct c2_thread ut_thread;
@@ -49,6 +50,7 @@ static void run_kernel_ut(int ignored)
 	c2_ut_add(&c2_net_bulk_mem_ut);
 	c2_ut_add(&c2_net_bulk_sunrpc_ut);
 	c2_ut_add(&c2_net_ksunrpc_ut);
+	c2_ut_add(&c2_net_lnet_ut);
 	c2_ut_add(&buffer_pool_ut);
 	c2_ut_run(NULL);
 	c2_uts_fini();
