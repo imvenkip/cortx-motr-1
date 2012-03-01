9cf02d09b72b8bfe0ce8f82f8ddca22ba7213b89
Madhavrao Vemuri <madhav_vemuri@xyratex.com>
2012-03-01 19:52:53 +0530

    st scripts are moved to c2t1fs/linux/kernel/st

---
+++ c2t1fs/linux_kernel/c2t1fs.c
@@ -0,0 +1,213 @@
+/*
+ * COPYRIGHT 2011 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Yuriy Umanets <yuriy_umanets@xyratex.com>
+ *                  Amit Jambure <amit_jambure@xyratex.com>
+ * Original creation date: 05/04/2010
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include "c2t1fs.h"
+#include "lib/trace.h"
+#include "ioservice/io_fops.h"
+
+static char *local_addr = "127.0.0.1:12345:1";
+
+module_param(local_addr, charp, S_IRUGO);
+MODULE_PARM_DESC(local_addr, "End-point address of c2t1fs e.g. 127.0.0.1:12345:1");
+
+static int  c2t1fs_net_init(void);
+static void c2t1fs_net_fini(void);
+
+static int  c2t1fs_rpc_init(void);
+static void c2t1fs_rpc_fini(void);
+
+extern struct c2_net_xprt c2_net_bulk_sunrpc_xprt;
+
+static struct file_system_type c2t1fs_fs_type = {
+	.owner        = THIS_MODULE,
+	.name         = "c2t1fs",
+	.get_sb       = c2t1fs_get_sb,
+	.kill_sb      = c2t1fs_kill_sb,
+	.fs_flags     = FS_BINARY_MOUNTDATA
+};
+
+enum {
+	C2T1FS_COB_DOM_ID = 40
+};
+#define C2T1FS_DB_NAME "c2t1fs.db"
+
+struct c2t1fs_globals c2t1fs_globals = {
+	.g_xprt       = &c2_net_bulk_sunrpc_xprt,
+	.g_cob_dom_id = { .id = C2T1FS_COB_DOM_ID },
+	.g_db_name    = C2T1FS_DB_NAME,
+};
+
+int c2t1fs_init(void)
+{
+	int rc;
+
+	C2_TRACE_START();
+
+	c2t1fs_globals.g_laddr = local_addr;
+
+	C2_TRACE("local_addr: %s\n", local_addr);
+
+	rc = c2_ioservice_fop_init();
+	if (rc != 0)
+		goto out;
+
+	rc = c2t1fs_inode_cache_init();
+	if (rc != 0)
+		goto out;
+
+	rc = c2t1fs_net_init();
+	if (rc != 0)
+		goto icache_fini;
+
+	rc = c2t1fs_rpc_init();
+	if (rc != 0)
+		goto net_fini;
+
+	rc = register_filesystem(&c2t1fs_fs_type);
+	if (rc != 0)
+		goto rpc_fini;
+
+	C2_TRACE_END(0);
+	return 0;
+
+rpc_fini:
+	c2t1fs_rpc_fini();
+
+net_fini:
+	c2t1fs_net_fini();
+
+icache_fini:
+	c2t1fs_inode_cache_fini();
+
+out:
+	C2_TRACE_END(rc);
+	C2_ASSERT(rc != 0);
+	return rc;
+}
+
+void c2t1fs_fini(void)
+{
+	C2_TRACE_START();
+
+	(void)unregister_filesystem(&c2t1fs_fs_type);
+
+	c2t1fs_rpc_fini();
+	c2t1fs_net_fini();
+	c2t1fs_inode_cache_fini();
+	c2_ioservice_fop_fini();
+
+	C2_TRACE_END(0);
+}
+
+static int c2t1fs_net_init(void)
+{
+	struct c2_net_xprt   *xprt;
+	struct c2_net_domain *ndom;
+	int                   rc;
+
+	C2_TRACE_START();
+
+	xprt =  c2t1fs_globals.g_xprt;
+	ndom = &c2t1fs_globals.g_ndom;
+
+	rc = c2_net_xprt_init(xprt);
+	if (rc != 0)
+		goto out;
+
+	rc = c2_net_domain_init(ndom, xprt);
+	if (rc != 0)
+		c2_net_xprt_fini(xprt);
+out:
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+static void c2t1fs_net_fini(void)
+{
+	C2_TRACE_START();
+
+	c2_net_domain_fini(&c2t1fs_globals.g_ndom);
+	c2_net_xprt_fini(c2t1fs_globals.g_xprt);
+
+	C2_TRACE_END(0);
+}
+
+static int c2t1fs_rpc_init(void)
+{
+	struct c2_dbenv          *dbenv;
+	struct c2_cob_domain     *cob_dom;
+	struct c2_cob_domain_id  *cob_dom_id;
+	struct c2_rpcmachine     *rpcmachine;
+	struct c2_net_domain     *ndom;
+	char                     *laddr;
+	char                     *db_name;
+	int                       rc;
+
+	C2_TRACE_START();
+
+	dbenv   = &c2t1fs_globals.g_dbenv;
+	db_name =  c2t1fs_globals.g_db_name;
+
+	rc = c2_dbenv_init(dbenv, db_name, 0);
+	if (rc != 0)
+		goto out;
+
+	cob_dom    = &c2t1fs_globals.g_cob_dom;
+	cob_dom_id = &c2t1fs_globals.g_cob_dom_id;
+
+	rc = c2_cob_domain_init(cob_dom, dbenv, cob_dom_id);
+	if (rc != 0)
+		goto dbenv_fini;
+
+	ndom       = &c2t1fs_globals.g_ndom;
+	laddr      =  c2t1fs_globals.g_laddr;
+	rpcmachine = &c2t1fs_globals.g_rpcmachine;
+
+	rc = c2_rpcmachine_init(rpcmachine, cob_dom, ndom, laddr, NULL/*reqh*/);
+	if (rc != 0)
+		goto cob_dom_fini;
+
+	C2_TRACE_END(rc);
+	return 0;
+
+cob_dom_fini:
+	c2_cob_domain_fini(cob_dom);
+
+dbenv_fini:
+	c2_dbenv_fini(dbenv);
+
+out:
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+static void c2t1fs_rpc_fini(void)
+{
+	C2_TRACE_START();
+
+	c2_rpcmachine_fini(&c2t1fs_globals.g_rpcmachine);
+	c2_cob_domain_fini(&c2t1fs_globals.g_cob_dom);
+	c2_dbenv_fini(&c2t1fs_globals.g_dbenv);
+
+	C2_TRACE_END(0);
+}
---
+++ c2t1fs/linux_kernel/dir.c
@@ -0,0 +1,517 @@
+/*
+ * COPYRIGHT 2011 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Amit Jambure <amit_jambure@xyratex.com>
+ * Original creation date: 10/14/2011
+ */
+
+#include "lib/misc.h"      /* C2_SET0() */
+#include "lib/memory.h"    /* C2_ALLOC_PTR() */
+#include "lib/trace.h"     /* C2_TRACE*() */
+#include "c2t1fs.h"
+
+static int c2t1fs_create(struct inode     *dir,
+			 struct dentry    *dentry,
+			 int               mode,
+			 struct nameidata *nd);
+
+static struct dentry *c2t1fs_lookup(struct inode     *dir,
+				    struct dentry    *dentry,
+				    struct nameidata *nd);
+
+static int c2t1fs_dir_ent_add(struct inode        *dir,
+			      const unsigned char *name,
+			      int                  namelen,
+			      const struct c2_fid *fid);
+
+static int c2t1fs_readdir(struct file *f,
+			  void        *dirent,
+			  filldir_t    filldir);
+
+static int c2t1fs_unlink(struct inode *dir, struct dentry *dentry);
+
+static int c2t1fs_create_component_objects(struct c2t1fs_inode *ci);
+
+static int c2t1fs_cob_create(struct c2t1fs_sb    *csb,
+			     const struct c2_fid *cob_fid);
+
+const struct file_operations c2t1fs_dir_file_operations = {
+	.read    = generic_read_dir,    /* provided by linux kernel */
+	.readdir = c2t1fs_readdir,
+	.fsync   = simple_fsync,	/* provided by linux kernel */
+	.llseek  = generic_file_llseek, /* provided by linux kernel */
+};
+
+const struct inode_operations c2t1fs_dir_inode_operations = {
+	.create = c2t1fs_create,
+	.lookup = c2t1fs_lookup,
+	.unlink = c2t1fs_unlink
+};
+
+/**
+   Allocate fid of global file.
+
+   See "Containers and component objects" section in c2t1fs.h for
+   more information.
+
+   XXX temporary.
+ */
+static struct c2_fid c2t1fs_fid_alloc(struct c2t1fs_sb *csb)
+{
+	struct c2_fid fid;
+
+	C2_PRE(c2t1fs_fs_is_locked(csb));
+
+	fid.f_container = 0;
+	fid.f_key       = csb->csb_next_key++;
+
+	return fid;
+}
+
+static int c2t1fs_create(struct inode     *dir,
+                         struct dentry    *dentry,
+                         int               mode,
+                         struct nameidata *nd)
+{
+	struct super_block  *sb = dir->i_sb;
+	struct c2t1fs_sb    *csb = C2T1FS_SB(sb);
+	struct c2t1fs_inode *ci;
+	struct inode        *inode;
+	int                  rc;
+
+	C2_TRACE_START();
+
+	/* Flat file system. create allowed only on root directory */
+	C2_ASSERT(c2t1fs_inode_is_root(dir));
+
+	/* new_inode() will call c2t1fs_alloc_inode() using super_operations */
+	inode = new_inode(sb);
+	if (inode == NULL) {
+		C2_TRACE_END(-ENOMEM);
+		return -ENOMEM;
+	}
+
+	c2t1fs_fs_lock(csb);
+
+	inode->i_uid    = 0;
+	inode->i_gid    = 0;
+	inode->i_mtime  = inode->i_atime = inode->i_ctime = CURRENT_TIME_SEC;
+	inode->i_blocks = 0;
+	inode->i_op     = &c2t1fs_reg_inode_operations;
+	inode->i_fop    = &c2t1fs_reg_file_operations;
+	inode->i_mode   = mode;
+
+	ci              = C2T1FS_I(inode);
+	ci->ci_fid      = c2t1fs_fid_alloc(csb);
+	inode->i_ino    = ci->ci_fid.f_key;
+
+	insert_inode_hash(inode);
+	mark_inode_dirty(inode);
+
+	rc = c2t1fs_inode_layout_init(ci, csb->csb_nr_data_units,
+					  csb->csb_nr_parity_units,
+					  csb->csb_pool_width,
+					  csb->csb_unit_size);
+	if (rc != 0)
+		goto out;
+
+	rc = c2t1fs_create_component_objects(ci);
+	if (rc != 0)
+		goto out;
+
+	rc = c2t1fs_dir_ent_add(dir, dentry->d_name.name, dentry->d_name.len,
+					&ci->ci_fid);
+	if (rc != 0)
+		goto out;
+
+	c2t1fs_fs_unlock(csb);
+
+	d_instantiate(dentry, inode);
+	C2_TRACE_END(0);
+	return 0;
+out:
+	inode_dec_link_count(inode);
+	c2t1fs_fs_unlock(csb);
+	iput(inode);
+
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+void c2t1fs_dir_ent_init(struct c2t1fs_dir_ent *de,
+			 const unsigned char   *name,
+			 int                    namelen,
+			 const struct c2_fid   *fid)
+{
+	C2_TRACE_START();
+
+	memcpy(&de->de_name, name, namelen);
+	de->de_name[namelen] = '\0';
+	de->de_fid           = *fid;
+	de->de_magic         = MAGIC_DIRENT;
+
+	dir_ents_tlink_init(de);
+
+	C2_TRACE_END(0);
+}
+
+void c2t1fs_dir_ent_fini(struct c2t1fs_dir_ent *de)
+{
+	C2_TRACE_START();
+
+	dir_ents_tlink_fini(de);
+	de->de_magic = 0;
+
+	C2_TRACE_END(0);
+}
+
+static int c2t1fs_dir_ent_add(struct inode        *dir,
+			      const unsigned char *name,
+			      int                  namelen,
+			      const struct c2_fid *fid)
+{
+	struct c2t1fs_inode   *ci;
+	struct c2t1fs_dir_ent *de;
+	int                    rc;
+
+	C2_TRACE("name=\"%s\" namelen=%d\n", name, namelen);
+
+	C2_ASSERT(c2t1fs_inode_is_root(dir));
+
+	if (namelen == 0) {
+		rc = -EINVAL;
+		goto out;
+	}
+
+	if (namelen >= C2T1FS_MAX_NAME_LEN) {
+		rc = -ENAMETOOLONG;
+		goto out;
+	}
+
+	ci = C2T1FS_I(dir);
+
+	C2_ALLOC_PTR(de);
+	if (de == NULL) {
+		rc = -ENOMEM;
+		goto out;
+	}
+
+	c2t1fs_dir_ent_init(de, name, namelen, fid);
+	dir_ents_tlist_add_tail(&ci->ci_dir_ents, de);
+
+	C2_TRACE("Added name: %s[%lu:%lu]\n", de->de_name,
+					      (unsigned long)fid->f_container,
+					      (unsigned long)fid->f_key);
+
+	mark_inode_dirty(dir);
+	rc = 0;
+out:
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+static bool name_eq(const unsigned char *name, const char *buf, int len)
+{
+	bool rc;
+
+	C2_TRACE_START();
+
+	if (len <= C2T1FS_MAX_NAME_LEN && buf[len] != '\0') {
+		rc = false;
+	} else {
+		C2_TRACE("buf: \"%s\" name: \"%s\" len: %d\n", buf, name, len);
+		rc = (memcmp(name, buf, len) == 0);
+	}
+
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+static struct c2t1fs_dir_ent *c2t1fs_dir_ent_find(struct inode        *dir,
+						  const unsigned char *name,
+						  int                  namelen)
+{
+	struct c2t1fs_inode   *ci;
+	struct c2t1fs_sb      *csb;
+	struct c2t1fs_dir_ent *de = NULL;
+
+	C2_TRACE_START();
+
+	C2_ASSERT(name != NULL && dir != NULL);
+
+	C2_TRACE("Name: \"%s\"\n", name);
+
+	ci  = C2T1FS_I(dir);
+	csb = C2T1FS_SB(dir->i_sb);
+
+	C2_ASSERT(c2t1fs_fs_is_locked(csb));
+
+	c2_tlist_for(&dir_ents_tl, &ci->ci_dir_ents, de) {
+
+		if (name_eq(name, de->de_name, namelen)) {
+			C2_TRACE_END(de);
+			return de;
+		}
+
+	} c2_tlist_endfor;
+
+	C2_TRACE_END(NULL);
+	return NULL;
+}
+
+static struct dentry *c2t1fs_lookup(struct inode     *dir,
+				    struct dentry    *dentry,
+				    struct nameidata *nd)
+{
+	struct c2t1fs_sb      *csb;
+	struct c2t1fs_dir_ent *de;
+	struct inode          *inode = NULL;
+
+	C2_TRACE_START();
+
+	if (dentry->d_name.len > C2T1FS_MAX_NAME_LEN) {
+		C2_TRACE_END(-ENAMETOOLONG);
+		return ERR_PTR(-ENAMETOOLONG);
+	}
+
+	C2_TRACE("Name: \"%s\"\n", dentry->d_name.name);
+
+	csb = C2T1FS_SB(dir->i_sb);
+
+	c2t1fs_fs_lock(csb);
+
+	de = c2t1fs_dir_ent_find(dir, dentry->d_name.name, dentry->d_name.len);
+	if (de != NULL) {
+		inode = c2t1fs_iget(dir->i_sb, &de->de_fid);
+		if (IS_ERR(inode)) {
+			c2t1fs_fs_unlock(csb);
+			C2_TRACE_END(ERR_CAST(inode));
+			return ERR_CAST(inode);
+		}
+	}
+
+	c2t1fs_fs_unlock(csb);
+	d_add(dentry, inode);
+	C2_TRACE_END(NULL);
+	return NULL;
+}
+
+static int c2t1fs_readdir(struct file *f,
+			  void        *buf,
+			  filldir_t    filldir)
+{
+	struct c2t1fs_dir_ent *de;
+	struct c2t1fs_inode   *ci;
+	struct c2t1fs_sb      *csb;
+	struct dentry         *dentry;
+	struct inode          *dir;
+	ino_t                  ino;
+	int                    i;
+	int                    skip;
+	int                    rc;
+
+	C2_TRACE_START();
+
+	dentry = f->f_path.dentry;
+	dir    = dentry->d_inode;
+	ci     = C2T1FS_I(dir);
+	csb    = C2T1FS_SB(dir->i_sb);
+	i      = f->f_pos;
+
+	c2t1fs_fs_lock(csb);
+
+	switch (i) {
+	case 0:
+		ino = dir->i_ino;
+		C2_TRACE("i = %d ino = %lu\n", i, (unsigned long)ino);
+		if (filldir(buf, ".", 1, i, ino, DT_DIR) < 0)
+			break;
+		C2_TRACE("filled: \".\"\n");
+		f->f_pos++;
+		i++;
+		/* Fallthrough */
+	case 1:
+		ino = parent_ino(dentry);
+		C2_TRACE("i = %d ino = %lu\n", i, (unsigned long)ino);
+		if (filldir(buf, "..", 2, i, 4, DT_DIR) < 0)
+			break;
+		C2_TRACE("filled: \"..\"\n");
+		f->f_pos++;
+		i++;
+		/* Fallthrough */
+	default:
+		/* previous call to readdir() returned f->f_pos number of
+		   entries Now we should continue after that point */
+		skip = i - 2;
+		c2_tlist_for(&dir_ents_tl, &ci->ci_dir_ents, de) {
+			char *name;
+			int   namelen;
+
+			if (skip != 0) {
+				skip--;
+				continue;
+			}
+
+			name    = de->de_name;
+			namelen = strlen(name);
+
+			C2_TRACE("off %lu ino %lu\n", (unsigned long)f->f_pos,
+					(unsigned long)i + 1);
+
+			rc = filldir(buf, name, namelen, f->f_pos,
+					++i, DT_REG);
+			if (rc < 0)
+				goto out;
+			C2_TRACE("filled: \"%s\"\n", name);
+
+			f->f_pos++;
+		} c2_tlist_endfor;
+	}
+out:
+	c2t1fs_fs_unlock(csb);
+	C2_TRACE_END(0);
+	return 0;
+}
+
+static int c2t1fs_dir_ent_remove(struct inode *dir, struct c2t1fs_dir_ent *de)
+{
+	C2_TRACE_START();
+
+	C2_TRACE("Name: \"%s\"\n", de->de_name);
+	dir_ents_tlist_del(de);
+	c2t1fs_dir_ent_fini(de);
+	c2_free(de);
+
+	C2_TRACE_END(0);
+	return 0;
+}
+
+static int c2t1fs_unlink(struct inode *dir, struct dentry *dentry)
+{
+	struct c2t1fs_sb      *csb;
+	struct c2t1fs_dir_ent *de;
+	struct inode          *inode;
+	int                    rc;
+
+	/* XXX c2t1fs_unlink() should remove component objects of a file */
+
+	C2_TRACE_START();
+
+	C2_TRACE("Name: \"%s\"\n", dentry->d_name.name);
+
+	inode = dentry->d_inode;
+	csb   = C2T1FS_SB(inode->i_sb);
+
+	c2t1fs_fs_lock(csb);
+
+	de = c2t1fs_dir_ent_find(dir, dentry->d_name.name, dentry->d_name.len);
+	if (de == NULL) {
+		rc = -ENOENT;
+		goto out;
+	}
+
+	rc = c2t1fs_dir_ent_remove(dir, de);
+	if (rc != 0)
+		goto out;
+
+	dir->i_ctime = dir->i_mtime = CURRENT_TIME_SEC;
+	mark_inode_dirty(dir);
+	inode->i_ctime = dir->i_ctime;
+	inode_dec_link_count(inode);
+
+out:
+	c2t1fs_fs_unlock(csb);
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+/**
+   See "Containers and component objects" section in c2t1fs.h for
+   more information.
+ */
+struct c2_fid c2t1fs_cob_fid(const struct c2_fid *gob_fid, int index)
+{
+	struct c2_fid fid;
+
+	C2_TRACE_START();
+
+	/* index 0 is currently reserved for gob_fid.f_container */
+	C2_ASSERT(gob_fid->f_container == 0);
+	C2_ASSERT(index > 0);
+
+	fid.f_container = index;
+	fid.f_key       = gob_fid->f_key;
+
+	C2_TRACE("Out: [%lu:%lu]\n", (unsigned long)fid.f_container,
+				     (unsigned long)fid.f_key);
+	C2_TRACE_END(0);
+	return fid;
+}
+
+static int c2t1fs_create_component_objects(struct c2t1fs_inode *ci)
+{
+	struct c2t1fs_sb *csb;
+	struct c2_fid     gob_fid;
+	struct c2_fid     cob_fid;
+	int               pool_width;
+	int               i;
+	int rc;
+
+	C2_TRACE_START();
+
+	gob_fid = ci->ci_fid;
+
+	C2_TRACE("Create component objects for [%lu:%lu]\n",
+				(unsigned long)gob_fid.f_container,
+				(unsigned long)gob_fid.f_key);
+
+	csb = C2T1FS_SB(ci->ci_inode.i_sb);
+	pool_width = csb->csb_pool_width;
+	C2_ASSERT(pool_width >= 1);
+
+	for (i = 1; i <= pool_width; i++) { /* i = 1 is intentional */
+
+		cob_fid = c2t1fs_cob_fid(&gob_fid, i);
+		rc      = c2t1fs_cob_create(csb, &cob_fid);
+		if (rc != 0) {
+			C2_TRACE("Failed: create [%lu:%lu]\n",
+				(unsigned long)cob_fid.f_container,
+				(unsigned long)cob_fid.f_key);
+			goto out;
+		}
+
+	}
+out:
+	C2_TRACE_END(rc);
+	return rc;
+}
+
+static int c2t1fs_cob_create(struct c2t1fs_sb    *csb,
+			     const struct c2_fid *cob_fid)
+{
+	struct c2_rpc_session *session;
+
+	C2_TRACE_START();
+
+	session = c2t1fs_container_id_to_session(csb, cob_fid->f_container);
+	C2_ASSERT(session != NULL);
+
+	C2_TRACE("Send cob_create [%lu:%lu] to session %lu\n",
+				(unsigned long)cob_fid->f_container,
+				(unsigned long)cob_fid->f_key,
+				(unsigned long)session->s_session_id);
+	C2_TRACE_END(0);
+	return 0;
+}
