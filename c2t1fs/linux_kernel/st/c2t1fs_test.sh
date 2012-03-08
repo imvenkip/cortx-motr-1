4ebf770e805e925b8abb175a5fc035fbe91da648
Rajanikant Chirmade <rajanikant_chirmade@xyratex.com>
2012-03-08 19:57:20 +0530

    Removed sleep from c2t1fs st

--- c2t1fs/linux_kernel/st/c2t1fs_test.sh
+++ c2t1fs/linux_kernel/st/c2t1fs_test.sh
@@ -99,10 +99,25 @@ colibri_service()
 }
 
 
-loadmodule()
+
+
+
+bulkio_test()
 {
+        strip_size=$1
+        io_size=$2
+        io_counts=$3
+        pool_width=$4
+        data_units=$5
+        parity_units=$6
+
         colibri_module=kcolibri
+        c2t1fs_mount_dir=$COLIBRI_C2T1FS_MOUNT_DIR
         colibri_module_path=$COLIBRI_CORE_ROOT/build_kernel_modules
+        io_service=$COLIBRI_IOSERVICE_ENDPOINT
+        local_input=$CILIBRI_C2T1FS_TEST_DIR/file1.data
+        local_output=$COLIBRI_C2T1FS_TEST_DIR/file2.data
+        c2t1fs_file=$c2t1fs_mount_dir/file.data
 
         lsmod | grep $colibri_module &> /dev/null
         if [ $? -eq "0" ]
@@ -123,83 +138,33 @@ loadmodule()
                 echo "Failed to insert module $colibri_module_path/$colibri_module.ko"
                 return 1
         fi
-}
-
-unloadmodule()
-{
-        colibri_module=kcolibri
-        colibri_module_path=$COLIBRI_CORE_ROOT/build_kernel_modules
-
-        rmmod $colibri_module
-        if [ $? -ne "0" ]
-        then
-                return 1
-        fi
-
-        return 1
-}
-
-mount_c2t1fs()
-{
-        strip_size=$1
-        pool_width=$2
-        data_units=$3
-        parity_units=$4
-
-        c2t1fs_mount_dir=$COLIBRI_C2T1FS_MOUNT_DIR
-        io_service=$COLIBRI_IOSERVICE_ENDPOINT
 
         #Create mount directory
         mkdir $c2t1fs_mount_dir
         if [ $? -ne "0" ]
         then
                 echo "Failed to create mount directory."
+                rmmod $colibri_module.ko
                 return 1
         fi
 
+        echo "Mounting file system..."
         mount -t c2t1fs -o ios=$io_service,unit_size=$strip_size,pool_width=$pool_width,nr_data_units=$data_units,nr_parity_units=$parity_units none $c2t1fs_mount_dir
         if [ $? -ne "0" ]
         then
                 echo "Failed to  mount c2t1fs file system."
+                rmmod $colibri_module.ko
                 return 1
         fi
-
-        return 0
-}
-
-umount_c2t1fs()
-{
-        c2t1fs_mount_dir=$COLIBRI_C2T1FS_MOUNT_DIR
-
-        umount $c2t1fs_mount_dir
-        if [ $? -ne "0" ]
-        then
-                echo "Failed to  unmount c2t1fs file system."
-                return 1
-        fi
-
-        rm -f $c2t1fs_mount_dir
-
-        return 0
-}
-
-
-bulkio_test()
-{
-        io_size=$1
-        io_counts=$2
-
-        c2t1fs_mount_dir=$COLIBRI_C2T1FS_MOUNT_DIR
-        io_service=$COLIBRI_IOSERVICE_ENDPOINT
-        local_input=$CILIBRI_C2T1FS_TEST_DIR/file1.data
-        local_output=$COLIBRI_C2T1FS_TEST_DIR/file2.data
-        c2t1fs_file=$c2t1fs_mount_dir/file.data
+        echo "Successfully mounted c2t1fs file system."
 
         echo "Creating local input file of I/O size ..."
         dd if=/dev/urandom of=$local_input bs=$io_size count=$io_counts
         if [ $? -ne "0" ]
         then
                 echo "Failed to create local input file."
+                umount $c2t1fs_mount_dir
+                rmmod $colibri_module.ko
                 return 1
         fi
         echo "Created local input file of I/O size."
@@ -209,6 +174,8 @@ bulkio_test()
         if [ $? -ne "0" ]
         then
                 echo "Failed to write data on c2t1fs file."
+                umount $c2t1fs_mount_dir
+                rmmod $colibri_module.ko
                 return 1
         fi
         echo "Successfully written data of specified size and count to c2t1fs file."
@@ -218,6 +185,8 @@ bulkio_test()
         if [ $? -ne "0" ]
         then
                 echo "Failed to read data from c2t1fs file."
+                umount $c2t1fs_mount_dir
+                rmmod $colibri_module.ko
                 return 1
         fi
         echo "Successfully read data of specified size and count from c2t1fs file."
@@ -227,9 +196,20 @@ bulkio_test()
         if [ $? -ne "0" ]
         then
                 echo "Failed, data written and data read from c2t1fs file are not same."
+                umount $c2t1fs_mount_dir
+                rmmod $colibri_module.ko
                 return 1
         fi
         echo "Successfully test $io_counts I/O of size $io_size ."
+
+        echo "Unmounting file system ..."
+        umount $c2t1fs_mount_dir &>/dev/null
+
+        echo "Cleaning up test directory..."
+        rm -rf $c2t1fs_mount_dir &>/dev/null
+
+        echo "Removing colibro module..."
+        rmmod kcolibri.ko &>/dev/null
 
         return 0
 }
@@ -242,8 +222,6 @@ io_combinations()
         data_units=$2
         parity_units=$3
 
-        c2t1fs_mount_dir=$COLIBRI_C2T1FS_MOUNT_DIR
-
         p=`expr $data_units + 2 '*' $parity_units`
         if [ $p -gt $pool_width ]
         then
@@ -255,38 +233,27 @@ io_combinations()
         # I/O sizes are multiple of stripe size
 
         # stripe size is in K
-        for strip_size_multiplyer in 4 8 # 12 16 20 24 #28 32
+        for strip_size_multiplyer in 4 8  12 16 20 24 #28 32
         do
-            strip_size=`expr $strip_size_multiplyer '*' 1024`
-
-            echo "Mounting file system ..."
-            mount_c2t1fs $strip_size $pool_width $data_units $parity_units
-            if [ $? -ne "0" ] 
-            then
-                    echo "Failed : failed to mount c2t1fs."
-                    return 1
-            fi
-
             # Small I/Os KBs
             for ((io_size_multiplyer=1; io_size_multiplyer<=8; io_size_multiplyer++))
             do
+                strip_size=`expr $strip_size_multiplyer '*' 1024`
                 io_size=`expr $io_size_multiplyer '*' $strip_size`
                 # I/O size in K
                 io_size=`expr $io_size / 1024`K
                 echo "Test : I/O for stripe_size = $strip_size, io_size = $io_size, Number of I/Os = 1."
-                bulkio_test $io_size 1 &>> $COLIBRI_TEST_LOGFILE
+                bulkio_test $strip_size $io_size 1 $pool_width $data_units $parity_units &>> $COLIBRI_TEST_LOGFILE
                 if [ $? -ne "0" ]
                 then
-                        umount_c2t1fs
                         return 1
                 fi
 
                 # Multiple I/Os
                 echo "Test : I/O for stripe_size = $strip_size, io_size = $io_size, Number of I/Os = 2."
-                bulkio_test $io_size 2 &>> $COLIBRI_TEST_LOGFILE
+                bulkio_test $strip_size $io_size 2 $pool_width $data_units $parity_units &>> $COLIBRI_TEST_LOGFILE
                 if [ $? -ne "0" ]
                 then
-                        umount_c2t1fs
                         return 1
                 fi
             done
@@ -297,35 +264,28 @@ io_combinations()
             for ((io_size_multiplyer=1; io_size_multiplyer<=8; io_size_multiplyer++))
             do
                 #Making I/O size in MBs by multiplying with size_multiplyer
+                strip_size=`expr $strip_size_multiplyer '*' 1024`
                 io_size=`expr $io_size_multiplyer '*' $strip_size '*' $size_multiplyer`
                 # I/O size in M
                 io_size=`expr $io_size / $one_mb`M
                 echo "Test : I/O for stripe_size = $strip_size, io_size = $io_size, Number of I/Os = 1."
-                bulkio_test $io_size 1 &>> $COLIBRI_TEST_LOGFILE
+                bulkio_test $strip_size $io_size 1 $pool_width $data_units $parity_units &>> $COLIBRI_TEST_LOGFILE
                 if [ $? -ne "0" ]
                 then
-                        umount_c2t1fs
                         return 1
                 fi
+
 
                 # Multiple I/Os
                 echo "Test : I/O for stripe_size = $strip_size, io_size = $io_size, Number of I/Os = 2."
-                bulkio_test $io_size 2 &>> $COLIBRI_TEST_LOGFILE
+                bulkio_test $strip_size $io_size 2 $pool_width $data_units $parity_units &>> $COLIBRI_TEST_LOGFILE
                 if [ $? -ne "0" ]
                 then
-                        umount_c2t1fs
                         return 1
                 fi
 
-            done
 
-            echo "Unmounting file system ..."
-            umount_c2t1fs
-            if [ $? -ne "0" ] 
-            then
-                    echo "Failed : failed to umount c2t1fs."
-                    return 1
-            fi
+            done
 
         done
         echo "Test log available at $COLIBRI_TEST_LOGFILE."
@@ -349,22 +309,10 @@ main()
 
         sleep 5 #Give time to start service properly.
 
-        loadmodule
-        if [ $? -ne "0" ]
-        then
-                echo "Failed : Module load failed."
-        fi
-
         io_combinations $POOL_WIDTH 1 1
         if [ $? -ne "0" ]
         then
                 echo "Failed : IO failed.."
-        fi
-
-        unloadmodule
-        if [ $? -ne "0" ]
-        then
-                echo "Failed : Module unload failed."
         fi
 
         colibri_service stop
