2e923120c9225256d69fe93106888f4b43c590f4
Andriy Tkachuk <andriy_tkachuk@xyratex.com>
2012-03-16 17:06:11 +0200

    balloc: cleanup on error fix from Rajani
    In c2_balloc_load_extents() if c2_db_cursor_get() failed
    c2_db_cursor_fini() was not called.
    (cherry picked from commit e53e827c87c6a9f3629c8b6aeb00516d0215e4fd)

--- balloc/balloc.c
+++ balloc/balloc.c
@@ -901,6 +901,7 @@ int c2_balloc_load_extents(struct c2_balloc *cb,
 			 &start.e_end, sizeof start.e_end);
 	result = c2_db_cursor_get(&cursor, &pair);
 	if ( result != 0) {
+		c2_db_cursor_fini(&cursor);
 		c2_balloc_release_extents(grp);
 		return result;
 	}
