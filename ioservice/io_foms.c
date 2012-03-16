09f46e2633f5ecbed1f6922538ef7823964a4448
anand.vidwansa <anand_vidwansa@xyratex.com>
2012-03-16 16:26:57 +0530

    Fixed defect #810 in ioservice create cob fom code.

--- ioservice/io_foms.c
+++ ioservice/io_foms.c
@@ -2114,12 +2114,7 @@ static int cc_cob_create(struct c2_fom *fom, struct c2_fom_cob_create *cc)
 	 * The flag CA_NSKEY_FREE takes care of deallocating memory for
 	 * nskey during cob finalization.
 	 */
-	if (rc == -ENOMEM) {
-		c2_free(nskey->cnk_name.b_data);
-		C2_ADDB_ADD(&fom->fo_fop->f_addb, &cc_fom_addb_loc,
-			    cc_fom_func_fail,
-			    "Memory allocation failed in cc_cob_create().", rc);
-	} else {
+	if (rc == 0) {
 		C2_ADDB_ADD(&fom->fo_fop->f_addb, &cc_fom_addb_loc,
 			    c2_addb_trace, "Cob created successfully.");
 		/**
@@ -2129,6 +2124,11 @@ static int cc_cob_create(struct c2_fom *fom, struct c2_fom_cob_create *cc)
 		 * otherwise would cause a memory leak.
 		 */
 		c2_cob_put(cob);
+	} else if (rc == -ENOMEM) {
+		c2_free(nskey->cnk_name.b_data);
+		C2_ADDB_ADD(&fom->fo_fop->f_addb, &cc_fom_addb_loc,
+			    cc_fom_func_fail,
+			    "Memory allocation failed in cc_cob_create().", rc);
 	}
 
 	return rc;
