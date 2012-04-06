83d5d35975653467b102379d601f6fbfe84152e3
Andriy Tkachuk <andriy_tkachuk@xyratex.com>
2012-04-06 12:05:26 +0300

    c1t1fs/st: common.sh script for server and client introduced
    This script contains shared configuration.

--- c2t1fs/linux_kernel/st/c2t1fs_client.sh
+++ c2t1fs/linux_kernel/st/c2t1fs_client.sh
@@ -8,16 +8,7 @@ fi
 
 echo 8 > /proc/sys/kernel/printk
 
-COLIBRI_CORE_ROOT=`dirname $0`/../../..
-COLIBRI_C2T1FS_MOUNT_DIR=/tmp/test_c2t1fs_`date +"%d-%m-%Y_%T"`
-COLIBRI_C2T1FS_TEST_DIR=/tmp/test_c2t1fs_$$
-COLIBRI_IOSERVICE_ENDPOINT="172.16.220.161:23124:1"
-COLIBRI_C2T1FS_ENDPOINT="172.16.220.159:23124:1"
-COLIBRI_MODULE=$COLIBRI_CORE_ROOT/build_kernel_modules/kcolibri.ko
-COLIBRI_GALOIS_MODULE=$COLIBRI_CORE_ROOT/../galois/src/linux_kernel/kgalois.ko
-COLIBRI_TEST_LOGFILE=`pwd`/bulkio_`date +"%d-%m-%Y_%T"`.log
-POOL_WIDTH=3
-
+. `dirname $0`/c2t1fs_common.sh
 
 bulkio_test()
 {
