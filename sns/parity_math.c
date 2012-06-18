6f0e763376f2614fbb320cdccca2df1416ce33db
Anup Barve <anup_barve@xyratex.com>
2012-06-18 15:58:10 +0530

    Added routine to xor 2 buffers.

--- sns/parity_math.c
+++ sns/parity_math.c
@@ -68,7 +68,7 @@ static int gpow(int x, int p)
 	return ret;
 }
 
-/* fills vandermonde matrix with initial values */
+/* Fills vandermonde matrix with initial values. */
 static int vandmat_init(struct c2_matrix *m, uint32_t data_count,
 			uint32_t parity_count)
 {
@@ -95,7 +95,7 @@ static void vandmat_fini(struct c2_matrix *mat)
 	c2_matrix_fini(mat);
 }
 
-/* checks if row has only one element equals 1, and 0 others */
+/* Checks if row has only one element equals 1, and 0 others */
 static bool check_row_is_id(struct c2_matrix *m, uint32_t row)
 {
 	bool ret = true;
@@ -108,7 +108,8 @@ static bool check_row_is_id(struct c2_matrix *m, uint32_t row)
 	return ret;
 }
 
-/* normalize vandermonde matrix. upper part of which become identity matrix in case of success. */
+/* Normalizes vandermonde matrix, upper part of which becomes identity matrix
+ * in case of success. */
 static int vandmat_norm(struct c2_matrix *m)
 {
 	uint32_t y;
@@ -122,7 +123,7 @@ static int vandmat_norm(struct c2_matrix *m)
 				c2_matrix_cols_operate(m, x, y, gsub, 0, gmul,
                                             *c2_matrix_elem_get(m, x, y), gsub);
 
-		/* Assert if units configured unproperly */
+		/* Assert if units are not configured properly. */
 		C2_ASSERT(check_row_is_id(m, y));
 	}
 
@@ -168,7 +169,7 @@ int  c2_parity_math_init(struct c2_parity_math *math,
 
 		math->pmi_parity_algo = C2_PARITY_CAL_ALGO_REED_SOLOMON;
 		/* init galois, only first call makes initialization,
-		 * no deinitialization needed */
+		 * no deinitialization needed. */
 		c2_parity_init();
 
 		ret = vandmat_init(&math->pmi_vandmat, data_count,
@@ -220,10 +221,10 @@ static void parity_math_xor_calculate(struct c2_parity_math *math,
 				      struct c2_buf *data,
 				      struct c2_buf *parity)
 {
-        uint32_t              ei; /* block element index */
-        uint32_t              ui; /* unit index */
-        uint32_t block_size = data[0].b_nob;
-	c2_parity_elem_t      pe;
+        uint32_t          ei; /* block element index. */
+        uint32_t          ui; /* unit index. */
+        uint32_t          block_size = data[0].b_nob;
+	c2_parity_elem_t  pe;
 
         for (ui = 0; ui < math->pmi_data_count; ++ui)
                 C2_ASSERT(block_size == data[ui].b_nob);
@@ -246,8 +247,8 @@ static void parity_math_reed_solomon_encode(struct c2_parity_math *math,
 					    struct c2_buf *data,
 					    struct c2_buf *parity)
 {
-	uint32_t              ei; /* block element index */
-	uint32_t              ui; /* unit index */
+	uint32_t ei; /* block element index. */
+	uint32_t ui; /* unit index. */
 	uint32_t block_size = data[0].b_nob;
 
 	for (ui = 0; ui < math->pmi_data_count; ++ui)
@@ -289,7 +290,7 @@ void c2_parity_math_refine(struct c2_parity_math *math,
 	c2_parity_math_calculate(math, data, parity);
 }
 
-/* counts number of blocks failed */
+/* Counts number of fialed blocks. */
 static uint32_t fails_count(uint8_t *fail, uint32_t unit_count)
 {
 	uint32_t x;
@@ -301,11 +302,11 @@ static uint32_t fails_count(uint8_t *fail, uint32_t unit_count)
 	return count;
 }
 
-/* fills 'mat' and 'vec' with data passed to recovery algorithm */
+/* Fills 'mat' and 'vec' with data passed to recovery algorithm. */
 static void recovery_data_fill(struct c2_parity_math *math,
-			       uint8_t *fail, uint32_t unit_count, /* in */
+			       uint8_t *fail, uint32_t unit_count, /* in. */
 			       struct c2_matrix *mat,
-			       struct c2_vector *vec) /* out */
+			       struct c2_vector *vec) /* out. */
 {
 	uint32_t f;
 	uint32_t y = 0;
@@ -313,14 +314,14 @@ static void recovery_data_fill(struct c2_parity_math *math,
 
 	for (f = 0; f < unit_count; ++f) {
 		/* if (block is ok) and
-		 * (not enough equations to solve system) */
+		 * (not enough equations to solve system). */
 		if (!fail[f] && y < vec->v_size) {
-			/* copy vec */
+			/* copy vec. */
 			*c2_vector_elem_get(vec, y) = f < math->pmi_data_count
 				? *c2_vector_elem_get(&math->pmi_data, f)
 				: *c2_vector_elem_get(&math->pmi_parity,
 						f - math->pmi_data_count);
-			/* copy mat */
+			/* copy mat. */
 			for (x = 0; x < mat->m_width; ++x)
 				*c2_matrix_elem_get(mat, x, y) =
 					*c2_matrix_elem_get(&math->pmi_vandmat,
@@ -331,10 +332,9 @@ static void recovery_data_fill(struct c2_parity_math *math,
 	}
 }
 
-/* update internal structures of 'math' with recovered data */
+/* Updates internal structures of 'math' with recovered data. */
 static void parity_math_recover(struct c2_parity_math *math,
 				uint8_t *fail, uint32_t unit_count)
-/* 1 in fail states of blocks */
 {
 	struct c2_matrix *mat = &math->pmi_sys_mat;
 	struct c2_vector *vec = &math->pmi_sys_vec;
@@ -352,8 +352,8 @@ static void xor_recover(struct c2_parity_math *math,
 			struct c2_buf *parity,
 			struct c2_buf *fails)
 {
-	uint32_t          ei; /* block element index */
-	uint32_t          ui; /* unit index */
+	uint32_t          ei; /* block element index. */
+	uint32_t          ui; /* unit index. */
 	uint8_t          *fail;
 	uint32_t          fail_count;
 	uint32_t          unit_count;
@@ -383,16 +383,16 @@ static void xor_recover(struct c2_parity_math *math,
 		    } else
 			fail_index = ui;
                 }
-		/* Now ui points to parity block, test if it was failed */
+		/* Now ui points to parity block, test if it was failed. */
 		if (fail[ui] != 1)
 			pe = pe ^ ((uint8_t*)parity[0].b_addr)[ei];
-		else /* Parity was lost, so recover it */
+		else /* Parity was lost, so recover it. */
 			((uint8_t*)parity[0].b_addr)[ei] = pe;
 
 		((uint8_t*)data[fail_index].b_addr)[ei] = pe;
         }
 
-	/* recalculate parity */
+	/* recalculate parity. */
 	c2_parity_math_calculate(math, data, parity);
 }
 
@@ -401,8 +401,8 @@ static void reed_solomon_recover(struct c2_parity_math *math,
 				 struct c2_buf *parity,
 				 struct c2_buf *fails)
 {
-	uint32_t ei; /* block element index */
-	uint32_t ui; /* unit index */
+	uint32_t ei; /* block element index. */
+	uint32_t ui; /* unit index. */
 	uint8_t *fail;
 	uint32_t fail_count;
 	uint32_t unit_count = math->pmi_data_count + math->pmi_parity_count;
@@ -423,7 +423,7 @@ static void reed_solomon_recover(struct c2_parity_math *math,
 	for (ei = 0; ei < block_size; ++ei) {
 		struct c2_vector *recovered = &math->pmi_sys_res;
 
-		/* load data and parity */
+		/* load data and parity. */
 		for (ui = 0; ui < math->pmi_data_count; ++ui)
 			*c2_vector_elem_get(&math->pmi_data, ui) =
 				((uint8_t*)data  [ui].b_addr)[ei];
@@ -432,15 +432,15 @@ static void reed_solomon_recover(struct c2_parity_math *math,
 			*c2_vector_elem_get(&math->pmi_parity, ui) =
 				((uint8_t*)parity[ui].b_addr)[ei];
 
-		/* recover data */
+		/* recover data. */
 		parity_math_recover(math, fail, unit_count);
-		/* store data */
+		/* store data. */
 		for (ui = 0; ui < math->pmi_data_count; ++ui)
 			((uint8_t*)data[ui].b_addr)[ei] =
 				*c2_vector_elem_get(recovered, ui);
 	}
 
-	/* recalculate parity */
+	/* recalculate parity. */
 	c2_parity_math_calculate(math, data, parity);
 }
 
@@ -458,10 +458,10 @@ void c2_parity_math_recover(struct c2_parity_math *math,
 static void fail_index_xor_recover(struct c2_parity_math *math,
 				   struct c2_buf *data,
 				   struct c2_buf *parity,
-				   uint32_t failure_index)
+				   const uint32_t failure_index)
 {
-        uint32_t          ei; /* block element index */
-        uint32_t          ui; /* unit index */
+        uint32_t          ei; /* block element index. */
+        uint32_t          ui; /* unit index. */
         uint32_t          unit_count;
         uint32_t          block_size = data[0].b_nob;
         c2_parity_elem_t  pe;
@@ -486,7 +486,7 @@ static void fail_index_xor_recover(struct c2_parity_math *math,
                 }
                 if (ui != failure_index)
                         pe = pe ^ ((uint8_t*)parity[0].b_addr)[ei];
-                else /* Parity was lost, so recover it */
+                else /* Parity was lost, so recover it. */
                         ((uint8_t*)parity[0].b_addr)[ei] = pe;
 
                 ((uint8_t*)data[failure_index].b_addr)[ei] = pe;
@@ -494,24 +494,35 @@ static void fail_index_xor_recover(struct c2_parity_math *math,
 
 }
 
-/** @todo Iterative reed-solomon decode to be implemented */
+/** @todo Iterative reed-solomon decode to be implemented. */
 static void fail_index_reed_solomon_recover(struct c2_parity_math *math,
 					    struct c2_buf *data,
 					    struct c2_buf *parity,
-					    uint32_t failure_index)
+					    const uint32_t failure_index)
 {
 }
 
-void c2_parity_math_single_block_recover(struct c2_parity_math *math,
-                                         struct c2_buf *data,
-                                         struct c2_buf *parity,
-                                         uint32_t failure_index)
+void c2_parity_math_fail_index_recover(struct c2_parity_math *math,
+                                       struct c2_buf *data,
+                                       struct c2_buf *parity,
+                                       const uint32_t failure_index)
 {
         if (math->pmi_parity_algo == C2_PARITY_CAL_ALGO_XOR)
 		fail_index_xor_recover(math, data, parity, failure_index);
         else
                 fail_index_reed_solomon_recover(math, data, parity,
 						failure_index);
+}
+
+void c2_parity_math_buffer_xor(struct c2_buf *src, struct c2_buf *dest)
+{
+        uint32_t  ei; /* block element index. */
+        uint32_t  block_size = src[0].b_nob;
+
+        for (ei = 0; ei < block_size; ++ei)
+		((uint8_t*)dest[0].b_addr)[ei] =
+			(c2_parity_elem_t)((uint8_t*)src[0].b_addr)[ei] ^
+			(c2_parity_elem_t)((uint8_t*)dest[0].b_addr)[ei];
 }
 
 /*
--- sns/parity_math.h
+++ sns/parity_math.h
@@ -27,9 +27,12 @@
 /**
    @defgroup parity_math Parity Math Component
 
-   A parity math component is a part of Colibri core and serving several purposes:
-   @li Provide algorithms for calculation of SNS parity units (checksums) for given data units;
-   @li Provide algorithms for quick update of parity units in case of minor data changes;
+   A parity math component is a part of Colibri core and serving
+   several purposes:
+   @li Provide algorithms for calculation of SNS parity units (checksums)
+       for given data units;
+   @li Provide algorithms for quick update of parity units in case of minor
+       data changes;
    @li Provide algorithms for SNS repair (recovery) in case of failure.
    @{
 */
@@ -50,17 +53,16 @@ enum c2_parity_cal_algo {
 struct c2_parity_math {
 	enum c2_parity_cal_algo pmi_parity_algo;
 
-	/* private: */
 	uint32_t                pmi_data_count;
 	uint32_t                pmi_parity_count;
 
-	/* structures used for parity calculation and recovery */
+	/* structures used for parity calculation and recovery. */
 	struct c2_vector        pmi_data;
 	struct c2_vector        pmi_parity;
 	struct c2_matrix        pmi_vandmat;
 	struct c2_matrix        pmi_vandmat_parity_slice;
 
-	/* structures used for recovery */
+	/* structures used for recovery. */
 	struct c2_matrix        pmi_sys_mat;
 	struct c2_vector        pmi_sys_vec;
 	struct c2_vector        pmi_sys_res;
@@ -70,24 +72,25 @@ struct c2_parity_math {
 /**
    Initialization of parity math algorithms.
    Fills '*math' with appropriate values.
-   @param data_count - count of SNS data units used in system
-   @param parity_count - count of SNS parity units used in system
-   @return 0 for success, -C2_SNS_PARITY_MATH_* codes or -ENOMEM for fail
+   @param data_count - count of SNS data units used in system.
+   @param parity_count - count of SNS parity units used in system.
+   @return 0 for success, -C2_SNS_PARITY_MATH_* codes or -ENOMEM for fail.
  */
 int  c2_parity_math_init(struct c2_parity_math *math,
 			 uint32_t data_count, uint32_t parity_count);
 
 /**
    Deinitializaton of parity math algorithms.
-   Frees all memory blocks allocated by c2_parity_math_init()
+   Frees all memory blocks allocated by c2_parity_math_init().
  */
 void c2_parity_math_fini(struct c2_parity_math *math);
 
 /**
    Calculates parity block data.
-   @param data[in] - data block, treated as uint8_t block with b_nob elements
-   @param parity[out] - parity block, treated as uint8_t block with b_nob elements
-   @pre c2_parity_math_init() succeeded
+   @param data[in] - data block, treated as uint8_t block with b_nob elements.
+   @param parity[out] - parity block, treated as uint8_t block with
+                        b_nob elements.
+   @pre c2_parity_math_init() succeeded.
  */
 void c2_parity_math_calculate(struct c2_parity_math *math,
 			      struct c2_buf *data,
@@ -95,10 +98,11 @@ void c2_parity_math_calculate(struct c2_parity_math *math,
 
 /**
    Parity block refinement iff one data word of one data unit had changed.
-   @param data[in] - data block, treated as uint8_t block with b_nob elements
-   @param parity[out] - parity block, treated as uint8_t block with b_nob elements
+   @param data[in] - data block, treated as uint8_t block with b_nob elements.
+   @param parity[out] - parity block, treated as uint8_t block with
+                        b_nob elements.
    @param data_ind_changed[in] - index of data unit recently changed.
-   @pre c2_parity_math_init() succeeded
+   @pre c2_parity_math_init() succeeded.
  */
 void c2_parity_math_refine(struct c2_parity_math *math,
 			   struct c2_buf *data,
@@ -107,11 +111,14 @@ void c2_parity_math_refine(struct c2_parity_math *math,
 
 /**
    Recovers data or parity units' data words from single or multiple errors.
-   @param data[inout] - data block, treated as uint8_t block with b_nob elements
-   @param parity[inout] - parity block, treated as uint8_t block with b_nob elements
-   @param fail[in] - block with flags, treated as uint8_t block with b_nob elements,
-   if element is '1' then data or parity block with given index is treated as broken
-   @pre c2_parity_math_init() succeded
+   @param data[inout] - data block, treated as uint8_t block with
+			b_nob elements.
+   @param parity[inout] - parity block, treated as uint8_t block with
+                          b_nob elements.
+   @param fail[in] - block with flags, treated as uint8_t block with
+                     b_nob elements, if element is '1' then data or parity
+                     block with given index is treated as broken.
+   @pre c2_parity_math_init() succeded.
  */
 void c2_parity_math_recover(struct c2_parity_math *math,
 			    struct c2_buf *data,
@@ -120,16 +127,27 @@ void c2_parity_math_recover(struct c2_parity_math *math,
 
 /**
  * Recovers data or parity units partially or fully depending on the parity
- * calculation algorithm.
+ * calculation algorithm, given the failure index.
  * @param math - math context.
- * @param data - data block, treaded as uint8_t block with b_nob elements.
- * @param parity - parity block, treaded as uint8_t block with b_nob elements.
+ * @param data - data block, treated as uint8_t block with b_nob elements.
+ * @param parity - parity block, treated as uint8_t block with b_nob elements.
  * @param failure_index - Index of the failed block.
+   @pre c2_parity_math_init() succeded.
  */
-void c2_parity_math_single_block_recover(struct c2_parity_math *math,
-		                         struct c2_buf *data,
-					 struct c2_buf *parity,
-					 uint32_t failure_index);
+void c2_parity_math_fail_index_recover(struct c2_parity_math *math,
+		                       struct c2_buf *data,
+				       struct c2_buf *parity,
+				       const uint32_t failure_index);
+
+
+/**
+ * XORs the source and destination buffers and stores the output in destination
+ * buffer.
+ * @param src - source buffer, treated as uint8_t block with b_nob elements.
+ * @param dest - destination buffer, treated as uint8_t block with
+ *               b_nob elements, containing the output of src XOR dest.
+ */
+void c2_parity_math_buffer_xor(struct c2_buf *src, struct c2_buf *dest);
 
 /** @} end group parity_math */
 
