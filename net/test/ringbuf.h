e23009d9dd0f2473d8e2ea71a137509158a5c03b
Maxim Medved <Max_Medved@xyratex.com>
2012-07-11 21:26:34 +0300

    LogD 385: documented c2_net_test_ringbuf

--- net/test/ringbuf.h
+++ net/test/ringbuf.h
@@ -24,19 +24,42 @@
 #include "lib/types.h"	/* size_t */
 #include "lib/atomic.h"	/* c2_atomic64 */
 
-/** Circular buffer */
+/**
+   Circular FIFO buffer with size_t elements.
+   @note c2_net_test_ringbuf.ntr_start and c2_net_test_ringbuf.ntr_end are
+   absolute indices.
+ */
 struct c2_net_test_ringbuf {
-	size_t		    ntr_size;
-	size_t		   *ntr_buf;
-	struct c2_atomic64  ntr_start;
-	struct c2_atomic64  ntr_end;
+	size_t		    ntr_size;	/**< Number of elements in ringbuf */
+	size_t		   *ntr_buf;	/**< Ringbuf array */
+	struct c2_atomic64  ntr_start;	/**< Start pointer */
+	struct c2_atomic64  ntr_end;	/**< End pointer */
 };
 
+/**
+   Initialize ring buffer.
+   @param rb ring buffer
+   @param size maximum number of elements.
+ */
 int c2_net_test_ringbuf_init(struct c2_net_test_ringbuf *rb, size_t size);
+/**
+   Finalize ring buffer.
+   @pre c2_net_test_ringbuf_invariant(rb)
+ */
 void c2_net_test_ringbuf_fini(struct c2_net_test_ringbuf *rb);
 bool c2_net_test_ringbuf_invariant(const struct c2_net_test_ringbuf *rb);
 
+/**
+   Put item to the ring buffer.
+   @pre c2_net_test_ringbuf_invariant(rb)
+   @post c2_net_test_ringbuf_invariant(rb)
+ */
 void c2_net_test_ringbuf_put(struct c2_net_test_ringbuf *rb, size_t value);
+/**
+   Get item from the ring buffer.
+   @pre c2_net_test_ringbuf_invariant(rb)
+   @post c2_net_test_ringbuf_invariant(rb)
+ */
 size_t c2_net_test_ringbuf_get(struct c2_net_test_ringbuf *rb);
 
 #endif /*  __NET_TEST_RINGBUF_H__ */
