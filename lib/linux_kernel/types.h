0af2b5c98b2a51c8943c1eaff7f58b328fc0a84e
Maxim Medved <Max_Medved@xyratex.com>
2012-07-17 22:16:15 +0300

    LogD 468: removed unused comments from lib/linux_kernel/types.h

--- lib/linux_kernel/types.h
+++ lib/linux_kernel/types.h
@@ -40,21 +40,6 @@
 #define INT64_MIN  C2_TYPES_INT_MIN(64)	 /* 0x8000000000000000 */
 #define INT64_MAX  C2_TYPES_INT_MAX(64)	 /* 0x7FFFFFFFFFFFFFFF */
 
-#if 0
-#define UINT8_MAX  ((uint8_t)(~((uint8_t) 0)))      /* 0xFF */
-#define INT8_MIN   ((int8_t)(- (INT8_MAX) - 1))     /* 0x80 */
-#define INT8_MAX   ((int8_t)((~(uint8_t)0) >> 1))   /* 0x7F */
-#define UINT16_MAX ((uint16_t)(~((uint16_t) 0)))    /* 0xFFFF */
-#define INT16_MIN  ((int16_t)(- (INT16_MAX) - 1))   /* 0x8000 */
-#define INT16_MAX  ((int16_t)((~(uint16_t)0) >> 1)) /* 0x7FFF */
-#define UINT32_MAX ((uint32_t)(~((uint32_t) 0)))    /* 0xFFFFFFFF */
-#define INT32_MIN  ((int32_t)(- (INT32_MAX) - 1))   /* 0x80000000 */
-#define INT32_MAX  ((int32_t)((~(uint32_t)0) >> 1)) /* 0x7FFFFFFF */
-#define UINT64_MAX ((uint64_t)(~((uint64_t) 0)))    /* 0xFFFFFFFFFFFFFFFF */
-#define INT64_MIN  ((int64_t)(- (INT64_MAX) - 1))   /* 0x8000000000000000 */
-#define INT64_MAX  ((int64_t)((~(uint64_t)0) >> 1)) /* 0x7FFFFFFFFFFFFFFF */
-#endif
-
 /* __COLIBRI_LIB_LINUX_KERNEL_TYPES_H_ */
 #endif
 /*
