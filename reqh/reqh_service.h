bf3f21eb5851cd13f6aca4c4f00abbf4490202fe
Nikita Danilov <nikita_danilov@xyratex.com>
2012-07-23 16:10:31 +0400

    Fix a typo:

--- reqh/reqh_service.h
+++ reqh/reqh_service.h
@@ -323,9 +323,9 @@ struct c2_reqh_service_type_ops {
 };
 
 /**
-   Represents a particular service type.
-   A c2_reqh_service_type instance is initialised and registered into a global
-   list of service types as a part of corresponding module initiasation process.
+   Represents a particular service type.  A c2_reqh_service_type instance is
+   initialised and registered into a global list of service types as a part of
+   corresponding module initialisation process.
 
    @see c2_reqh_service_type_init()
  */
