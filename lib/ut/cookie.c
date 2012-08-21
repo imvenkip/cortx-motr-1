263313d19152bbe1f9b8d87362cb4adcae1524fe
Nachiket Sahasrabudhe <nachiket_sahasrabuddhe@xyratex.com>
2012-08-21 18:12:48 +0530

    Removed extra-spaces from comments in lib/ut/cookie.c

--- lib/ut/cookie.c
+++ lib/ut/cookie.c
@@ -19,8 +19,8 @@
  */
 
 /**
- * This file has seven test-cases in all. Out of these seven cases, 
- * six cases deal with cookie-APIs, and one case tests the function 
+ * This file has seven test-cases in all. Out of these seven cases,
+ * six cases deal with cookie-APIs, and one case tests the function
  * c2_addr_is_sane.
  * The six cases related to cookie-APIs are as below:
  * Test No.0: Test for a valid cookie.
