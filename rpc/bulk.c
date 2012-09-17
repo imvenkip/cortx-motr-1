0885bcaf1cff8bd8ecdcf1c70968a019831a514e
Rohan Puri <rohan_puri@xyratex.com>
2012-09-17 12:28:43 -0700

    Trace logs added for file 'rpc/bulk.c'

--- rpc/bulk.c
+++ rpc/bulk.c
@@ -20,6 +20,8 @@
  * Original creation date: 06/27/2012
  */
 
+#define C2_TRACE_SUBSYSTEM C2_TRACE_SUBSYS_RPC
+#include "lib/trace.h"
 #include "lib/tlist.h"
 #include "lib/memory.h"
 #include "lib/errno.h"
@@ -72,6 +74,7 @@ static bool rpc_bulk_buf_invariant(const struct c2_rpc_bulk_buf *rbuf)
 
 static void rpc_bulk_buf_fini(struct c2_rpc_bulk_buf *rbuf)
 {
+	C2_ENTRY("rpc_bulk_buf: '%p'", rbuf);
 	C2_PRE(rbuf != NULL);
 
 	c2_net_desc_free(&rbuf->bb_nbuf->nb_desc);
@@ -79,6 +82,7 @@ static void rpc_bulk_buf_fini(struct c2_rpc_bulk_buf *rbuf)
 	if (rbuf->bb_flags & C2_RPC_BULK_NETBUF_ALLOCATED)
 		c2_free(rbuf->bb_nbuf);
 	c2_free(rbuf);
+	C2_LEAVE();
 }
 
 static int rpc_bulk_buf_init(struct c2_rpc_bulk_buf *rbuf, uint32_t segs_nr,
@@ -89,18 +93,24 @@ static int rpc_bulk_buf_init(struct c2_rpc_bulk_buf *rbuf, uint32_t segs_nr,
 	struct c2_buf	cbuf;
 	c2_bindex_t	index = 0;
 
+	C2_ENTRY("rpc_bulk_buf: '%p', net_buf: '%p'", rbuf, nb);
 	C2_PRE(rbuf != NULL);
 	C2_PRE(segs_nr > 0);
 
 	rc = c2_0vec_init(&rbuf->bb_zerovec, segs_nr);
-	if (rc != 0)
+	if (rc != 0) {
+		C2_LEAVE("rpc_bulk_buf: Zero vector initialization: FAILED with"
+			 " err: '%d'", rc);
 		return rc;
+	}
 
 	rbuf->bb_flags = 0;
 	if (nb == NULL) {
 		C2_ALLOC_PTR(rbuf->bb_nbuf);
 		if (rbuf->bb_nbuf == NULL) {
 			c2_0vec_fini(&rbuf->bb_zerovec);
+			C2_LEAVE("net_buf memory allocation: FAILED, "
+				 "err: '-ENOMEM'");
 			return -ENOMEM;
 		}
 		rbuf->bb_flags |= C2_RPC_BULK_NETBUF_ALLOCATED;
@@ -118,6 +128,8 @@ static int rpc_bulk_buf_init(struct c2_rpc_bulk_buf *rbuf, uint32_t segs_nr,
 			rc = c2_0vec_cbuf_add(&rbuf->bb_zerovec, &cbuf, &index);
 			if (rc != 0) {
 				c2_0vec_fini(&rbuf->bb_zerovec);
+				C2_LEAVE("Addition of cbuf: FAILED with "
+					 "err: '%d'", rc);
 				return rc;
 			}
 		}
@@ -125,6 +137,7 @@ static int rpc_bulk_buf_init(struct c2_rpc_bulk_buf *rbuf, uint32_t segs_nr,
 
 	rpcbulk_tlink_init(rbuf);
 	rbuf->bb_magic = C2_RPC_BULK_BUF_MAGIC;
+	C2_LEAVE("rc: '%d'", rc);
 	return rc;
 }
 
@@ -135,6 +148,7 @@ static void rpc_bulk_buf_cb(const struct c2_net_buffer_event *evt)
 	struct c2_net_buffer	*nb;
 	bool			 receiver = false;
 
+	C2_ENTRY("net_buf_evt: '%p'", evt);
 	C2_PRE(evt != NULL);
 	C2_PRE(evt->nbe_buffer != NULL);
 
@@ -177,6 +191,7 @@ static void rpc_bulk_buf_cb(const struct c2_net_buffer_event *evt)
 
 	rpc_bulk_buf_fini(buf);
 	c2_mutex_unlock(&rbulk->rb_mutex);
+	C2_LEAVE();
 }
 
 const struct c2_net_buffer_callbacks rpc_bulk_cb  = {
@@ -190,6 +205,7 @@ const struct c2_net_buffer_callbacks rpc_bulk_cb  = {
 
 void c2_rpc_bulk_init(struct c2_rpc_bulk *rbulk)
 {
+	C2_ENTRY("rpc_bulk: '%p'", rbulk);
 	C2_PRE(rbulk != NULL);
 
 	rpcbulk_tlist_init(&rbulk->rb_buflist);
@@ -198,11 +214,13 @@ void c2_rpc_bulk_init(struct c2_rpc_bulk *rbulk)
 	rbulk->rb_magic = C2_RPC_BULK_MAGIC;
 	rbulk->rb_bytes = 0;
 	rbulk->rb_rc = 0;
+	C2_LEAVE();
 }
 C2_EXPORTED(c2_rpc_bulk_init);
 
 void c2_rpc_bulk_fini(struct c2_rpc_bulk *rbulk)
 {
+	C2_ENTRY("rpc_bulk: '%p'", rbulk);
 	C2_PRE(rbulk != NULL);
 	c2_mutex_lock(&rbulk->rb_mutex);
 	C2_PRE(rpc_bulk_invariant(rbulk));
@@ -212,6 +230,7 @@ void c2_rpc_bulk_fini(struct c2_rpc_bulk *rbulk)
 	c2_chan_fini(&rbulk->rb_chan);
 	c2_mutex_fini(&rbulk->rb_mutex);
 	rpcbulk_tlist_fini(&rbulk->rb_buflist);
+	C2_LEAVE();
 }
 C2_EXPORTED(c2_rpc_bulk_fini);
 
@@ -237,20 +256,28 @@ int c2_rpc_bulk_buf_add(struct c2_rpc_bulk *rbulk,
 	int			rc;
 	struct c2_rpc_bulk_buf *buf;
 
+	C2_ENTRY("rpc_bulk: '%p', net_dom: '%p', net_buf: '%p'", rbulk, netdom,
+		 nb);
 	C2_PRE(rbulk != NULL);
 	C2_PRE(netdom != NULL);
 	C2_PRE(out != NULL);
 
-	if (segs_nr > c2_net_domain_get_max_buffer_segments(netdom))
+	if (segs_nr > c2_net_domain_get_max_buffer_segments(netdom)) {
+		C2_LEAVE("Cannot exceed net_max_buf_seg: rc: '-EMSGSIZE'");
 		return -EMSGSIZE;
+	}
 
 	C2_ALLOC_PTR(buf);
-	if (buf == NULL)
+	if (buf == NULL) {
+		C2_LEAVE("rpc_bulk_buf: memory allocation: FAILED:"
+			 " rc: 'ENOMEM'");
 		return -ENOMEM;
+	}
 
 	rc = rpc_bulk_buf_init(buf, segs_nr, nb);
 	if (rc != 0) {
 		c2_free(buf);
+		C2_LEAVE("rc: '%d'", rc);
 		return rc;
 	}
 
@@ -261,6 +288,7 @@ int c2_rpc_bulk_buf_add(struct c2_rpc_bulk *rbulk,
 	c2_mutex_unlock(&rbulk->rb_mutex);
 	*out = buf;
 	C2_POST(rpc_bulk_buf_invariant(buf));
+	C2_LEAVE("rc: '0'");
 	return 0;
 }
 C2_EXPORTED(c2_rpc_bulk_buf_add);
@@ -275,6 +303,7 @@ int c2_rpc_bulk_buf_databuf_add(struct c2_rpc_bulk_buf *rbuf,
 	struct c2_buf		 cbuf;
 	struct c2_rpc_bulk	*rbulk;
 
+	C2_ENTRY("rbuf: '%p', netdom: '%p'", rbuf, netdom);
 	C2_PRE(rbuf != NULL);
 	C2_PRE(rpc_bulk_buf_invariant(rbuf));
 	C2_PRE(buf != NULL);
@@ -283,21 +312,26 @@ int c2_rpc_bulk_buf_databuf_add(struct c2_rpc_bulk_buf *rbuf,
 
 	if (c2_vec_count(&rbuf->bb_zerovec.z_bvec.ov_vec) + count >
 	    c2_net_domain_get_max_buffer_size(netdom) ||
-	    count > c2_net_domain_get_max_buffer_segment_size(netdom))
+	    count > c2_net_domain_get_max_buffer_segment_size(netdom)) {
+		C2_LEAVE("Cannot exceed net_dom_max_buf_segs: rc: -EMSGSIZE");
 		return -EMSGSIZE;
+	}
 
 	cbuf.b_addr = buf;
 	cbuf.b_nob = count;
 	rbulk = rbuf->bb_rbulk;
 	rc = c2_0vec_cbuf_add(&rbuf->bb_zerovec, &cbuf, &index);
-	if (rc != 0)
+	if (rc != 0) {
+		C2_LEAVE("Addition of cbuf: FAILED with err: '%d'", rc);
 		return rc;
+	}
 
 	rbuf->bb_nbuf->nb_buffer = rbuf->bb_zerovec.z_bvec;
 	C2_POST(rpc_bulk_buf_invariant(rbuf));
 	c2_mutex_lock(&rbulk->rb_mutex);
 	C2_POST(rpc_bulk_invariant(rbulk));
 	c2_mutex_unlock(&rbulk->rb_mutex);
+	C2_LEAVE("rc: '%d'", rc);
 	return rc;
 }
 C2_EXPORTED(c2_rpc_bulk_buf_databuf_add);
@@ -306,6 +340,7 @@ void c2_rpc_bulk_qtype(struct c2_rpc_bulk *rbulk, enum c2_net_queue_type q)
 {
 	struct c2_rpc_bulk_buf *rbuf;
 
+	C2_ENTRY("rpc_bulk: '%p', qtype: '%d'", rbulk, q);
 	C2_PRE(rbulk != NULL);
 	C2_PRE(c2_mutex_is_locked(&rbulk->rb_mutex));
 	C2_PRE(!rpcbulk_tlist_is_empty(&rbulk->rb_buflist));
@@ -317,6 +352,7 @@ void c2_rpc_bulk_qtype(struct c2_rpc_bulk *rbulk, enum c2_net_queue_type q)
 	c2_tl_for(rpcbulk, &rbulk->rb_buflist, rbuf) {
 		rbuf->bb_nbuf->nb_qtype = q;
 	} c2_tl_endfor;
+	C2_LEAVE();
 }
 
 static int rpc_bulk_op(struct c2_rpc_bulk *rbulk,
@@ -332,6 +368,8 @@ static int rpc_bulk_op(struct c2_rpc_bulk *rbulk,
 	struct c2_net_domain		*netdom;
 	struct c2_rpc_machine		*rpcmach;
 
+	C2_ENTRY("rpc_bulk: '%p', rpc_conn: '%p', rpc_bulk_op_type: '%d'",
+		 rbulk, conn, op);
 	C2_PRE(rbulk != NULL);
 	C2_PRE(descs != NULL);
 	C2_PRE(op == C2_RPC_BULK_STORE || op == C2_RPC_BULK_LOAD);
@@ -421,6 +459,7 @@ static int rpc_bulk_op(struct c2_rpc_bulk *rbulk,
 	C2_POST(rpc_bulk_invariant(rbulk));
 	c2_mutex_unlock(&rbulk->rb_mutex);
 
+	C2_LEAVE("rc: '%d'", rc);
 	return rc;
 cleanup:
 	C2_ASSERT(rc != 0);
@@ -432,6 +471,7 @@ cleanup:
 			c2_net_buffer_del(rbuf->bb_nbuf, tm);
 	} c2_tl_endfor;
 	c2_mutex_unlock(&rbulk->rb_mutex);
+	C2_LEAVE("rc: '%d'", rc);
 	return rc;
 }
 
