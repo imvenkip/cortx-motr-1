d2083307378053e96ac399ac3a0f77179aa1e45c
Nikita Danilov <nikita_danilov@xyratex.com>
2012-11-21 23:10:31 +0400

    Optionally attach debugger to colibri executable on failure.
    If COLIBRI_DEBUGGER environment variable is set when c2_init() is called,
    c2_panic() would fork a process and execute specified debugger in the created
    child process. The parent spins in an infinite loop, waiting for the debugger to
    attach. The debugger must accept gdb command line parameters: "executable_name
    pid". Note that environment variables do not survive su or sudo, so ./script/c2
    cannot be used as is.
    
    Reviewed-on: http://reviewboard.clusterstor.com/r/1110/
    Reviewed-by: Dmitriy Chumak <dmitriy_chumak@xyratex.com>

--- lib/assert.c
+++ lib/assert.c
@@ -20,6 +20,7 @@
 
 #include <stdio.h>  /* fprintf, fflush */
 #include <stdlib.h> /* abort */
+#include <unistd.h> /* fork, execvp */
 
 #ifdef HAVE_BACKTRACE
 #  include <execinfo.h>
@@ -41,6 +42,8 @@
 enum {
 	BACKTRACE_DEPTH_MAX = 256
 };
+
+C2_EXTERN char *c2_debugger_args[4];
 
 /**
    Simple user space panic function: issue diagnostics to the stderr, flush the
@@ -65,6 +68,22 @@ void c2_panic(const char *expr, const char *func, const char *file, int lineno)
 	}
 #endif
 	C2_LOG(C2_FATAL, "panic: %s %s() (%s:%i)", expr, func, file, lineno);
+	if (c2_debugger_args[0] != NULL) {
+		int rc;
+
+		rc = fork();
+		if (rc > 0) {
+			/* parent */
+			volatile bool stop = true;
+
+			while (stop) {
+				;
+			}
+		} else if (rc == 0) {
+			/* child */
+			rc = execvp(c2_debugger_args[0], c2_debugger_args);
+		}
+	}
 	abort();
 }
 
