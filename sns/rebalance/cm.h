0e48202a5fac42e63275dfc47db4ca06cf586968
Mandar Sawant <mandar_sawant@xyratex.com>
2012-11-29 13:06:31 +0530

    Rebalance copy machine in-progress

---
+++ sns/rebalance/cm.h
@@ -0,0 +1,40 @@
+/* -*- C -*- */
+/*
+ * COPYRIGHT 2012 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Mandar Sawant <mandar_sawant@xyratex.com>
+ * Original creation date: 11/28/2012
+ */
+
+#pragma once
+
+#ifndef __COLIBRI_SNS_REBALANCE_CM_H__
+#define __COLIBRI_SNS_REBALANCE_CM_H__
+
+/**
+  @page SNSRebalanceCMDLD-fspec SNS Rebalance copy machine.
+
+  - @ref SNSRebalanceCMDLD-fspec-ds
+  - @ref SNSRebalanceCMDLD-fspec-if
+  - @ref SNSRebalanceCMDLD-fspec-usecases
+
+  @section SNSRebalanceCMDLD-fspec Functional Specification
+  SNS Repair copy machine
+
+  @subsection SNSRebalanceCMDLD-fspec-ds Data Structures
+
+ */
+
+#endif /* __COLIBRI_SNS_REBALANCE_CM_H__ */
