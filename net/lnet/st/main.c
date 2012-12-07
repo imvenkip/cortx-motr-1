81661f2c7da70bfd9204289669cf4d4552b60f1c
Dmitriy Chumak <dmitriy_chumak@xyratex.com>
2012-12-07 12:27:50 +0200

    net/lnet: rename lnetping => m0lnetping

--- net/lnet/st/main.c
+++ net/lnet/st/main.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 	rc = m0_init();
 	M0_ASSERT(rc == 0);
 
-	rc = M0_GETOPTS("lnetping", argc, argv,
+	rc = M0_GETOPTS("m0lnetping", argc, argv,
 			M0_FLAGARG('s', "run server only", &server_only),
 			M0_FLAGARG('c', "run client only", &client_only),
 			M0_FORMATARG('b', "number of buffers", "%i", &nr_bufs),
