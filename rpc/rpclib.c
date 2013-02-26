imvenkip/cortx-motr-1

commit 065edc3083c8a7f1a1b07a3cde683f6e82c2dca3

rpc/rpclib.c: remove unneeded M0_EXPORTED() lines
+ scripts/m0: fix whitespace

rpc/rpclib.c
@@ -128,7 +128,6 @@ M0_INTERNAL int m0_rpc_client_connect(struct m0_rpc_conn    *conn,
 
 	M0_RETURN(rc);
 }
-M0_EXPORTED(m0_rpc_client_connect);
 
 int m0_rpc_client_start(struct m0_rpc_client_ctx *cctx)
 {
@@ -193,7 +192,6 @@ int m0_rpc_client_call(struct m0_fop *fop,
 		rc = m0_rpc_item_wait_for_reply(item, M0_TIME_NEVER);
 	M0_RETURN(rc);
 }
-M0_EXPORTED(m0_rpc_client_call);
 
 int m0_rpc_client_stop(struct m0_rpc_client_ctx *cctx)
 {

rpc/rpclib.h
@@ -100,7 +100,7 @@ void m0_rpc_server_stop(struct m0_rpc_server_ctx *sctx);
 M0_INTERNAL struct m0_rpc_machine *
 m0_rpc_server_ctx_get_rmachine(struct m0_rpc_server_ctx *sctx);
 
-#endif
+#endif /* !__KERNEL__ */
 
 struct m0_net_xprt;
 struct m0_net_domain;
@@ -243,4 +243,3 @@ int m0_rpc_client_call(struct m0_fop *fop,
 int m0_rpc_client_stop(struct m0_rpc_client_ctx *cctx);
 
 #endif /* __MERO_RPC_RPCLIB_H__ */
-

scripts/m0
@@ -70,7 +70,7 @@ dist_check() {
     tar --no-same-owner --strip-components=1 -xzf $DISTFILE
 
     for lib in extra-libs/*; do
-        ln -s ../../$lib  $DIST/extra-libs/
+	ln -s ../../$lib $DIST/extra-libs/
     done
 
     cd $DIST

commit 2598b512096bedb9a66b204a98a3643deb8ceb5c

rpc/rpclib.c: restore M0_EXPORTED(m0_rpc_client_call)
This change partially reverts commit 065edc3, which caused a warning
to appear while building kernel modules:

WARNING: "m0_rpc_client_call" [/mnt/hgfs/vvv/src/mero/build_kernel_modules/../rpc/it/linux_kernel/m0rpcping.ko] undefined!

Reported-by: Dmitriy Chumak <dmitriy_chumak@xyratex.com>

rpc/rpclib.c
@@ -192,6 +192,7 @@ int m0_rpc_client_call(struct m0_fop *fop,
 		rc = m0_rpc_item_wait_for_reply(item, M0_TIME_NEVER);
 	M0_RETURN(rc);
 }
+M0_EXPORTED(m0_rpc_client_call);
 
 int m0_rpc_client_stop(struct m0_rpc_client_ctx *cctx)
 {