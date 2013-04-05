9196773fd414dcab5332920642069880c23d1604
Carl Braganza <carl_braganza@xyratex.com>
2013-04-05 16:19:40 -0700

    Cleaned up the DLD index.

--- doc/dld-index.c
+++ doc/dld-index.c
@@ -1,55 +1,40 @@
 /**
 
+<!-- Note that the extra-long lines cannot be helped here -->
+
 @page DLDIX Detailed Designs
 
-Detailed designs, ordered alphabetically:
+Detailed designs, ordered alphabetically by title:
 
-- @subpage ADDB-DLD "ADDB detailed design specification"
-           <!-- addb/addb.c -->
-- @subpage m0t1fs "M0T1FS detailed level design specification"
-           <!-- m0t1fs/linux_kernel/m0t1fs.h -->
-- @subpage conf
-           <!-- conf/obj.c -->
-- @subpage DLD_conf_schema "DLD for configuration schema"
-           <!-- cfg/cfg.h -->
-- @subpage m0_long_lock-dld "FOM Long lock DLD"
-           <!-- fop/fom_long_lock.h -->
-- @subpage CMDLD "Copy Machine DLD"
-           <!-- cm/cm.c -->
-- @subpage CPDLD "Copy Packet DLD"
-           <!-- cm/cp.c -->
-- @subpage Layout-DB "Layout DB DLD"
-           <!-- layout/layout_db.c -->
-- @subpage LNetDLD "LNet Transport DLD"
-           <!-- net/lnet/lnet_main.c -->
-- @subpage net-test "Mero Network Benchmark"
-           <!-- net/test/main.c -->
-- @subpage MGMT-DLD "Management Interface Design"
-           <!-- mgmt/mgmt.c -->
-- @subpage rpc-layer-core-dld "RPC layer core DLD"
-           <!-- rpc/rpc.h -->
-- @subpage rpc-slot "RPC Slot"
-           <!-- rpc/slot.h -->
-- @subpage SNSRepairCMDLD "SNS Repair copy machine DLD"
-           <!-- sns/repair/cm.c -->
-- @subpage NetRQProvDLD "Auto-Provisioning of Receive Message Queue Buffers DLD"
-           <!-- net/tm_provision.c -->
-- @subpage io_bulk_client "IO bulk transfer Detailed Level Design"
-           <!-- ioservice/io_fops.c -->
-- @subpage DLD-bulk-server "DLD of Bulk Server"
-           <!-- ioservice/io_foms.c -->
-- @subpage m0loop-dld "The new m0loop device driver DLD"
-           <!-- m0t1fs/linux_kernel/m0loop.c -->
-- @subpage rmw_io_dld "DLD for read-modify-write IO requests"
-           <!-- m0t1fs/linux_kernel/file_internal.h -->
-- @subpage IOFOLDLD "IO FOL DLD"
-           <!-- ioservice/io_fops.c -->
+- @subpage ADDB-DLD "ADDB detailed design specification"  <!-- addb/addb.c -->
+- @subpage NetRQProvDLD "Auto-Provisioning of Receive Message Queue Buffers DLD" <!-- net/tm_provision.c -->
+- @subpage CMDLD "Copy Machine DLD" <!-- cm/cm.c -->
+- @subpage CPDLD "Copy Packet DLD"  <!-- cm/cp.c -->
+- @subpage conf "DLD of configuration caching" <!-- conf/obj.c -->
+- @subpage DLD_conf_schema "DLD for configuration schema" <!-- cfg/cfg.h -->
+- @subpage m0_long_lock-dld "FOM Long lock DLD" <!-- fop/fom_long_lock.h -->
+- <i>I/O Related</i>
+  - @subpage DLD-bulk-server "DLD of Bulk Server" <!-- ioservice/io_foms.c -->
+  - @subpage io_calls_params_dld "DLD of I/O calls Parameters Server-side" <!-- ioservice/io_device.c -->
+  - @subpage rmw_io_dld "DLD for read-modify-write IO requests" <!-- m0t1fs/linux_kernel/file_internal.h -->
+  - @subpage io_bulk_client "IO bulk transfer Detailed Level Design" <!-- ioservice/io_fops.c -->
+  - @subpage IOFOLDLD "IO FOL DLD" <!-- ioservice/io_fops.c -->
+  - @subpage iosnsrepair "I/O with SNS and SNS repair" <!-- m0t1fs/linux_kernel/
+file.c -->
+  - @subpage SNSCMDLD "SNS copy machine DLD" <!-- sns/cm/cm.c -->
+- @subpage Layout-DB "Layout DB DLD" <!-- layout/layout_db.c -->
+- @subpage LNetDLD "LNet Transport DLD" <!-- net/lnet/lnet_main.c -->
+- @subpage net-test "Mero Network Benchmark" <!-- net/test/main.c -->
+- @subpage MGMT-DLD "Management Interface Design" <!-- mgmt/mgmt.c -->
+- @subpage m0t1fs "M0T1FS detailed level design specification" <!-- m0t1fs/linux_kernel/m0t1fs.h -->
+- @subpage rpc-layer-core-dld "RPC layer core DLD" <!-- rpc/rpc.h -->
+- @subpage m0loop-dld "The new m0loop device driver DLD" <!-- m0t1fs/linux_kernel/m0loop.c -->
 
 Detailed designs should use the <i>@subpage DLD "Mero DLD Template"
 <!-- doc/dld-template.c --> </i> as a style guide.
 Higher level designs can be found in the
 <a href="https://docs.google.com/a/xyratex.com/#folders/
 0B-6nd3Xo8hzBZGJlNjE2NTctZDA1OC00NTIyLWJhOGItM2U5MTc2MjJjODNm">
-Mero Design Folder</a>.
+Google Drive Mero Design Folder</a>.
 
  */
