2ce6e5fd38c6564965f8ec59b4e1926225c28325
Nikita Danilov <nikita_danilov@xyratex.com>
2013-04-17 18:03:23 +0400

    dtm: add m0_dtm_catalogue_find() prototype.

--- dtm/catalogue.h
+++ dtm/catalogue.h
@@ -52,6 +52,11 @@ M0_INTERNAL int m0_dtm_catalogue_add(struct m0_dtm_catalogue *cat,
 				     struct m0_dtm_history *history);
 M0_INTERNAL int m0_dtm_catalogue_del(struct m0_dtm_catalogue *cat,
 				     struct m0_dtm_history *history);
+M0_INTERNAL int m0_dtm_catalogue_find(struct m0_dtm_catalogue *cat,
+				      struct m0_uint128 *id,
+				      struct m0_dtm_history *(*alloc)(void *),
+				      void *datum,
+				      struct m0_dtm_history **out);
 
 /** @} end of dtm group */
 
