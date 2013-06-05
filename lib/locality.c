e95424dd413e8131fa138e51ef8ef3c504ebb9af
Nikita Danilov <nikita_danilov@xyratex.com>
2013-06-05 21:52:50 +0400

    lib: add m0_locality code and UT.

---
+++ lib/locality.c
@@ -0,0 +1,114 @@
+/* -*- C -*- */
+/*
+ * COPYRIGHT 2013 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Nikita Danilov <nikita_danilov@xyratex.com>
+ * Original creation date: 04-Jun-2013
+ */
+
+/**
+ * @addtogroup locality
+ *
+ * @{
+ */
+
+#include "sm/sm.h"
+#include "lib/chan.h"
+#include "lib/arith.h"                    /* max_check */
+#include "lib/errno.h"                    /* ENOMEM */
+#include "lib/thread.h"
+#include "lib/memory.h"
+#include "lib/processor.h"
+#include "lib/locality.h"
+
+static struct m0_locality  locs_fallback;
+static struct m0_sm_group  locs_grp;
+static struct m0_locality *locs;
+static m0_processor_nr_t   locs_nr;
+static m0_processor_nr_t   locs_allocated;
+static struct m0_thread    locs_ast_thread;
+static bool                locs_shutdown;
+
+M0_INTERNAL struct m0_locality *m0_locality_here(void)
+{
+	return m0_locality_get(m0_processor_id_get());
+}
+
+M0_INTERNAL struct m0_locality *m0_locality_get(uint64_t value)
+{
+	struct m0_locality *loc = &locs[value % locs_nr];
+
+	return loc->lo_grp != NULL ? loc : &locs_fallback;
+}
+
+M0_INTERNAL void m0_locality_set(m0_processor_nr_t id, struct m0_sm_group *grp)
+{
+	M0_PRE(id < locs_allocated);
+	if (locs[id].lo_grp == NULL)
+		locs[id].lo_grp = grp;
+	locs_nr = max_check(locs_nr, id + 1);
+}
+
+static void locs_ast_handler(void *__unused)
+{
+	while (!locs_shutdown) {
+		m0_chan_wait(&locs_grp.s_clink);
+		m0_sm_group_lock(&locs_grp);
+		m0_sm_asts_run(&locs_grp);
+		m0_sm_group_unlock(&locs_grp);
+	}
+}
+
+M0_INTERNAL int m0_localities_init(void)
+{
+	int result;
+
+	m0_sm_group_init(&locs_grp);
+	locs_fallback.lo_grp = &locs_grp;
+	result = M0_THREAD_INIT(&locs_ast_thread, void *, NULL,
+				&locs_ast_handler, NULL, "fallback ast");
+	if (result == 0) {
+		locs_allocated = m0_processor_nr_max();
+		M0_ALLOC_ARR(locs, locs_allocated);
+		if (locs == NULL)
+			result = -ENOMEM;
+	}
+	return result;
+}
+
+M0_INTERNAL void m0_localities_fini(void)
+{
+	locs_shutdown = true;
+	m0_clink_signal(&locs_grp.s_clink);
+	m0_thread_join(&locs_ast_thread);
+	m0_thread_fini(&locs_ast_thread);
+	m0_free(locs);
+	m0_sm_group_fini(&locs_grp);
+}
+
+/** @} end of locality group */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
+/*
+ * vim: tabstop=8 shiftwidth=8 noexpandtab textwidth=80 nowrap
+ */
---
+++ lib/locality.h
@@ -0,0 +1,97 @@
+/* -*- C -*- */
+/*
+ * COPYRIGHT 2013 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Nikita Danilov <nikita_danilov@xyratex.com>
+ * Original creation date: 04-Jun-2013
+ */
+
+#pragma once
+
+#ifndef __MERO_LIB_LOCALITY_H__
+#define __MERO_LIB_LOCALITY_H__
+
+/**
+ * @defgroup locality
+ *
+ * @{
+ */
+
+/* import */
+#include "lib/types.h"
+#include "lib/processor.h"
+struct m0_sm_group;
+
+/* export */
+struct m0_locality;
+
+/**
+ * Per-core state maintained by Mero.
+ */
+struct m0_locality {
+	/**
+	 * State machine group associated with the core.
+	 *
+	 * This group can be used to post ASTs to be executed on a current or
+	 * specified core.
+	 *
+	 * This group comes from request handler locality, so that execution of
+	 * ASTs is serialised with state transitions of foms in this locality.
+	 */
+	struct m0_sm_group *lo_grp;
+	/* Other fields might be added here. */
+};
+
+/**
+ * Returns locality corresponding to the core the call is made on.
+ *
+ * @post result->lo_grp != NULL
+ */
+M0_INTERNAL struct m0_locality *m0_locality_here(void);
+
+/**
+ * Returns locality corresponding in some unspecified, but deterministic way to
+ * the supplied value.
+ *
+ * @post result->lo_grp != NULL
+ */
+M0_INTERNAL struct m0_locality *m0_locality_get(uint64_t value);
+
+/**
+ * Initialises per-core state for the specified core.
+ *
+ * This has effect only once per-core.
+ */
+M0_INTERNAL void m0_locality_set(m0_processor_nr_t id, struct m0_sm_group *grp);
+
+M0_INTERNAL int  m0_localities_init(void);
+M0_INTERNAL void m0_localities_fini(void);
+
+/** @} end of locality group */
+
+#endif /* __MERO_LIB_LOCALITY_H__ */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
+/*
+ * vim: tabstop=8 shiftwidth=8 noexpandtab textwidth=80 nowrap
+ */
