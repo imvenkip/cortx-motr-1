imvenkip/cortx-motr-1

commit 3f1608639f097149dabe069beda490d2f322ba02

be/btree: copy_key_val() memleak fixed
It should not allocate and copy key/value data.

be/btree.c
@@ -172,10 +172,7 @@ static struct m0_be_bnode *merge_siblings(struct m0_be_btree *btree,
 				      struct m0_be_bnode *parent,
 				      unsigned int index, enum position_t pos);
 
-static void copy_key_val(struct m0_be_btree	 *btree,
-			 struct m0_be_tx	 *tx,
-			 struct bt_key_val	 *src,
-			 struct bt_key_val	 *dst);
+static void copy_key_val(struct bt_key_val *src, struct bt_key_val *dst);
 
 
 /* ------------------------------------------------------------------
@@ -760,10 +757,9 @@ del_loop:
 				move_key(btree, tx, parent, i, P_LEFT);
 			} else
 			/*  The current node has (t - 1) keys but the */
-			/*  left sibling has (t - 1) keys */
+			/*  left sibling has > (t - 1) keys */
 			if (lsibling
-				    && (lsibling->b_nr_active >
-						BTREE_FAN_OUT - 1)) {
+			    && (lsibling->b_nr_active > BTREE_FAN_OUT - 1)) {
 				move_key(btree, tx, parent, i, P_RIGHT);
 			} else
 			/*  Left sibling has (t - 1) keys */
@@ -782,19 +778,11 @@ del_loop:
 	/* Case 1:
 	 * The node containing the key is found and is the leaf node. */
 	/* Also the leaf node has keys greater than the minimum required. */
-	/* Simply remove the key */
-	if (node->b_leaf && (node->b_nr_active > BTREE_FAN_OUT - 1)) {
-		node_pos.p_node = node;
-		node_pos.p_index = index;
-		delete_key_from_node(btree, tx, &node_pos);
-		mem_update(btree, tx, btree, sizeof(struct m0_be_btree));
-		node_update(btree->bb_root, btree, tx);
-		M0_POST(btree_invariant(btree));
-		return 0;
-	}
 	/* If the leaf node is the root permit deletion even if the */
-	/* number of keys is less than (t - 1) */
-	if (node->b_leaf && (node == btree->bb_root)) {
+	/* number of keys is less than (t - 1). */
+	/* Simply remove the key */
+	if (node->b_leaf && (node->b_nr_active > BTREE_FAN_OUT - 1 ||
+	                     node == btree->bb_root)) {
 		node_pos.p_node = node;
 		node_pos.p_index = index;
 		delete_key_from_node(btree, tx, &node_pos);
@@ -819,7 +807,7 @@ del_loop:
 			/* @todo: analyse return code */
 			M0_ASSERT(new_key_val != NULL);
 
-			copy_key_val(btree, tx, key_val, new_key_val);
+			copy_key_val(key_val, new_key_val);
 			node->b_key_vals[index] = new_key_val;
 
 			/* Update key vals pointer, WARNING: can be XXX. */
@@ -843,7 +831,7 @@ del_loop:
 				mem_alloc(btree, tx, sizeof(struct bt_key_val));
 			M0_ASSERT(new_key_val != NULL);	/* @todo: analyse return code */
 
-			copy_key_val(btree, tx, key_val, new_key_val);
+			copy_key_val(key_val, new_key_val);
 			node->b_key_vals[index] = new_key_val;
 
 			/* Update key vals pointer, WARNING: can be XXX. */
@@ -1022,28 +1010,12 @@ static struct bt_key_val *btree_search(struct m0_be_btree *btree, void *key)
  * @param src The source key value
  * @param dst The dest key value
  */
-static void copy_key_val(struct m0_be_btree *btree,
-			 struct m0_be_tx    *tx,
-			 struct bt_key_val  *src,
-			 struct bt_key_val  *dst)
+static void copy_key_val(struct bt_key_val  *src, struct bt_key_val  *dst)
 {
-	unsigned int keysize;
-	unsigned int datasize;
-
 	M0_ENTRY();
 
-	keysize = btree->bb_ops->ko_ksize(src->key);
-	dst->key = (void *)mem_alloc(btree, tx, keysize);
-	M0_ASSERT(dst->key != NULL);	/* @todo: analyse return code */
-
-	bcopy(src->key, dst->key, keysize);
-
-	if (src->val) {
-		datasize = btree->bb_ops->ko_vsize(src->val);
-		dst->val = (void *)mem_alloc(btree, tx, datasize);
-		M0_ASSERT(dst->val != NULL);	/* @todo: analyse return code */
-		bcopy(src->val, dst->val, datasize);
-	}
+	dst->key = src->key;
+	dst->val = src->val;
 
 	M0_LEAVE();
 }

be/extmap.c
@@ -21,6 +21,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_BE
+#include "lib/trace.h"
 #include "lib/vec.h"
 #include "lib/errno.h"
 #include "lib/arith.h"   /* M0_3WAY */
@@ -119,6 +121,30 @@ static const struct m0_be_btree_kv_ops be_emap_ops = {
         .ko_vsize =   be_emap_vsize,
         .ko_compare = be_emap_cmp
 };
+
+static void emap_dump(struct m0_be_emap_cursor *it)
+{
+	struct m0_be_emap_cursor	 scan;
+	struct m0_uint128		*prefix	= &it->ec_key.ek_prefix;
+	struct m0_be_emap_seg		*seg	= &scan.ec_seg;
+	int				 i;
+	int				 rc;
+
+	rc = be_emap_lookup(it->ec_map, prefix, 0, &scan);
+	M0_ASSERT(rc == 0);
+
+	M0_LOG(M0_DEBUG, "%010lx:%010lx:", prefix->u_hi, prefix->u_lo);
+	for (i = 0; ; ++i) {
+		M0_LOG(M0_DEBUG, "\t%5.5i %16lx .. %16lx: %16lx %10lx", i,
+		       seg->ee_ext.e_start, seg->ee_ext.e_end,
+		       m0_ext_length(&seg->ee_ext), seg->ee_val);
+		if (m0_be_emap_ext_is_last(&seg->ee_ext))
+			break;
+		rc = be_emap_next(&scan);
+		M0_ASSERT(rc == 0);
+	}
+	be_emap_close(&scan);
+}
 
 M0_INTERNAL void m0_be_emap_init(struct m0_be_emap *map,
 				 struct m0_be_seg  *db)
@@ -311,6 +337,7 @@ M0_INTERNAL void m0_be_emap_split(struct m0_be_emap_cursor *it,
 	m0_be_op_state_set(&it->ec_op, M0_BOS_ACTIVE);
 	be_emap_split(it, tx, vec, it->ec_seg.ee_ext.e_start);
 	m0_be_op_state_set(&it->ec_op, M0_BOS_SUCCESS);
+	//emap_dump(it);
 	M0_ASSERT_EX(be_emap_invariant(it));
 }
 
@@ -791,16 +818,14 @@ be_emap_prev(struct m0_be_emap_cursor *it)
 	return emap_it_open(it);
 }
 
-#if 0
+#if 1
 static bool
 be_emap_invariant_check(struct m0_be_emap_cursor *it)
 {
-	int                   result;
-	m0_bindex_t           reached;
-	m0_bcount_t           total;
+	int                   rc;
+	m0_bindex_t           reached	= 0;
+	m0_bcount_t           total	= 0;
 
-	reached = 0;
-	total   = 0;
 	if (!m0_be_emap_ext_is_first(&it->ec_seg.ee_ext))
 		return false;
 	while (1) {
@@ -812,8 +837,8 @@ be_emap_invariant_check(struct m0_be_emap_cursor *it)
 		total += m0_ext_length(&it->ec_seg.ee_ext);
 		if (m0_be_emap_ext_is_last(&it->ec_seg.ee_ext))
 			break;
-		result = be_emap_next(it);
-		if (result != 0)
+		rc = be_emap_next(it);
+		if (rc != 0)
 			return true;
 	}
 	if (total != M0_BCOUNT_MAX)
@@ -827,16 +852,19 @@ static bool
 be_emap_invariant(struct m0_be_emap_cursor *it)
 {
 	struct m0_be_emap_cursor scan;
-	int                      result;
-	bool                     check;
+	int                      rc;
+	bool                     is_good = true;
 
-	result = be_emap_lookup(it->ec_map, &it->ec_key.ek_prefix, 0, &scan);
-	if (result == 0) {
-		check = be_emap_invariant_check(&scan);
+	rc = be_emap_lookup(it->ec_map, &it->ec_key.ek_prefix, 0, &scan);
+	if (rc == 0) {
+		is_good = be_emap_invariant_check(&scan);
 		be_emap_close(&scan);
-	} else
-		check = true;
-	return check;
+	}
+
+	if (!is_good)
+		emap_dump(it);
+
+	return is_good;
 }
 
 #else
@@ -913,6 +941,7 @@ be_emap_caret_invariant(const struct m0_be_emap_caret *car)
 		 car->ct_index == M0_BINDEX_MAX + 1);
 }
 
+#undef M0_TRACE_SUBSYSTEM
 /** @} end group extmap */
 
 /*

be/ut/extmap.c
@@ -251,7 +251,8 @@ static void split(m0_bindex_t offset, int nr, bool commit)
 		m0_bcount_t total;
 
 		seglen = m0_ext_length(&seg->ee_ext);
-		M0_LOG(M0_DEBUG, "seglen=%llu", (unsigned long long)seglen);
+		M0_LOG(M0_DEBUG, "%3i: seglen=%llu", i,
+					(unsigned long long)seglen);
 		total  = 102; /* 100 + 2, the sum of elements in len[]. */
 		M0_UT_ASSERT(seglen > total);
 		len[ARRAY_SIZE(len) - 1] = seglen - total;

commit fce5dd44c9e4ea6fad4ca52007519dd8bdc523a7

be/btree.c: btree_delete_key() memleak fixed
Few issues are fixed here. First, the regression
introduced after 3f16086: child key/value data was deleted.
Second, the actual key/value data still was not freed.

be/btree.c
@@ -172,8 +172,6 @@ static struct m0_be_bnode *merge_siblings(struct m0_be_btree *btree,
 				      struct m0_be_bnode *parent,
 				      unsigned int index, enum position_t pos);
 
-static void copy_key_val(struct bt_key_val *src, struct bt_key_val *dst);
-
 
 /* ------------------------------------------------------------------
  * Btree invariant implementation:
@@ -695,7 +693,7 @@ static int btree_delete_key(struct m0_be_btree   *btree,
 	unsigned int i, index;
 	struct m0_be_bnode *node = NULL, *rsibling, *lsibling;
 	struct m0_be_bnode *comb_node, *parent;
-	struct node_pos sub_node_pos;
+	struct node_pos child;
 	struct node_pos node_pos;
 	struct bt_key_val *key_val;
 	void *kv = key;
@@ -796,42 +794,41 @@ del_loop:
 	 * The node containing the key is found and is an internal node */
 	if (node->b_leaf == false) {
 		if (node->b_children[index]->b_nr_active > BTREE_FAN_OUT - 1) {
-			get_max_key_pos(btree, node->b_children[index],
-					&sub_node_pos);
-			key_val =
-			  sub_node_pos.p_node->b_key_vals[sub_node_pos.p_index];
+			get_max_key_pos(btree, node->b_children[index], &child);
+			key_val = child.p_node->b_key_vals[child.p_index];
 
-			copy_key_val(key_val, node->b_key_vals[index]);
+			M0_SWAP(*key_val, *node->b_key_vals[index]);
 			mem_update(btree, tx, node->b_key_vals[index],
 						sizeof(struct bt_key_val));
 
 			btree_delete_key(btree, tx, node->b_children[index],
 					 key_val->key);
-			if (sub_node_pos.p_node->b_leaf == false) {
+			if (child.p_node->b_leaf == false) {
 				M0_LOG(M0_ERROR, "Not leaf");
 			}
 		} else if (node->b_children[index + 1]->b_nr_active >
 			   BTREE_FAN_OUT - 1) {
 			get_min_key_pos(btree, node->b_children[index + 1],
-					&sub_node_pos);
-			key_val =
-			  sub_node_pos.p_node->b_key_vals[sub_node_pos.p_index];
+					&child);
+			key_val = child.p_node->b_key_vals[child.p_index];
 
-			copy_key_val(key_val, node->b_key_vals[index]);
+			M0_SWAP(*key_val, *node->b_key_vals[index]);
 			mem_update(btree, tx, node->b_key_vals[index],
 						sizeof(struct bt_key_val));
 
 			btree_delete_key(btree, tx, node->b_children[index + 1],
 					 key_val->key);
-			if (sub_node_pos.p_node->b_leaf == false) {
+			if (child.p_node->b_leaf == false) {
 				M0_LOG(M0_ERROR, "Not leaf");
 			}
 
-		} else if (node->b_children[index]->b_nr_active == BTREE_FAN_OUT - 1
-			   && node->b_children[index + 1]->b_nr_active ==
-			   BTREE_FAN_OUT - 1) {
+		} else if (node->b_children[index]->b_nr_active ==
+							BTREE_FAN_OUT - 1 &&
+			   node->b_children[index + 1]->b_nr_active ==
+							BTREE_FAN_OUT - 1) {
 
-			comb_node = merge_nodes(btree, tx, node->b_children[index],
+			comb_node = merge_nodes(btree, tx,
+						node->b_children[index],
 						node->b_key_vals[index],
 						node->b_children[index + 1]);
 			node->b_children[index] = comb_node;
@@ -987,21 +984,6 @@ static struct bt_key_val *btree_search(struct m0_be_btree *btree, void *key)
 		key_val = kp.p_node->b_key_vals[kp.p_index];
 	}
 	return key_val;
-}
-
-/**
- * Used to copy key value from source to destination
- * @param src The source key value
- * @param dst The dest key value
- */
-static void copy_key_val(struct bt_key_val  *src, struct bt_key_val  *dst)
-{
-	M0_ENTRY();
-
-	dst->key = src->key;
-	dst->val = src->val;
-
-	M0_LEAVE();
 }
 
 /**