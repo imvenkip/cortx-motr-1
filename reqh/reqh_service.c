5a3e7b7cd85b2ab65c1a96aacda60a156d28df16
Andriy Tkachuk <andriy_tkachuk@xyratex.com>
2013-09-05 10:44:38 +0300

    reqh_service.c: rpc-related calls restored

--- reqh/reqh_service.c
+++ reqh/reqh_service.c
@@ -353,14 +353,14 @@ M0_INTERNAL void m0_reqh_service_stop(struct m0_reqh_service *service)
 	m0_rwlock_write_unlock(&reqh->rh_rwlock);
 
 	service->rs_ops->rso_stop(service);
-//XXX_BE_DB 	m0_rpc_service_reverse_session_put(&service->rs_rpc_svc);
+	m0_rpc_service_reverse_session_put(&service->rs_rpc_svc);
 	M0_ASSERT(m0_reqh_lockers_get(reqh, key) == service);
 	m0_reqh_lockers_clear(reqh, key);
 }
 
 static void reqh_rpc_svc_fini_and_free(struct m0_rpc_service *service)
 {
-//XXX_BE_DB 	m0_rpc__service_fini(service);
+	m0_rpc__service_fini(service);
 	service->svc_state = M0_RPC_SERVICE_STATE_UNDEFINED;
 }
 
@@ -374,26 +374,20 @@ reqh_rpc_svc_alloc_and_init(struct m0_rpc_service_type *service_type,
 			    const struct m0_uint128    *uuid,
 			    struct m0_rpc_service     **out)
 {
-#if 0 /* XXX_BE_DB */
 	int rc;
 
 	rc = m0_rpc__service_init(*out, service_type, ep_addr, uuid,
 				  &reqh_rpc_svc_ops);
 	(*out)->svc_state = M0_RPC_SERVICE_STATE_INITIALISED;
 	return rc;
-#else
-	return -1;
-#endif
 }
 
 static const struct m0_rpc_service_type_ops reqh_rpc_svct_ops = {
 	.rsto_alloc_and_init = reqh_rpc_svc_alloc_and_init,
 };
 
-#if 0 /* XXX_BE_DB */
 M0_RPC_SERVICE_TYPE_DEFINE(static, reqh_rpc_svct, "Reqh service rpc svc",
 			   M0_REQH_SVC_RPC_SERVICE_TYPE, &reqh_rpc_svct_ops);
-#endif
 
 M0_INTERNAL void m0_reqh_service_init(struct m0_reqh_service *service,
 				      struct m0_reqh         *reqh,
@@ -401,11 +395,9 @@ M0_INTERNAL void m0_reqh_service_init(struct m0_reqh_service *service,
 {
 	struct m0_addb_ctx_type *serv_addb_ct;
 	struct m0_rpc_machine   *rmach;
-#if 0 /* XXX_BE_DB */
 	const char              *ep;
 	struct m0_uint128        uuid1;
 	struct m0_rpc_service   *rpcsvc = &service->rs_rpc_svc;
-#endif
 
 	M0_PRE(service != NULL && reqh != NULL &&
 		service->rs_sm.sm_state == M0_RST_INITIALISING);
@@ -430,8 +422,8 @@ M0_INTERNAL void m0_reqh_service_init(struct m0_reqh_service *service,
 	m0_chan_init(&service->rs_rev_conn_wait, &service->rs_mutex);
 	rmach = m0_reqh_rpc_mach_tlist_head(&reqh->rh_rpc_machines);
 	M0_ASSERT(rmach != NULL);
-//XXX_BE_DB 	ep = m0_rpc_machine_ep(rmach);
-//XXX_BE_DB 	m0_rpc_service_alloc_and_init(&reqh_rpc_svct, ep, &uuid1, &rpcsvc);
+	ep = m0_rpc_machine_ep(rmach);
+	m0_rpc_service_alloc_and_init(&reqh_rpc_svct, ep, &uuid1, &rpcsvc);
 	reqh_service_state_set(service, M0_RST_INITIALISED);
 
 	/** @todo: Need to pass the service uuid "hi" & "low"
@@ -463,7 +455,7 @@ M0_INTERNAL void m0_reqh_service_fini(struct m0_reqh_service *service)
 	m0_sm_group_lock(&service->rs_reqh->rh_sm_grp);
 	m0_sm_fini(&service->rs_sm);
 	m0_sm_group_unlock(&service->rs_reqh->rh_sm_grp);
-//XXX_BE_DB 	m0_rpc_service_fini_and_free(&service->rs_rpc_svc);
+	m0_rpc_service_fini_and_free(&service->rs_rpc_svc);
 	m0_chan_fini_lock(&service->rs_rev_conn_wait);
 	m0_mutex_fini(&service->rs_mutex);
 	service->rs_ops->rso_fini(service);
