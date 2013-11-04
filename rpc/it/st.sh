20f524a7cacc6e9094832a2ef3604e439f20cfe1
Maxim Medved <max_medved@xyratex.com>
2013-11-04 22:16:36 +0200

    scripts/m0: moved rpcping ST to rpc/it/st.sh

---
+++ rpc/it/st.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CWD=$(cd "$( dirname "$0")" && pwd)
+SRC="$CWD/../.."
+
+rc=0
+sudo "$SRC/m0t1fs/linux_kernel/st/st" insmod
+"$CWD/st" || rc=$?
+sudo "$SRC/m0t1fs/linux_kernel/st/st" rmmod
+[ $rc -eq 0 ] || exit $rc
