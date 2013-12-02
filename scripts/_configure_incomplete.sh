459d426d5ae4e031447899e47e35c2a298003017
Maxim Medved <max_medved@xyratex.com>
2013-12-02 03:37:51 +0200

    deleted temporary files used in db => be convertation

--- scripts/_configure_incomplete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# XXX remove this file after m0_db -> m0_be conversion
-
-make distclean
-sh autogen.sh && ./configure --disable-system-libs --enable-debug --disable-rpm --disable-altogether-mode && make -j3
-make -j3 all-user
--- scripts/_make_incomplete.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# XXX remove this file after m0_db -> m0_be conversion
-
-make -j3 all-user
