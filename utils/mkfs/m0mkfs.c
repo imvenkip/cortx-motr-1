c8e528dadce2c73ee61e924f6109a211a54c0721
Valery V. Vorotyntsev <valery_vorotyntsev@xyratex.com>
2014-05-04 13:51:15 +0300

    - added comments to mkfs tool

--- utils/mkfs/m0mkfs.c
+++ utils/mkfs/m0mkfs.c
@@ -38,6 +38,50 @@
 /**
    @addtogroup m0mkfs
    @{
+
+   Abstract.
+
+   This is mkfs stage0 tool. It prepares storage in a way m0d did before
+   and allows to run m0d after that.
+
+   Currently mkfs does the following:
+   - prepare stob for IO;
+   - prepare stob for addb;
+   - prepare metadata and create initial metadata structures, such as:
+     * root cob;
+     * hierarachy root;
+     * initial metadata tables.
+   - segmap is created in normal file and saved in working dir. It then
+     can be found by m0d in startup time and used;
+
+   Options.
+
+   Mkfs fully reuses startup code from mero/setup.c with tiny modificatios
+   in order to support "erase" mode, that is, when mkfs is run on top of
+   existing stobs and structures that have already been used by m0d.
+
+   This is why m0_init() and m0_cs_init() and m0_cs_setup_env() are used
+   same way as they used by m0d. The difference is that m0_cs_setup_env()
+   has "mkfs" argument set to true and services and fops handling is not
+   started for mkfs path.
+
+   That said, that both, mkfs and m0d share the same set of options such
+   as endpoints, bufer sizes, etc. Obviously, not all of them needed for
+   mkfs as they are clearly m0d start options and not all of them needed
+   by m0d as they may be applied to mkfs once, saved and reussd by m0d
+   in startup time. This set of options that will not change between runs
+   and needs to be saved by mkfs to some storare structures.
+
+   This structure that we can use for saving things like that is called
+   seg0. Integration with seg0 is part of stage1 work that will be started
+   as soon as stage0 is finished.
+
+   Anticipating stage1 work.
+
+   - seg0 integration;
+   - save those m0d start options, that don't change between runs, to seg0;
+   - stop sharing options between m0d and mkfs. Each of them should
+     handle only minimaly need set of them.
  */
 
 /**
