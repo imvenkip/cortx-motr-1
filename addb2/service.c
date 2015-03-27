1397de9136202db50fd62a6f86bb4a016a1dfb94
Your Name <you@example.com>
2015-03-27 16:06:28 +0300

    addb2: add service.[ch] to build.
    Change-Id: I153a8f26fe5cf135625ee9135fadca0917e40745

--- addb2/service.c
+++ addb2/service.c
@@ -24,25 +24,30 @@
  * @{
  */
 
+#undef M0_ADDB_CT_CREATE_DEFINITION
+#define M0_ADDB_CT_CREATE_DEFINITION
 #define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_ADDB
 
 #include "lib/trace.h"
 #include "lib/assert.h"
 #include "lib/chan.h"
 #include "lib/memory.h"
+#include "lib/errno.h"                  /* ENOMEM */
 #include "lib/misc.h"                   /* M0_AMB */
 #include "addb/addb.h"
+#include "reqh/reqh_service.h"
+#include "reqh/reqh.h"
+#include "fop/fom.h"
+#include "fop/fop.h"
 
 #include "addb2/addb2.h"
 #include "addb2/internal.h"
 #include "addb2/consumer.h"
-#include "addb2/net.h"
-
-#undef M0_TRACE_SUBSYSTEM
+#include "addb2/sys.h"
+#include "addb2/service.h"
 
 struct addb2_service {
 	struct m0_reqh_service   ase_service;
-	struct m0_addb2_storgae *ase_stor;
 	struct m0_addb2_source   ase_src;
 };
 
@@ -56,7 +61,7 @@ struct addb2_fom {
 static int    addb2_service_start(struct m0_reqh_service *service);
 static void   addb2_service_stop(struct m0_reqh_service *service);
 static void   addb2_service_fini(struct m0_reqh_service *service);
-static void   addb2_trace_done(struct m0_addb2_trace_obj *obj);
+static void   addb2_done(struct m0_addb2_trace_obj *obj);
 static size_t addb2_fom_home_locality(const struct m0_fom *fom);
 static int    addb2_service_type_allocate(struct m0_reqh_service **service,
 				      const struct m0_reqh_service_type *stype);
@@ -94,18 +99,16 @@ static int addb2_service_start(struct m0_reqh_service *svc)
 
 static void addb2_service_stop(struct m0_reqh_service *svc)
 {
-	struct addb2_service *service = M0_AMB(svc, service, ase_service);
+	struct addb2_service *service = M0_AMB(service, svc, ase_service);
 
 	M0_PRE(m0_reqh_service_state_get(svc) == M0_RST_STOPPED);
-	m0_addb2_storage_stop(service->ase_service);
 }
 
 static void addb2_service_fini(struct m0_reqh_service *svc)
 {
-	struct addb2_service *service = M0_AMB(svc, service, ase_service);
+	struct addb2_service *service = M0_AMB(service, svc, ase_service);
 
 	M0_PRE(m0_reqh_service_state_get(svc) == M0_RST_STOPPED);
-	m0_addb2_storage_fini(service->ase_service);
 	m0_addb2_source_fini(&service->ase_src);
 	m0_free(service);
 }
@@ -117,18 +120,13 @@ static int addb2_service_type_allocate(struct m0_reqh_service **svc,
 
 	M0_ALLOC_PTR(service);
 	if (service != NULL) {
-		service->ase_stor = m0_addb2_storage_init(stob, size,
-							  last,
-							  ops);
-		if (service->ase_stor != NULL) {
-			*svc = &service->ase_reqhs;
-			(*svc)->rs_type = stype;
-			(*svc)->rs_ops  = &addb2_service_ops;
-			m0_addb2_source_init(&service->ase_src);
-			return M0_RC(0);
-		}
-	}
-	return M0_ERR(-ENOMEM);
+		*svc = &service->ase_service;
+		(*svc)->rs_type = stype;
+		(*svc)->rs_ops  = &addb2_service_ops;
+		m0_addb2_source_init(&service->ase_src);
+		return M0_RC(0);
+	} else
+		return M0_ERR(-ENOMEM);
 }
 
 static int addb2_fom_create(struct m0_fop *fop,
@@ -139,12 +137,12 @@ static int addb2_fom_create(struct m0_fop *fop,
 	M0_ALLOC_PTR(fom);
 	if (fom != NULL) {
 		*out = &fom->a2_fom;
-		m0_chan_init(&fom->a2_chan);
+		m0_chan_init(&fom->a2_chan, NULL);
 		m0_fom_init(*out, &fop->f_type->ft_fom_type,
 			    &addb2_fom_ops, fop, NULL, reqh);
 		return M0_RC(0);
 	} else
-		return M0_RC(-ENOMEM);
+		return M0_ERR(-ENOMEM);
 }
 
 enum {
@@ -158,26 +156,27 @@ static int addb2_fom_tick(struct m0_fom *fom0)
 {
 	struct addb2_fom          *fom     = M0_AMB(fom, fom0, a2_fom);
 	struct m0_addb2_trace_obj *obj     = &fom->a2_obj;
-	struct m0_addb2_trace     *trace   = m0_fop_data(fom->fo_fop);
-	struct addb2_service      *service = M0_AMB(service, fom0->fo_service,
-						    ase_service);
-	struct m0_addb2_storage   *stor    = service->ase_stor;
+	struct m0_addb2_trace     *trace   = m0_fop_data(fom0->fo_fop);
+	struct m0_reqh_service    *svc     = fom0->fo_service;
+	struct addb2_service      *service = M0_AMB(service, svc, ase_service);
 	struct m0_addb2_source    *src     = &service->ase_src;
+	struct m0_addb2_cursor    *cur     = &fom->a2_cur;
+	struct m0_addb2_sys       *sys;
 
-	trace = m0_fop_data(fom->fo_fop);
-	switch (m0_fom_phase(fom)) {
+	sys = &svc->rs_reqh->rh_fom_dom.fd_addb2_sys;
+	switch (m0_fom_phase(fom0)) {
 	case ADDB2_CONSUME:
-		m0_addb2_cursor_init(&fom->a2_cur, trace);
-		while (m0_addb2_cursor_next(&fom->a2_cur) > 0)
+		m0_addb2_cursor_init(cur, trace);
+		while (m0_addb2_cursor_next(cur) > 0)
 			m0_addb2_consume(src, &cur->cu_rec);
-		m0_addb2_cursor_fini(&fom->a2_cur);
+		m0_addb2_cursor_fini(cur);
 		m0_fom_phase_set(fom0, ADDB2_SUBMIT);
 		return M0_FSO_AGAIN;
 	case ADDB2_SUBMIT:
 		obj->o_tr   = *trace;
-		obj->o_done = &addb2_trace_done;
+		obj->o_done = &addb2_done;
 		m0_fom_wait_on(fom0, &fom->a2_chan, &fom0->fo_cb);
-		m0_addb2_storage_submit(stor, obj);
+		m0_addb2_sys_submit(sys, obj);
 		m0_fom_phase_set(fom0, ADDB2_DONE);
 		return M0_FSO_WAIT;
 	case ADDB2_DONE:
@@ -203,7 +202,7 @@ static void addb2_fom_addb_init(struct m0_fom *fom, struct m0_addb_mc *mc)
 			 &fom->fo_service->rs_addb_ctx);
 }
 
-static void addb2_trace_done(struct m0_addb2_trace_obj *obj)
+static void addb2_done(struct m0_addb2_trace_obj *obj)
 {
 	struct addb2_fom *fom = M0_AMB(fom, obj, a2_obj.o_tr);
 	m0_chan_signal(&fom->a2_chan);
@@ -227,10 +226,10 @@ static const struct m0_fom_type_ops addb2_fom_type_ops = {
 };
 
 static const struct m0_reqh_service_type_ops addb2_service_type_ops = {
-	.rsto_service_allocate = &addb2_service_type_service_allocate
+	.rsto_service_allocate = &addb2_service_type_allocate
 };
 
-static const struct m0_reqh_addb2_service_ops addb2_service_ops = {
+static const struct m0_reqh_service_ops addb2_service_ops = {
 	.rso_start = &addb2_service_start,
 	.rso_stop  = &addb2_service_stop,
 	.rso_fini  = &addb2_service_fini
@@ -238,6 +237,8 @@ static const struct m0_reqh_addb2_service_ops addb2_service_ops = {
 
 M0_REQH_SERVICE_TYPE_DEFINE(m0_addb2_service_type, &addb2_service_type_ops,
 			    "addb2", &m0_addb_ct_addb2_svc, 2);
+
+#undef M0_TRACE_SUBSYSTEM
 
 /** @} end of addb2 group */
 
