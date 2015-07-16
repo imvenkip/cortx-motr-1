bac0e2ebb354726a04431fb254df3cceebed718f
Dima Chumak <dmitriy.chumak@seagate.com>
2015-07-16 21:44:17 +0700

    st: add jenkins status line to fsync and fwait tests
    Change-Id: Ie64620a901b0e544b4a06360ef156494b468f044

--- m0t1fs/linux_kernel/st/m0t1fs_fsync_test.sh
+++ m0t1fs/linux_kernel/st/m0t1fs_fsync_test.sh
@@ -52,3 +52,9 @@ main()
 
 trap unprepare EXIT
 main
+
+# this msg is used by Jenkins as a test success criteria;
+# it should appear on STDOUT
+if [ $? -eq 0 ] ; then
+    echo "fsync: test status: SUCCESS"
+fi
