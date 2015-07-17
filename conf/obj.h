imvenkip/cortx-motr-1

commit 1672c0231274435668740cd4d06381a735b789b4

Add Process reconfig functionality:
Add restart functionality to m0d
- signal to m0d
- flag reconfig to mero instance
- handle singnal with select reconfig/stop
- restart functionality to m0d

Fix stop session when service is died by reconfig

Add clean data for lockers fini
Add clean ldata for locality fini
Add clean locality data for addb2 fini
Add fini addb2 ctx, fop ctx, fop type, confx,
Add quiesce stage init/fini modules

Add reqh lockers free

Change Process configure cores field as m0_bitmap for Conf & Spiel interface

Update sss/st system test - add Process reconfig test

Change-Id: I45636b3d72b5886cc5859fe6e08058a513a56715

conf/obj.h
@@ -418,8 +418,16 @@ struct m0_conf_process {
 	struct m0_conf_obj  pc_obj;
 	struct m0_conf_dir *pc_services;
 /* configuration data (for the application) */
-	uint32_t            pc_cores;
-	uint32_t            pc_memlimit;
+	/** Core mask  */
+	struct m0_bitmap    pc_cores;
+	/** Memory limits - AS */
+	uint64_t            pc_memlimit_as;
+	/** Memory limits - RSS */
+	uint64_t            pc_memlimit_rss;
+	/** Memory limits - Stack */
+	uint64_t            pc_memlimit_stack;
+	/** Memory limits - Memory Lock */
+	uint64_t            pc_memlimit_memlock;
 };
 
 struct m0_conf_service {

utils/m0confgen
@@ -35,6 +35,7 @@ input_format = """Input format:
     attr    = ID "=" ( NUMBER | STRING | fid | objid | list )
     fid     = "(" NUMBER "," NUMBER ")"
     list    = "[" [ csv(NUMBER) | csv(STRING) | csv(fid) | csv(objid) ] "]"
+    bitmap  = "[NUMBER : NUMBER]"
     objtype = { %(objtypes)s }
 
 Examples:
@@ -69,7 +70,9 @@ conf_obj_types = {
     'objv': ('j', 'real=objid children=[objid]'),
     'node': ('n', 'memsize=NUMBER nr_cpu=NUMBER last_state=NUMBER flags=NUMBER'
              ' pool_id=objid processes=[objid]'),
-    'process': ('r', 'mem_limit=NUMBER cores=NUMBER services=[objid]'),
+    'process': ('r', 'mem_limit_as=NUMBER mem_limit_rss=NUMBER'
+                ' mem_limit_stack=NUMBER mem_limit_memlock=NUMBER cores=bitmap'
+                ' services=[objid]'),
     'service': ('s', 'type=NUMBER endpoints=[STRING] sdevs=[objid]'),
     'sdev': ('d', 'iface=NUMBER media=NUMBER bsize=NUMBER size=NUMBER'
              ' last_state=NUMBER flags=NUMBER filename=STRING'),
@@ -117,7 +120,8 @@ def conf_type2enc(t):
         'NUMBER': str,
         'STRING': quote,
         'fid': repr,
-        'objid': conf_objid_encode
+        'objid': conf_objid_encode,
+        'bitmap': repr
     }[t]
     return list_encode_with(enc) if list_p else enc
 
@@ -210,6 +214,10 @@ def p_fid(p):
     "fid : '(' NUMBER ',' NUMBER ')'"
     p[0] = p[2], p[4]
 
+def p_bitmap(p):
+    "bitmap : '[' NUMBER ':' NUMBER ']'"
+    p[0] = p[2], p[4]
+
 def p_attrs(p):
     """
     attrs : attr
@@ -298,6 +306,7 @@ class TestEncoders(unittest.TestCase):
         self.assertEqual(conf_type2enc('NUMBER')(42), '42')
         self.assertEqual(conf_type2enc('STRING')('abc'), '"abc"')
         self.assertEqual(conf_type2enc('fid')((11, 22)), '(11, 22)')
+        self.assertEqual(conf_type2enc('bitmap')((1, 3)), '[1:3]')
         self.assertEqual(conf_type2enc('[NUMBER]')([3, 4]), '[2: 3, 4]')
         self.assertEqual(conf_type2enc('[STRING]')('abc'), '[3: "a", "b", "c"]')
         self.assertEqual(conf_type2enc('[fid]')([(11, 22)]), '[1: (11, 22)]')

sss/st
@@ -9,13 +9,14 @@ umask 0002
 ## CAUTION: This path will be removed by superuser.
 SANDBOX_DIR=${SANDBOX_DIR:-/var/mero/sandbox.sss-st}
 
-SERVER_TMID=1001
+SERVER_TMID=1
 CLIENT_TMID=1002
 SVC_IOS_FID='<7300000000000001:1>'
 CONFD_FID='<7300000000000001:2>'
 SVC_IOS_ID='(8286623314361712641, 1)'
 SVC_ID_NOENT='(8286623314361712641, 10)'
 SVC_ID=$SVC_IOS_ID
+PROC_ID='(0x7200000000000001, 5)'
 
 M0_CORE_DIR=`readlink -f $0`
 M0_CORE_DIR=${M0_CORE_DIR%/*/*}
@@ -52,6 +53,26 @@ sss_send() {
 		-d "($CMD, \"$SVC\", $ID, \"${SS_PARAM:-}\")" -v
 }
 
+sss_process_send() {
+	[ $# -eq 2 ] || die 'Invalid arguments'
+	local CMD=
+	case "$1" in  # see m0_ss_process_req_cmd for the values
+		M0_PROCESS_STOP) CMD=0;;
+		M0_PROCESS_RECONFIG) CMD=1;;
+		M0_PROCESS_HEALTH) CMD=2;;
+		M0_PROCESS_QUIESCE) CMD=3;;
+		M0_PROCESS_RUNNING_LIST) CMD=4;;
+		*) die 'Invalid value';;
+	esac
+	local ID="$2"
+
+	env ${TRACE_console:-} timeout 60 $M0_CORE_DIR/console/bin/m0console \
+		-f $(opcode M0_SSS_PROCESS_REQ_OPCODE) \
+		-s $(endpoint $SERVER_TMID) -c $(endpoint $CLIENT_TMID) \
+		-d "($CMD, $ID)" -v || true
+
+}
+
 svc_state() {
 	case "${1:-}" in  # see m0_reqh_service_state for the values
 		M0_RST_INITIALISED) echo 1;;
@@ -62,6 +83,7 @@ svc_state() {
 		_EINVAL)   echo -22;;
 		_EPROTO)   echo -71;;
 		_EALREADY) echo -114;;
+		OK) echo 0;;
 		*) die 'Invalid value';;
 	esac
 }
@@ -81,6 +103,28 @@ sss_check() {
 	[ "$ACTUAL" = "$EXPECTED" ] || \
 		die "Expected service state (${EXPECTED}) != actual (${ACTUAL})"
 	echo 'OK' >&2
+}
+
+sss_process_check() {
+	[ $# -eq 4 ] || die 'Invalid arguments'
+	local COMMAND="$1"
+	local PROCESS_ID="$2"
+	local KEY_NAME="$3"
+	local EXPECTED_VALUE="$4"
+
+	echo -n "$COMMAND $PROCESS_ID... " >&2
+	sss_process_send $COMMAND "$PROCESS_ID" &>$SANDBOX_DIR/console.log
+	local ACTUAL=$(awk "/$KEY_NAME/ {print \$3}" $SANDBOX_DIR/console.log)
+	local EXPECTED=$(svc_state $EXPECTED_VALUE)
+	[ "$ACTUAL" = "$EXPECTED" ] || \
+		die "Expected service state (${EXPECTED}) != actual (${ACTUAL})"
+	echo 'OK' >&2
+}
+
+sss_process_restart_check() {
+	local EXPECTED=$(grep Restarting $SANDBOX_DIR/m0d.log)
+	[ A"$EXPECTED" = A"Restarting" ] || die "Expected restarting state"
+	echo 'Restart OK' >&2
 }
 
 sandbox_init() {
@@ -155,6 +199,11 @@ esac
 $M0_CORE_DIR/conf/st insmod
 sandbox_init
 m0d_start
+
+sss_process_check M0_PROCESS_RECONFIG "$PROC_ID" sspr_rc OK
+# wait reconfigure
+sleep 5
+sss_process_restart_check
 
 sss_check ioservice M0_SERVICE_STATUS "$SVC_ID" ssr_state M0_RST_STARTED
 sss_check ioservice M0_SERVICE_STATUS "$SVC_ID_NOENT" ssr_rc _ENOENT  # invalid id

be/tx.c
@@ -638,7 +638,12 @@ M0_INTERNAL int m0_be_tx_mod_init(void)
 }
 
 M0_INTERNAL void m0_be_tx_mod_fini(void)
-{;}
+{
+	m0_sm_addb2_fini(&op_states_conf);
+	m0_sm_addb2_fini(&be_tx_sm_conf);
+	m0_sm_conf_fini(&op_states_conf);
+	m0_sm_conf_fini(&be_tx_sm_conf);
+}
 
 #undef BE_TX_LOCKED_AT_STATE
 

conf/helpers.c
@@ -62,6 +62,28 @@ M0_INTERNAL int m0_conf_fs_get(const char *profile,
 	return M0_RC(rc);
 }
 
+M0_INTERNAL int m0_conf_process_get(struct m0_confc         *confc,
+				    struct m0_fid           *fid,
+				    struct m0_conf_process **process)
+{
+	struct m0_conf_obj *obj;
+	int                 rc;
+
+	M0_PRE(confc != NULL);
+	M0_PRE(fid != NULL);
+
+	m0_conf_cache_lock(&confc->cc_cache);
+	rc = m0_conf_obj_find(&confc->cc_cache, fid, &obj);
+	m0_conf_cache_unlock(&confc->cc_cache);
+
+	if (rc == 0)
+		rc = m0_confc_open_sync(&obj, obj, M0_FID0);
+	if (rc == 0)
+		*process = M0_CONF_CAST(obj, m0_conf_process);
+
+	return M0_RC(rc);
+}
+
 M0_INTERNAL bool m0_obj_is_pver(const struct m0_conf_obj *obj)
 {
 	return m0_conf_obj_type(obj) == &M0_CONF_PVER_TYPE;

conf/helpers.h
@@ -22,14 +22,18 @@
 #ifndef __MERO_CONF_HELPERS_H__
 #define __MERO_CONF_HELPERS_H__
 
+#include "lib/tlist.h"
+
 /* import */
 struct m0_rpc_machine;
 struct m0_sm_group;
 struct m0_tl;
+struct m0_fid;
 struct m0_confc;
 struct m0_conf_filesystem;
 struct m0_conf_pver;
 struct m0_conf_obj;
+struct m0_conf_process;
 struct m0_conf_root;
 struct m0_conf_service;
 struct m0_rpc_session;
@@ -59,6 +63,13 @@ struct m0_confc_args {
 M0_INTERNAL int m0_conf_fs_get(const char                 *profile,
 			       struct m0_confc            *confc,
 			       struct m0_conf_filesystem **result);
+
+/**
+ * Obtains process object associated with given profile.
+ */
+M0_INTERNAL int m0_conf_process_get(struct m0_confc         *confc,
+				    struct m0_fid           *fid,
+				    struct m0_conf_process **process);
 
 /** Finds pool version which does not intersect with the given failure set. */
 M0_INTERNAL int m0_conf_poolversion_get(struct m0_conf_filesystem *fs,

conf/obj.c
@@ -425,5 +425,6 @@ M0_INTERNAL void m0_conf_obj_fini(void)
 	M0_CONF_OBJ_TYPES;
 #undef X_CONF
 	m0_fid_type_unregister(&M0_CONF_RELFID_TYPE);
+	m0_xcode_union_fini(m0_confx_obj_xc);
 }
 #undef M0_TRACE_SUBSYSTEM

conf/objs/process.c
@@ -46,26 +46,43 @@ static int process_decode(struct m0_conf_obj        *dest,
 	struct m0_conf_process        *d = M0_CONF_CAST(dest, m0_conf_process);
 	const struct m0_confx_process *s = XCAST(src);
 
-	d->pc_cores    = s->xr_cores;
-	d->pc_memlimit = s->xr_mem_limit;
+	rc = m0_bitmap_init(&d->pc_cores, s->xr_cores.bo_size);
+	if (rc != 0)
+		goto done;
+	m0_bitmap_load(&s->xr_cores, &d->pc_cores);
+
+	d->pc_memlimit_as      = s->xr_mem_limit_as;
+	d->pc_memlimit_rss     = s->xr_mem_limit_rss;
+	d->pc_memlimit_stack   = s->xr_mem_limit_stack;
+	d->pc_memlimit_memlock = s->xr_mem_limit_memlock;
 
 	rc = dir_new(cache, &dest->co_id, &M0_CONF_PROCESS_SERVICES_FID,
 		     &M0_CONF_SERVICE_TYPE, &s->xr_services, &d->pc_services);
 	if (rc == 0)
 		child_adopt(dest, &d->pc_services->cd_obj);
 
+done:
 	return M0_RC(rc);
 }
 
 static int
 process_encode(struct m0_confx_obj *dest, const struct m0_conf_obj *src)
 {
+	int                      rc;
 	struct m0_conf_process  *s = M0_CONF_CAST(src, m0_conf_process);
 	struct m0_confx_process *d = XCAST(dest);
 
 	confx_encode(dest, src);
-	d->xr_cores     = s->pc_cores;
-	d->xr_mem_limit = s->pc_memlimit;
+
+	rc = m0_bitmap_onwire_init(&d->xr_cores, s->pc_cores.b_nr);
+	if (rc != 0)
+		return M0_ERR(rc);
+	m0_bitmap_store(&s->pc_cores, &d->xr_cores);
+
+	d->xr_mem_limit_as      = s->pc_memlimit_as;
+	d->xr_mem_limit_rss     = s->pc_memlimit_rss;
+	d->xr_mem_limit_stack   = s->pc_memlimit_stack;
+	d->xr_mem_limit_memlock = s->pc_memlimit_memlock;
 	return M0_RC(arrfid_from_dir(&d->xr_services, s->pc_services));
 }
 
@@ -76,8 +93,10 @@ process_match(const struct m0_conf_obj *cached, const struct m0_confx_obj *flat)
 	const struct m0_conf_process  *obj =
 		M0_CONF_CAST(cached, m0_conf_process);
 
-	return  obj->pc_memlimit == xobj->xr_mem_limit &&
-		obj->pc_cores	 == xobj->xr_cores     &&
+	return  obj->pc_memlimit_as      == xobj->xr_mem_limit_as &&
+		obj->pc_memlimit_rss     == xobj->xr_mem_limit_rss &&
+		obj->pc_memlimit_stack   == xobj->xr_mem_limit_stack &&
+		obj->pc_memlimit_memlock == xobj->xr_mem_limit_memlock &&
 		m0_conf_dir_elems_match(obj->pc_services, &xobj->xr_services);
 }
 
@@ -100,6 +119,8 @@ static void process_delete(struct m0_conf_obj *obj)
 {
 	struct m0_conf_process *x = M0_CONF_CAST(obj, m0_conf_process);
 	m0_conf_process_bob_fini(x);
+	if (x->pc_cores.b_nr != 0)
+		m0_bitmap_fini(&x->pc_cores);
 	m0_free(x);
 }
 

commit 2860d632364caaae751e077071b87a45e7b72346

conf: fix the ST; add m0confgen to `m0 run-st'
conf/st:
  - Update conf description and fix the test, which was broken by commit
    1672c02 "Add Process reconfig functionality".
  - Remove kernel modules in case of m0confgen error.
  - Remove unneeded '\' after '||'.

utils/m0confgen:
  - Fix `--self-check'.
  - Delete "bitmap" parser.
    No special parser is needed for a sequence of numbers, which
    m0_bitmap_onwire (m0_confx_process::xr_cores) effectively is.

Add m0confgen self-checks to `m0 run-st'.

Change-Id: Ie3ac368ae87a8cc631f4c891a299a0c0843ad665

conf/obj.h
@@ -414,6 +414,11 @@ struct m0_conf_node {
 	struct m0_conf_pool *cn_pool;
 };
 
+/**
+ * Process configuration.
+ *
+ * @see m0_processors_online(), m0_proc_attr
+ */
 struct m0_conf_process {
 	struct m0_conf_obj  pc_obj;
 	struct m0_conf_dir *pc_services;

conf/st
@@ -65,7 +65,7 @@ sandbox_fini() {
 }
 
 modules_insert() {
-    insmod $M0_CORE_DIR/extra-libs/gf-complete/src/linux_kernel/m0gf.ko || \
+    insmod $M0_CORE_DIR/extra-libs/gf-complete/src/linux_kernel/m0gf.ko ||
 	die 'Inserting m0gf.ko failed'
 
     insmod $M0_CORE_DIR/mero/m0mero.ko \
@@ -120,10 +120,14 @@ confdb() {
     mdpool=pool-0 nodes=[node-0] pools=[pool-0] racks=[rack-0])
 (node-0 memsize=16000 nr_cpu=2 last_state=3 flags=2 pool_id=pool-0
     processes=[process-0, process-1, process-2, process-3])
-(process-0 mem_limit=4000 cores=2 services=[service-0])
-(process-1 mem_limit=4000 cores=2 services=[service-1])
-(process-2 mem_limit=4000 cores=1 services=[service-2])
-(process-3 mem_limit=4000 cores=2 services=[service-3, service-4])
+(process-0 cores=[2] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0 services=[service-0])
+(process-1 cores=[2] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0 services=[service-1])
+(process-2 cores=[1] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0 services=[service-2])
+(process-3 cores=[2] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+     mem_limit_memlock=0 services=[service-3, service-4])
 (service-0 type=5 endpoints=["$SSS_ENDPOINT"] sdevs=[])
 (service-1 type=4 endpoints=["$RMS_ENDPOINT"] sdevs=[])
 (service-2 type=1 endpoints=["$MDS_ENDPOINT"] sdevs=[sdev-0])
@@ -154,7 +158,8 @@ confdb() {
     mdpool=pool-10 nodes=[node-10] pools=[pool-10] racks=[])
 (node-10 memsize=16000 nr_cpu=2 last_state=3 flags=2 pool_id=pool-10
     processes=[process-10])
-(process-10 mem_limit=4000 cores=2 services=[service-10])
+(process-10 cores=[2] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0 services=[service-10])
 (service-10 type=2 endpoints=["$IOS1_ENDPOINT"] sdevs=[sdev-10])
 (sdev-10 iface=4 media=2 bsize=8192 size=0 last_state=2 flags=4
     filename="/dev/null")
@@ -198,7 +203,8 @@ services_start() {
 
     OPTS="$OPTS -f <0x7200000000000001:1>"
 
-    confdb | $M0_CORE_DIR/utils/m0confgen >$CONF_FILE
+    confdb | $M0_CORE_DIR/utils/m0confgen >$CONF_FILE ||
+	error 'm0confgen failed'
 
     echo "--- `date` ---" >>$SANDBOX_DIR/m0d.log
     cd $SANDBOX_DIR
@@ -206,14 +212,14 @@ services_start() {
     [ "$MODE" = autoconf ] || {
 	## m0mkfs should be executed only once. It is usually executed
 	## during cluster initial setup.
-	$M0_CORE_DIR/utils/mkfs/m0mkfs $OPTS >>$SANDBOX_DIR/mkfs.log || \
+	$M0_CORE_DIR/utils/mkfs/m0mkfs $OPTS >>$SANDBOX_DIR/mkfs.log ||
 	    error 'm0mkfs failed'
     }
 
     $M0_CORE_DIR/mero/m0d $OPTS >>$SANDBOX_DIR/m0d.log 2>&1 &
     local PID=$!
     sleep 1
-    kill -0 $PID 2>/dev/null || \
+    kill -0 $PID 2>/dev/null ||
 	error "Failed to start m0d. See $SANDBOX_DIR/m0d.log for details."
 }
 

scripts/m0
@@ -79,14 +79,17 @@ run_st() {
     say start/stop service ST
     $SUDO SANDBOX_DIR=$RUNDIR/sandbox.sss-st "$SRC/sss/st"
 
-    say conf ST
     if python -c 'import ply' 2>/dev/null; then
+	say m0confgen
+	cd $SRC; utils/m0confgen --self-check; cd -
+
+	say conf ST
 	$SUDO SANDBOX_DIR=$RUNDIR/sandbox.conf-st "$SRC/conf/st"
-    else # XXX `ply' may become mandatory, if utils/m0confgen gains popularity.
+    else
 	{
 	    cat <<"EOF"
-*WARNING* Unable to run the test due to unmet dependency.
-`ply' Python module is missing. Try to install it:
+*WARNING* Unable to run some of the tests due to unmet dependency.
+`ply' Python module is missing. Please install it:
     sudo yum install python-ply
 EOF
 	} >&2

ut/conf-str.txt.in
@@ -6,9 +6,11 @@
     nodes=[node-2, node-48] pools=[pool-4, pool-56] racks=[rack-3, rack-52])
 (node-2 memsize=16000 nr_cpu=2 last_state=3 flags=2 pool_id=pool-4
     processes=[process-5, process-6])
-(process-5 mem_limit=4000 cores=2
+(process-5 cores=[2] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0
     services=[service-9, service-10, service-20, service-21])
-(process-6 mem_limit=4000 cores=1 services=[])
+(process-6 cores=[1] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0 services=[])
 (service-9 type=4 endpoints=["0@lo:12345:34:1"]
     sdevs=[sdev-13, sdev-14])
 (service-10 type=6 endpoints=["0@lo:12345:34:1"] sdevs=[sdev-15])
@@ -34,7 +36,8 @@
 
 (node-48 memsize=16000 nr_cpu=2 last_state=3 flags=2 pool_id=pool-56
     processes=[process-49])
-(process-49 mem_limit=4000 cores=1 services=[service-50])
+(process-49 cores=[1] mem_limit_as=0 mem_limit_rss=0 mem_limit_stack=0
+    mem_limit_memlock=0 services=[service-50])
 (service-50 type=1 endpoints=["0@lo:12345:34:1"]
     sdevs=[sdev-51])
 (sdev-51 iface=4 media=1 bsize=4096 size=596000000000 last_state=3 flags=4

utils/m0confgen
@@ -35,7 +35,6 @@ input_format = """Input format:
     attr    = ID "=" ( NUMBER | STRING | fid | objid | list )
     fid     = "(" NUMBER "," NUMBER ")"
     list    = "[" [ csv(NUMBER) | csv(STRING) | csv(fid) | csv(objid) ] "]"
-    bitmap  = "[NUMBER : NUMBER]"
     objtype = { %(objtypes)s }
 
 Examples:
@@ -70,8 +69,8 @@ conf_obj_types = {
     'objv': ('j', 'real=objid children=[objid]'),
     'node': ('n', 'memsize=NUMBER nr_cpu=NUMBER last_state=NUMBER flags=NUMBER'
              ' pool_id=objid processes=[objid]'),
-    'process': ('r', 'mem_limit_as=NUMBER mem_limit_rss=NUMBER'
-                ' mem_limit_stack=NUMBER mem_limit_memlock=NUMBER cores=bitmap'
+    'process': ('r', 'cores=[NUMBER] mem_limit_as=NUMBER mem_limit_rss=NUMBER'
+                ' mem_limit_stack=NUMBER mem_limit_memlock=NUMBER'
                 ' services=[objid]'),
     'service': ('s', 'type=NUMBER endpoints=[STRING] sdevs=[objid]'),
     'sdev': ('d', 'iface=NUMBER media=NUMBER bsize=NUMBER size=NUMBER'
@@ -120,8 +119,7 @@ def conf_type2enc(t):
         'NUMBER': str,
         'STRING': quote,
         'fid': repr,
-        'objid': conf_objid_encode,
-        'bitmap': repr
+        'objid': conf_objid_encode
     }[t]
     return list_encode_with(enc) if list_p else enc
 
@@ -214,10 +212,6 @@ def p_fid(p):
     "fid : '(' NUMBER ',' NUMBER ')'"
     p[0] = p[2], p[4]
 
-def p_bitmap(p):
-    "bitmap : '[' NUMBER ':' NUMBER ']'"
-    p[0] = p[2], p[4]
-
 def p_attrs(p):
     """
     attrs : attr
@@ -265,7 +259,7 @@ parser = yacc.yacc(debug=False)
 ### Tests
 
 class TestParser(unittest.TestCase):
-    def test_obj_profile(self):
+    def test_parse(self):
         self.assertEqual(parser.parse(''), [])
         self.assertEqual(parser.parse('(profile-0 filesystem=filesystem-1)'),
                          [(('profile', 0), dict(filesystem=('filesystem', 1)))])
@@ -298,15 +292,14 @@ class TestParser(unittest.TestCase):
                               nodes=[('node', 2)],
                               pools=[('pool', 4)],
                               racks=[('rack', 3)]))])
-        self.assertEqual(21, len(parser.parse(
-            file('conf/ut/conf-str.txt.in').read())))
+        self.assertEqual(37,
+                         len(parser.parse(file('ut/conf-str.txt.in').read())))
 
 class TestEncoders(unittest.TestCase):
     def test_type2enc(self):
         self.assertEqual(conf_type2enc('NUMBER')(42), '42')
         self.assertEqual(conf_type2enc('STRING')('abc'), '"abc"')
         self.assertEqual(conf_type2enc('fid')((11, 22)), '(11, 22)')
-        self.assertEqual(conf_type2enc('bitmap')((1, 3)), '[1:3]')
         self.assertEqual(conf_type2enc('[NUMBER]')([3, 4]), '[2: 3, 4]')
         self.assertEqual(conf_type2enc('[STRING]')('abc'), '[3: "a", "b", "c"]')
         self.assertEqual(conf_type2enc('[fid]')([(11, 22)]), '[1: (11, 22)]')
@@ -350,6 +343,9 @@ class TestEncoders(unittest.TestCase):
  [1: (0x6100000000000001, 3)])},
  {0x6a| (((0x6a00000000000001, 19)), (0x6b00000000000001, 16),
  [0])}]""".replace('\n', ''))
+
+    def test_file(self):
+        conf_objs_encode(parser.parse(file('ut/conf-str.txt.in').read()))
 
 ### ------------------------------------------------------------------
 ### main