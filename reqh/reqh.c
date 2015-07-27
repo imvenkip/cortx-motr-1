imvenkip/cortx-motr-1

commit 0a3aee95cfa845fa349babc13b4596c92ea983ff

mgmt: get rid of mgmt/
* Management service is obsoleted in favour of "sss" service
  (sss = start/stop/status), which is due to be landed soon.
* m0d cannot be bootstrapped from genders file any more.
  Bootstrapping logic was a heavy user of mgmt API.

Acked-by: Nikita Danilov
Change-Id: I0ce3095e0ddd4334705dbf0bbaa1cfd0094428bf

reqh/reqh.c
@@ -42,7 +42,6 @@
 #include "reqh/reqh_service.h"
 #include "reqh/reqh.h"
 #include "layout/pdclust.h"
-#include "mgmt/mgmt.h"
 
 #include "be/ut/helper.h"
 
@@ -83,16 +82,8 @@ static struct m0_sm_state_descr m0_reqh_sm_descr[] = {
         [M0_REQH_ST_INIT] = {
                 .sd_flags       = M0_SDF_INITIAL | M0_SDF_FINAL,
                 .sd_name        = "Init",
-                .sd_allowed     = M0_BITS(M0_REQH_ST_MGMT_STARTED,
-					  M0_REQH_ST_NORMAL,
-					  M0_REQH_ST_SVCS_STOP)
-        },
-        [M0_REQH_ST_MGMT_STARTED] = {
-                .sd_flags       = 0,
-                .sd_name        = "ManagementStart",
                 .sd_allowed     = M0_BITS(M0_REQH_ST_NORMAL,
-					  M0_REQH_ST_SVCS_STOP,
-					  M0_REQH_ST_STOPPED)
+					  M0_REQH_ST_SVCS_STOP)
         },
         [M0_REQH_ST_NORMAL] = {
                 .sd_flags       = 0,
@@ -108,12 +99,6 @@ static struct m0_sm_state_descr m0_reqh_sm_descr[] = {
         [M0_REQH_ST_SVCS_STOP] = {
                 .sd_flags       = 0,
                 .sd_name        = "ServicesStop",
-                .sd_allowed     = M0_BITS(M0_REQH_ST_MGMT_STOP,
-					  M0_REQH_ST_STOPPED)
-        },
-        [M0_REQH_ST_MGMT_STOP] = {
-                .sd_flags       = 0,
-                .sd_name        = "ManagementStop",
                 .sd_allowed     = M0_BITS(M0_REQH_ST_STOPPED)
         },
         [M0_REQH_ST_STOPPED] = {
@@ -136,7 +121,6 @@ M0_INTERNAL bool m0_reqh_invariant(const struct m0_reqh *reqh)
 {
 	return	reqh != NULL &&
 		ergo(M0_IN(reqh->rh_sm.sm_state,(M0_REQH_ST_INIT,
-						 M0_REQH_ST_MGMT_STARTED,
 						 M0_REQH_ST_NORMAL)),
 		     reqh->rh_mdstore != NULL) &&
 		     m0_fom_domain_invariant(&reqh->rh_fom_dom);
@@ -332,17 +316,15 @@ M0_INTERNAL int m0_reqh_services_state_count(struct m0_reqh *reqh, int state)
 	struct m0_reqh_service *svc;
 
 	M0_PRE(reqh != NULL);
-	M0_PRE(M0_IN(m0_reqh_state_get(reqh),
-		     (M0_REQH_ST_MGMT_STARTED, M0_REQH_ST_NORMAL,
-		      M0_REQH_ST_DRAIN, M0_REQH_ST_SVCS_STOP)));
-
+	M0_PRE(M0_IN(m0_reqh_state_get(reqh), (M0_REQH_ST_NORMAL,
+					       M0_REQH_ST_DRAIN,
+					       M0_REQH_ST_SVCS_STOP)));
 	m0_rwlock_read_lock(&reqh->rh_rwlock);
 	m0_tl_for(m0_reqh_svc, &reqh->rh_services, svc) {
 		if (m0_reqh_service_state_get(svc) == state)
 			++cnt;
 	} m0_tl_endfor;
 	m0_rwlock_read_unlock(&reqh->rh_rwlock);
-
 	return cnt;
 }
 
@@ -358,14 +340,12 @@ M0_INTERNAL int m0_reqh_fop_allow(struct m0_reqh *reqh, struct m0_fop *fop)
 	rh_st = m0_reqh_state_get(reqh);
 	if (rh_st == M0_REQH_ST_INIT)
 		return -EAGAIN;
-	if (rh_st == M0_REQH_ST_MGMT_STOP || rh_st == M0_REQH_ST_STOPPED)
+	if (rh_st == M0_REQH_ST_STOPPED)
 		return -ESHUTDOWN;
 
 	svc = m0_reqh_service_find(fop->f_type->ft_fom_type.ft_rstype, reqh);
 	if (svc == NULL) {
-		if (rh_st == M0_REQH_ST_MGMT_STARTED)
-			return -EAGAIN;
-		/**
+		/** XXX FIXME
 		 * @todo temporary allow serviceless foms for UT.
 		 *
 		 * Specifically, rm clients should start a service to accept
@@ -392,13 +372,8 @@ M0_INTERNAL int m0_reqh_fop_allow(struct m0_reqh *reqh, struct m0_fop *fop)
 		    svc->rs_ops->rso_fop_accept != NULL)
 			return (*svc->rs_ops->rso_fop_accept)(svc, fop);
 		return -ESHUTDOWN;
-	case M0_REQH_ST_MGMT_STARTED:
 	case M0_REQH_ST_SVCS_STOP:
-		if (svc == reqh->rh_mgmt_svc) {
-			M0_ASSERT(svc->rs_ops->rso_fop_accept != NULL);
-			return (*svc->rs_ops->rso_fop_accept)(svc, fop);
-		}
-		return rh_st == M0_REQH_ST_MGMT_STARTED ? -EAGAIN : -ESHUTDOWN;
+		return rh_st == -ESHUTDOWN;
 	default:
 		return -ENOSYS;
 	};
@@ -544,10 +519,8 @@ M0_INTERNAL void m0_reqh_pre_storage_fini_svcs_stop(struct m0_reqh *reqh)
 	M0_PRE(reqh != NULL);
         m0_rwlock_write_lock(&reqh->rh_rwlock);
 	M0_PRE(m0_reqh_invariant(reqh));
-	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_DRAIN ||
-	       m0_reqh_state_get(reqh) == M0_REQH_ST_NORMAL ||
-	       m0_reqh_state_get(reqh) == M0_REQH_ST_MGMT_STARTED ||
-	       m0_reqh_state_get(reqh) == M0_REQH_ST_INIT);
+	M0_PRE(M0_IN(m0_reqh_state_get(reqh),
+		     (M0_REQH_ST_DRAIN, M0_REQH_ST_NORMAL, M0_REQH_ST_INIT)));
 
 	reqh_state_set(reqh, M0_REQH_ST_SVCS_STOP);
 	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_SVCS_STOP);
@@ -556,17 +529,13 @@ M0_INTERNAL void m0_reqh_pre_storage_fini_svcs_stop(struct m0_reqh *reqh)
 	__reqh_svcs_stop(reqh, 2);
 
         m0_rwlock_write_lock(&reqh->rh_rwlock);
-	if (reqh->rh_mgmt_svc != NULL)
-		reqh_state_set(reqh, M0_REQH_ST_MGMT_STOP);
-	else
-		reqh_state_set(reqh, M0_REQH_ST_STOPPED);
+	reqh_state_set(reqh, M0_REQH_ST_STOPPED);
         m0_rwlock_write_unlock(&reqh->rh_rwlock);
 }
 
 M0_INTERNAL void m0_reqh_post_storage_fini_svcs_stop(struct m0_reqh *reqh)
 {
-	M0_PRE(M0_IN(m0_reqh_state_get(reqh), (M0_REQH_ST_MGMT_STOP,
-					       M0_REQH_ST_STOPPED)));
+	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_STOPPED);
 	__reqh_svcs_stop(reqh, 1);
 }
 
@@ -576,75 +545,10 @@ M0_INTERNAL void m0_reqh_start(struct m0_reqh *reqh)
         m0_rwlock_write_lock(&reqh->rh_rwlock);
 	M0_PRE(m0_reqh_invariant(reqh));
 
-	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_MGMT_STARTED ||
-	       m0_reqh_state_get(reqh) == M0_REQH_ST_INIT);
+	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_INIT);
 	reqh_state_set(reqh, M0_REQH_ST_NORMAL);
 
         m0_rwlock_write_unlock(&reqh->rh_rwlock);
-}
-
-M0_INTERNAL int m0_reqh_mgmt_service_start(struct m0_reqh *reqh)
-{
-	struct m0_reqh_service *service;
-	int                     rc;
-
-	M0_PRE(reqh != NULL);
-        m0_rwlock_write_lock(&reqh->rh_rwlock);
-	M0_PRE(m0_reqh_invariant(reqh));
-
-	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_INIT);
-	M0_PRE(reqh->rh_mgmt_svc == NULL);
-
-	reqh_state_set(reqh, M0_REQH_ST_MGMT_STARTED);
-	rc = m0_mgmt_service_allocate(&service);
-	if (rc != 0)
-		goto allocate_failed;
-	m0_reqh_service_init(service, reqh, NULL);
-	reqh->rh_mgmt_svc = service;
-
-	m0_rwlock_write_unlock(&reqh->rh_rwlock);
-
-	rc = m0_reqh_service_start(service);
-	m0_rwlock_write_lock(&reqh->rh_rwlock);
-
-	if (rc != 0)
-		goto start_failed;
-	M0_POST(reqh->rh_mgmt_svc == service);
-	M0_POST(m0_reqh_state_get(reqh) == M0_REQH_ST_MGMT_STARTED);
- done:
-	m0_rwlock_write_unlock(&reqh->rh_rwlock);
-	return rc;
-
- start_failed:
-	reqh->rh_mgmt_svc = NULL;
-	m0_reqh_service_fini(service);
- allocate_failed:
-	reqh_state_set(reqh, M0_REQH_ST_STOPPED);
-	M0_POST(reqh->rh_mgmt_svc == NULL);
-	M0_POST(rc != 0);
-	goto done;
-}
-
-M0_INTERNAL void m0_reqh_mgmt_service_stop(struct m0_reqh *reqh)
-{
-	struct m0_reqh_service *service;
-
-	M0_PRE(reqh != NULL);
-        m0_rwlock_write_lock(&reqh->rh_rwlock);
-	M0_PRE(m0_reqh_invariant(reqh));
-
-	M0_PRE(m0_reqh_state_get(reqh) == M0_REQH_ST_MGMT_STOP);
-	service = reqh->rh_mgmt_svc;
-	reqh_state_set(reqh, M0_REQH_ST_STOPPED);
-        m0_rwlock_write_unlock(&reqh->rh_rwlock);
-
-	if (service != NULL) {
-		m0_reqh_service_prepare_to_stop(service);
-		m0_reqh_idle_wait(reqh); /* drain mgmt fops */
-		m0_reqh_service_stop(service);
-		m0_reqh_service_fini(service);
-		reqh->rh_mgmt_svc = NULL;
-	}
 }
 
 M0_INTERNAL void m0_reqh_stats_post_addb(struct m0_reqh *reqh)
@@ -677,7 +581,6 @@ M0_INTERNAL void m0_reqh_stats_post_addb(struct m0_reqh *reqh)
 M0_INTERNAL uint64_t m0_reqh_nr_localities(const struct m0_reqh *reqh)
 {
 	M0_PRE(m0_reqh_invariant(reqh));
-
 	return reqh->rh_fom_dom.fd_localities_nr;
 }
 

Kbuild.in
@@ -73,7 +73,6 @@ include $(src)/ioservice/Kbuild.sub
 include $(src)/layout/Kbuild.sub
 include $(src)/lib/Kbuild.sub
 include $(src)/mdservice/Kbuild.sub
-include $(src)/mgmt/Kbuild.sub
 include $(src)/module/Kbuild.sub
 include $(src)/net/Kbuild.sub
 include $(src)/net/bulk_emulation/Kbuild.sub

Makefile.am
@@ -194,7 +194,6 @@ include $(top_srcdir)/m0t1fs/Makefile.sub
 include $(top_srcdir)/mdservice/Makefile.sub
 include $(top_srcdir)/mdstore/Makefile.sub
 include $(top_srcdir)/mero/Makefile.sub
-include $(top_srcdir)/mgmt/Makefile.sub
 include $(top_srcdir)/module/Makefile.sub
 include $(top_srcdir)/mw/Makefile.sub
 include $(top_srcdir)/net/Makefile.sub
@@ -246,8 +245,6 @@ include $(top_srcdir)/layout/ut/Makefile.sub
 include $(top_srcdir)/lib/ut/Makefile.sub
 include $(top_srcdir)/mdservice/ut/Makefile.sub
 include $(top_srcdir)/mero/ut/Makefile.sub
-include $(top_srcdir)/mgmt/svc/ut/Makefile.sub
-include $(top_srcdir)/mgmt/ut/Makefile.sub
 include $(top_srcdir)/module/ut/Makefile.sub
 include $(top_srcdir)/net/bulk_emulation/ut/Makefile.sub
 include $(top_srcdir)/net/lnet/ut/Makefile.sub
@@ -636,16 +633,6 @@ addb_dump_m0addbdump_CPPFLAGS  = -DM0_TARGET='m0addbdump' $(AM_CPPFLAGS)
 addb_dump_m0addbdump_LDADD     = $(top_builddir)/mero/libmero.la
 
 include $(top_srcdir)/addb/dump/Makefile.sub
-
-#XXX_BE_DB #
-#XXX_BE_DB # mgmt/ctl/m0ctl
-#XXX_BE_DB # (This is unrelated to utils/linux_kernel/ctl.c).
-#XXX_BE_DB #
-#XXX_BE_DB
-#XXX_BE_DB bin_PROGRAMS               += mgmt/ctl/m0ctl
-#XXX_BE_DB mgmt_ctl_m0ctl_LDADD        = $(top_builddir)/mero/libmero.la
-#XXX_BE_DB
-#XXX_BE_DB include $(top_srcdir)/mgmt/ctl/Makefile.sub
 
 #
 # stats/util/m0stats
@@ -1149,7 +1136,6 @@ EXTRA_DIST += addb/Kbuild.sub \
               m0t1fs/linux_kernel/ut/Kbuild.sub \
               mdservice/Kbuild.sub \
               mero/Kbuild.sub \
-              mgmt/Kbuild.sub \
               module/Kbuild.sub \
               net/Kbuild.sub \
               net/bulk_emulation/Kbuild.sub \

doc/addb-ids
@@ -69,12 +69,6 @@ value          enum                    variable  name              file
 1400-1499                                                          fop/fop_addb.h
   1400 M0_ADDB_CTXID_FOP_MOD           m0_addb_ct_fop_mod
   1420 M0_ADDB_CTXID_FOM_LOCALITY      m0_addb_ct_fom_locality
-1500-1599                                                          mgmt/mgmt_addb.h
-  1500 M0_ADDB_CTXID_MGMT_MOD          m0_addb_ct_mgmt_mod
-  1501 M0_ADDB_CTXID_MGMT_SERVICE      m0_addb_ct_mgmt_service
-  1502 M0_ADDB_CTXID_MGMT_FOM_SS       m0_addb_ct_mgmt_fom_ss
-  1503 M0_ADDB_CTXID_MGMT_CTL          m0_addb_ct_mgmt_ctl
-  1504 M0_ADDB_CTXID_MGMT_FOM_RUN      m0_addb_ct_mgmt_fom_run
 1600-1699
    1600 M0_ADDB_CTXID_RM_MOD           m0_addb_ct_rm_mod           rm/rm_addb.h
    1601 M0_ADDB_CTXID_RMS_SERV         m0_addb_ct_rms_serv
@@ -163,7 +157,6 @@ value            enum                 variable name         file
 1200-1299							   db/db_addb.h
 1300-1399							   stob/stob_addb.h
 1400-1499							   fop/fop_addb.h
-1500-1599                                                          mgmt/mgmt_addb.h
 1600-1699                                                          rm/rm_addb.h
   1600 M0_ADDB_RECID_RM_BORROW_RATE      m0_addb_rt_rm_borrow_rate
   1601 M0_ADDB_RECID_RM_REVOKE_RATE      m0_addb_rt_rm_revoke_rate

doc/dld-index.c
@@ -28,7 +28,6 @@ file.c -->
 - @subpage Layout-DB "Layout DB DLD" <!-- layout/layout_db.c -->
 - @subpage LNetDLD "LNet Transport DLD" <!-- net/lnet/lnet_main.c -->
 - @subpage net-test "Mero Network Benchmark" <!-- net/test/main.c -->
-- @subpage MGMT-DLD "Management Interface Design" <!-- mgmt/mgmt.c -->
 - @subpage m0t1fs "M0T1FS detailed level design specification" <!-- m0t1fs/linux_kernel/m0t1fs.h -->
 - @subpage rpc-layer-core-dld "RPC layer core DLD" <!-- rpc/rpc.h -->
 - @subpage m0loop-dld "The new m0loop device driver DLD" <!-- m0t1fs/linux_kernel/m0loop.c -->

mero/conf.c
@@ -18,18 +18,17 @@
  * Original creation date: 07-Dec-2012
  */
 
-#include "lib/errno.h"
-#include "lib/memory.h"
 #define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_M0D
 #include "lib/trace.h"
 
+#include "lib/errno.h"
+#include "lib/memory.h"
 #include "lib/string.h"           /* m0_strdup */
 #include "mero/setup.h"           /* cs_args */
 #include "rpc/rpclib.h"           /* m0_rpc_client_ctx */
 #include "conf/obj.h"             /* m0_conf_filesystem */
 #include "conf/confc.h"           /* m0_confc */
 #include "conf/schema.h"          /* m0_conf_service_type */
-#include "mgmt/mgmt.h"            /* m0_mgmt_conf */
 
 /* ----------------------------------------------------------------
  * Mero options
@@ -286,94 +285,6 @@ xprt:
 	m0_net_xprt_fini(xprt);
 
 	return M0_RC(rc);
-}
-
-M0_INTERNAL int cs_genders_to_args(struct cs_args *args, const char *argv0,
-				   const char *genders)
-{
-	struct m0_mgmt_conf            conf;
-	struct m0_mgmt_node_conf       node;
-	struct m0_mgmt_svc_conf       *svc;
-	struct m0_mgmt_service_ep_conf sep;
-	char                           nbuf[16];
-	char                          *bp;
-	size_t                         l;
-	int                            i;
-	int                            rc;
-
-	M0_PRE(args != NULL && args->ca_argc == 0);
-	rc = m0_mgmt_conf_init(&conf, genders);
-	if (rc != 0)
-		return rc;
-	rc = m0_mgmt_node_get(&conf, NULL, &node);
-	if (rc != 0)
-		goto error;
-
-	/* NB: allocation failures checked at end of block */
-	option_add(args, m0_strdup(argv0));
-	l = strlen(node.mnc_m0d_ep) + strlen(m0_net_lnet_xprt.nx_name) + 2;
-	bp = m0_alloc(l);
-	if (bp != NULL)
-		sprintf(bp, "%s:%s", m0_net_lnet_xprt.nx_name, node.mnc_m0d_ep);
-	option_add(args, m0_strdup("-e"));
-	option_add(args, bp);
-	if (node.mnc_max_rpc_msg != 0) {
-		option_add(args, m0_strdup("-m"));
-		i = snprintf(nbuf, sizeof nbuf, "%lu", node.mnc_max_rpc_msg);
-		if (i >= sizeof nbuf) {
-			rc = -EINVAL;
-			goto done;
-		}
-		option_add(args, m0_strdup(nbuf));
-	}
-	if (node.mnc_recvq_min_len != 0) {
-		option_add(args, m0_strdup("-q"));
-		i = snprintf(nbuf, sizeof nbuf, "%u", node.mnc_recvq_min_len);
-		if (i >= sizeof nbuf) {
-			rc = -EINVAL;
-			goto done;
-		}
-		option_add(args, m0_strdup(nbuf));
-	}
-
-	rc = m0_mgmt_service_ep_get(&conf, service_name[M0_CST_MDS], &sep);
-	if (rc == -ENOENT) {
-		rc = 0;
-	} else if (rc != 0) {
-		goto done;
-	} else {
-		M0_ASSERT(sep.mse_ep_nr > 0);
-		/** @todo use HA or something to determine correct instance */
-		option_add(args, m0_strdup("-G"));
-		l = strlen(sep.mse_ep[0]) +
-		    strlen(m0_net_lnet_xprt.nx_name) + 2;
-		bp = m0_alloc(l);
-		if (bp != NULL)
-			sprintf(bp, "%s:%s",
-				m0_net_lnet_xprt.nx_name, sep.mse_ep[0]);
-		option_add(args, bp);
-		m0_mgmt_service_ep_free(&sep);
-	}
-	m0_tl_for(m0_mgmt_conf, &node.mnc_svc, svc) {
-		option_add(args, m0_strdup("-s"));
-		l = strlen(svc->msc_name) + strlen(svc->msc_uuid) + 2;
-		bp = m0_alloc(l);
-		if (bp != NULL)
-			sprintf(bp, "%s:%s", svc->msc_name, svc->msc_uuid);
-		option_add(args, bp);
-		for (i = 0; i < svc->msc_argc; ++i)
-			option_add(args, m0_strdup(svc->msc_argv[i]));
-	} m0_tlist_endfor;
-	/* detect any earlier memory allocation failures */
-	for (i = 0; i < args->ca_argc && rc == 0; ++i) {
-		if (args->ca_argv[i] == NULL)
-			rc = -ENOMEM;
-	}
-done:
-	m0_mgmt_node_free(&node);
-error:
-	m0_mgmt_conf_fini(&conf);
-	return rc;
 }
 
 /* ----------------------------------------------------------------

mero/init.c
@@ -76,7 +76,6 @@
 #include "cm/cm.h"
 #include "addb/addb_fops.h"
 #include "stats/stats_fops.h"
-#include "mgmt/mgmt.h"
 #include "ha/epoch.h"
 #include "module/instance.h"  /* m0_instance_init */
 
@@ -169,7 +168,6 @@ struct init_fini_call subsystem[] = {
 	{ &m0_mdstore_mod_init, &m0_mdstore_mod_fini, "mdstore" },
 	{ &m0_stats_svc_init,   &m0_stats_svc_fini,   "stats-service" },
 #endif /* __KERNEL__ */
-	{ &m0_mgmt_init,        &m0_mgmt_fini,        "mgmt" },
 	{ &m0_parity_init,      &m0_parity_fini,      "parity_math" },
 	{ &m0_dtm_global_init,  &m0_dtm_global_fini,  "dtm" }
 };

mero/magic.h
@@ -471,22 +471,6 @@ enum m0_magic_satchel {
 	/* m0_layout_linear_enum::lla_magic (boldface blob) */
 	M0_LAYOUT_LINEAR_ENUM_MAGIC = 0x33b01dfaceb10b77,
 
-/* MGMT */
-	/* mgmt_svc::ms_magic (bald dada casa) */
-	M0_MGMT_SVC_MAGIC = 0x33ba1ddadaca5a77,
-
-	/* mgmt_fop_ss_fom_bob (obese seafood) */
-	M0_MGMT_FOP_SS_FOM_MAGIC = 0x330be5e5eaf00d77,
-
-	/* mgmt_fop_run_fom_bob (obese caboose) */
-	M0_MGMT_FOP_RUN_FOM_MAGIC = 0x330be5ecab005e77,
-
-	/* m0_mgmt_svc_conf::msc_magic (calico saddle) */
-	M0_MGMT_SVC_CONF_MAGIC = 0x33ca11c05add1e77,
-
-	/* m0_mgmt_node_conf::mnc_svc magic (calico lilacs) */
-	M0_MGMT_NODE_CONF_MAGIC = 0x33ca11c0111ac577,
-
 /* Net */
 	/* m0_net_domain::nd_magix (acidic access) */
 	M0_NET_DOMAIN_MAGIC = 0x33ac1d1cacce5577,

commit 697b9f5c32214580e68e391692b03ce3da3524ca

reqh (m0_reqh_fop_handle): let error code through
* m0_reqh_fop_handle(): Do not change the error returned by m0_reqh_fop_allow().
* m0_reqh_fop_allow(): Return -ESHUTDOWN in "M0_REQH_ST_SVCS_STOP" case.
  (Fixes regression introduced by 0a3aee95.)
* m0_reqh_fop_allow(): s/EBUSY/EAGAIN/.

Closes: MERO-988
Reviewed-by: Rajanikant Chirmade <rajanikant.chirmade@seagate.com>

Change-Id: I86051fa64a9494e84966303aec24a33895d83892

reqh/reqh.c
@@ -420,9 +420,11 @@ M0_INTERNAL int m0_reqh_fop_allow(struct m0_reqh *reqh, struct m0_fop *fop)
 	stype = fop->f_type->ft_fom_type.ft_rstype;
 	if (stype == NULL)
 		return M0_ERR(-ENOSYS);
+
 	svc = m0_reqh_service_find(stype, reqh);
 	if (svc == NULL)
 		return M0_ERR(-ECONNREFUSED);
+
 	rh_st = m0_reqh_state_get(reqh);
 	if (rh_st == M0_REQH_ST_INIT) {
 		/*
@@ -430,7 +432,7 @@ M0_INTERNAL int m0_reqh_fop_allow(struct m0_reqh *reqh, struct m0_fop *fop)
 		 * startup.
 		 */
 		if (svc != NULL && stype == &m0_rpc_service_type)
-			return 0;
+			return M0_RC(0);
 		return M0_ERR(-EAGAIN);
 	}
 	if (rh_st == M0_REQH_ST_STOPPED)
@@ -442,11 +444,11 @@ M0_INTERNAL int m0_reqh_fop_allow(struct m0_reqh *reqh, struct m0_fop *fop)
 	switch (rh_st) {
 	case M0_REQH_ST_NORMAL:
 		if (svc_st == M0_RST_STARTED)
-			return 0;
+			return M0_RC(0);
 		if (svc_st == M0_RST_STARTING)
-			return M0_ERR(-EBUSY);
-		else if (svc_st == M0_RST_STOPPING &&
-			 svc->rs_ops->rso_fop_accept != NULL)
+			return M0_ERR(-EAGAIN);
+		if (svc_st == M0_RST_STOPPING &&
+		    svc->rs_ops->rso_fop_accept != NULL)
 			return (*svc->rs_ops->rso_fop_accept)(svc, fop);
 		return M0_ERR(-ESHUTDOWN);
 	case M0_REQH_ST_DRAIN:
@@ -455,12 +457,11 @@ M0_INTERNAL int m0_reqh_fop_allow(struct m0_reqh *reqh, struct m0_fop *fop)
 			return (*svc->rs_ops->rso_fop_accept)(svc, fop);
 		return M0_ERR(-ESHUTDOWN);
 	case M0_REQH_ST_SVCS_STOP:
-		return rh_st == -ESHUTDOWN;
+		return M0_ERR(-ESHUTDOWN);
 	default:
 		return M0_ERR(-ENOSYS);
-	};
+	}
 }
-
 
 static int disallowed_fop_tick(struct m0_fom *fom, void *data, int *phase)
 {
@@ -480,7 +481,6 @@ static int disallowed_fop_tick(struct m0_fom *fom, void *data, int *phase)
 		}
 		m0_rpc_reply_post(&reply->ffr_fop->f_item, &fop->f_item);
 	}
-
 	return -1;
 }
 
@@ -536,8 +536,7 @@ M0_INTERNAL int m0_reqh_fop_handle(struct m0_reqh *reqh, struct m0_fop *fop)
 			    m0_reqh_service_find(rst, reqh) == NULL)
 				return M0_ERR_INFO(-ESHUTDOWN,
 						   "Service shutdown.");
-
-			fop_disallowed(reqh, fop, M0_ERR(-ESHUTDOWN));
+			fop_disallowed(reqh, fop, M0_ERR(rc));
 		}
 		/*
 		 * Note :

reqh/ut/reqh_fop_allow_ut.c
@@ -195,7 +195,7 @@ static void fop_allow_test(void)
 	ut_sss_req(ds1_service_type.rst_name, M0_SERVICE_STATUS, -ENOENT, 0);
 
 	rc = send_fop();
-	M0_UT_ASSERT(rc == -ESHUTDOWN);
+	M0_UT_ASSERT(rc == -ECONNREFUSED);
 
 	ut_sss_req(ds1_service_type.rst_name,
 		   M0_SERVICE_INIT, 0,  M0_RST_INITIALISED);