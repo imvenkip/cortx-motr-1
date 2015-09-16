ba644612bfe86c79d18bbde7f235c5d8ab7261c2
Igor Perelyotov <igor.m.perelyotov@seagate.com>
2015-09-16 16:06:06 +0300

    spiel: test IO after process reconfig
    * Add '-w' option to disable warnings when building 'mero' Python module.
    
    * Add paramters to mkfs and m0d.
    
    * remove sss and stats services from configuration database.
    
    * Setup loop devices before start of the test.
    
    * Uncomment IO operations in the test.
    
    Change-Id: I4d83fe2414d0937440b459618e446a67fdb888cb

--- utils/m0spiel/setup.py
+++ utils/m0spiel/setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup, Extension
 mero = Extension('mero',
                  define_macros=[('M0_INTERNAL', ''), ('M0_EXTERN', 'extern')],
                  include_dirs=['../../', '../../extra-libs/galois/include/'],
-                 sources=['mero.c'])
+                 sources=['mero.c'],
+                 extra_compile_args = ['-w'])
 
 
 setup(name='mero', version='1.0',
