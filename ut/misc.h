dff97e48f1d05c92826d5807bc584bed9bfc559a
Maxim Medved <max.medved@seagate.com>
2015-09-18 21:00:49 +0300

    ut/misc: add m0_ut_random_{shuffle,arr_with_sum}()
    Change-Id: I0a8f7bc1c65f27df6b28976589efee99a18f57fc

---
+++ ut/misc.h
@@ -0,0 +1,72 @@
+/* -*- C -*- */
+/*
+ * COPYRIGHT 2015 XYRATEX TECHNOLOGY LIMITED
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF XYRATEX TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF XYRATEX TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF XYRATEX LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF XYRATEX'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A XYRATEX REPRESENTATIVE
+ * http://www.xyratex.com/contact
+ *
+ * Original author: Maxim Medved <max.medved@seagate.com>
+ * Original creation date: 26-Sep-2015
+ */
+
+#pragma once
+
+#ifndef __MERO_UT_MISC_H__
+#define __MERO_UT_MISC_H__
+
+#include "lib/types.h"  /* uint64_t */
+
+/**
+ * @defgroup ut
+ *
+ * @{
+ */
+
+/**
+ * Random shuffles an array.
+ * Uses seed parameter as the seed for RNG.
+ *
+ * @note It uses an UT-grade RNG.
+ */
+M0_INTERNAL void m0_ut_random_shuffle(uint64_t *arr,
+				      uint64_t  nr,
+				      uint64_t *seed);
+
+/**
+ * Gives an array with random values with the given sum.
+ *
+ * @pre nr > 0
+ * @post m0_reduce(i, nr, 0, + arr[i]) == sum
+ *
+ * @note It uses an UT-grade RNG.
+ */
+M0_INTERNAL void m0_ut_random_arr_with_sum(uint64_t *arr,
+					   uint64_t  nr,
+					   uint64_t  sum,
+					   uint64_t *seed);
+
+
+/** @} end of ut group */
+#endif /* __MERO_UT_MISC_H__ */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
+/*
+ * vim: tabstop=8 shiftwidth=8 noexpandtab textwidth=80 nowrap
+ */
