ae3c5bba68c14c1ae09008858c771795a776ee86
Dima Chumak <dmitriy.chumak@seagate.com>
2015-12-16 20:56:46 +0700

    st/m0d-fsync: ensure that mero services are stopped before test is finished
    Also, remove sudo, and enforce script to be executed with root privileges.
    
    Change-Id: I1fd749bd2c59daa7a05398f37109177636ec550c

--- st/m0d-fsync-test.sh
+++ st/m0d-fsync-test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+[[ $UID -eq 0 ]] || {
+    echo 'Please, run this script with "root" privileges.' >&2
+    exit 1
+}
+
+
 SANDBOX_DIR=${SANDBOX_DIR:-/var/mero/sandbox.fsync-st}
 M0_SRC_DIR=`readlink -f $0`
 M0_SRC_DIR=${M0_SRC_DIR%/*/*}
@@ -10,24 +16,26 @@ M0_SRC_DIR=${M0_SRC_DIR%/*/*}
 cd $M0_SRC_DIR
 
 echo "Installing Mero services"
-sudo scripts/install-mero-service -u
-sudo scripts/install-mero-service -l
-sudo utils/m0setup -v -P 12 -N 2 -K 1 -i 3 -d /var/mero/img -s 8 -c
-sudo utils/m0setup -v -P 12 -N 2 -K 1 -i 3 -d /var/mero/img -s 8
+scripts/install-mero-service -u
+rm -rf /etc/mero
+rm -f  /etc/sysconfig/mero
+scripts/install-mero-service -l
+utils/m0setup -v -P 12 -N 2 -K 1 -i 3 -d /var/mero/img -s 8 -c
+utils/m0setup -v -P 12 -N 2 -K 1 -i 3 -d /var/mero/img -s 8
 
 # update Mero configuration: turn on fdatasync with '-I' option
-sudo sed -i "s/.*MERO_M0D_EXTRA_OPTS.*/MERO_M0D_EXTRA_OPTS='-I'/" \
+sed -i "s/.*MERO_M0D_EXTRA_OPTS.*/MERO_M0D_EXTRA_OPTS='-I'/" \
      /etc/sysconfig/mero
 
 # update Mero configuration: set specific dir for test artifacts
-sudo sed -i "s@.*MERO_LOG_DIR.*@MERO_LOG_DIR=${SANDBOX_DIR}/log@" \
+sed -i "s@.*MERO_LOG_DIR.*@MERO_LOG_DIR=${SANDBOX_DIR}/log@" \
      /etc/sysconfig/mero
-sudo sed -i "s@.*MERO_M0D_DATA_DIR.*@MERO_M0D_DATA_DIR=${SANDBOX_DIR}/mero@" \
+sed -i "s@.*MERO_M0D_DATA_DIR.*@MERO_M0D_DATA_DIR=${SANDBOX_DIR}/mero@" \
      /etc/sysconfig/mero
 
 echo "Start Mero services"
-sudo systemctl start mero-mkfs
-sudo systemctl start mero-singlenode
+systemctl start mero-mkfs
+systemctl start mero-singlenode
 
 echo "Perform fsync test"
 for i in 0:1{0..9}0000; do touch /mnt/m0t1fs/$i & done
@@ -35,14 +43,19 @@ wait
 for i in 0:1{0..9}0000; do setfattr -n lid -v 8 /mnt/m0t1fs/$i & done
 wait
 for i in 0:1{0..9}0000; do dd if=/dev/zero of=/mnt/m0t1fs/$i \
-			      bs=8M count=20 conv=fsync & done
+    bs=8M count=20 conv=fsync & done
 wait
 
 echo "Tear down Mero services"
-sudo systemctl stop mero-singlenode
+systemctl stop mero-singlenode
+systemctl stop mero-kernel
 rc=$?
-sudo scripts/install-mero-service -u
+
+scripts/install-mero-service -u
+utils/m0setup -v -P 12 -N 2 -K 1 -i 3 -d /var/mero/img -s 8 -c
+
 if [ $rc -eq 0 ]; then
-    sudo rm -r $SANDBOX_DIR
+    rm -r $SANDBOX_DIR
 fi
+
 report_and_exit m0d-fsync $rc
