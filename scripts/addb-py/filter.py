0b5dee4cf68aec0bd1a24318de4bf94986642ce8
Nikita Danilov <nikita.danilov@seagate.com>
2016-03-30 01:01:34 +0300

    scripts/addb-py/filter.py: split input stream into output files.
    Split input stream into per-node,process,second output files. These files can
    them be re-combined (by cat) as desired.
    
    Change-Id: I1cd6987c4692681294fde62e04c6803525ec13a3

--- scripts/addb-py/filter.py
+++ scripts/addb-py/filter.py
@@ -8,12 +8,30 @@ import sys
 def filter(argv):
     tr = record.trace(height = 10, width = 1000, loc_nr = 1, duration = 1,
                       step = 1)
+    rec = ""
+    fname = ""
+    f = None
     for line in fileinput.input([]):
         params = line[1:].split()
         if line[0] == "*":
-            keep = record.keep(line.split()[2])
+            if rec != "":
+                name = "out." + node + "." + pid + "." + time
+                if name != fname:
+                    if f != None:
+                        f.close()
+                    f = open(name, "w+")
+                    fname = name
+                f.write(rec)
+                rec = ""
+            time = params[0][0:19]
+            keep = record.keep(params[1])
+        elif params[0] == "node":
+            node = params[1]
+        elif params[0] == "pid":
+            pid = params[1]
         if keep:
-            sys.stdout.write(line)
+            rec += line
+    f.close()
 
 if __name__ == "__main__":
     filter(sys.argv)
