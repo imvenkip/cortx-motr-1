879b86acdfd7a76f375701a2ee6e7793bd0824ce
Valery V. Vorotyntsev <valery.vorotyntsev@seagate.com>
2016-05-23 16:37:20 +0300

    [MERO-1720] conf: Check dev_idx against global pool width
    * Check m0_conf_sdev::sd_dev_idx against global pool width (total number
      of devices of IO services), not pver width (m0_conf_pver::pv_attr::pa_P).
    
    * Check m0_conf_service::cs_sdevs attribute: the list must not be empty
      for an IO service (type=2) and must be empty for a non-IO service.
    
    * _conf_filesystem_error(): Zero redundancy is valid for non-oostore mode.
      Don't check the value of m0_conf_filesystem::cf_mdpool in this case.
    
    * conf_fs_apply(), conf_filesystem_stats_get(): New helper functions.
    
    Fixes: MERO-1720
    Reviewed-by: Madhavrao Vemuri <madhav.vemuri@seagate.com>
    Change-Id: I851b8d2392c743e2ecb3cb0b8827f663b96db383

--- conf/validation.c
+++ conf/validation.c
@@ -34,6 +34,14 @@
 #include "lib/memory.h"    /* M0_ALLOC_ARR */
 #include "net/net.h"       /* m0_net_endpoint_is_valid */
 
+enum { CONF_GLOB_BATCH = 16 }; /* the value is arbitrary */
+
+/** @see conf_filesystem_stats_get() */
+struct conf_filesystem_stats {
+	uint32_t cs_nr_ioservices;
+	uint32_t cs_nr_iodevices;  /* pool width */
+};
+
 static const struct m0_conf_ruleset conf_rules;
 
 static const struct m0_conf_ruleset *conf_validity_checks[] = {
@@ -91,51 +99,6 @@ m0_conf_validation_error_locked(const struct m0_conf_cache *cache,
 	}
 	return NULL;
 }
-
-/* ------------------------------------------------------------------
- * Auxiliary functions used by conf validation rules
- * ------------------------------------------------------------------ */
-
-enum { CONF_GLOB_BATCH = 16 }; /* the value is arbitrary */
-
-/**
- * Counts services of given filesystem.
- *
- * If svc_type == 0, conf_services_count() returns the number of all services.
- * Otherwise only services of specified type are counted.
- *
- * Returns negative value in case of error.
- */
-static int conf_services_count(const struct m0_conf_filesystem *fs,
-			       enum m0_conf_service_type svc_type,
-			       char *buf, size_t buflen)
-{
-	struct m0_conf_glob       glob;
-	const struct m0_conf_obj *objv[CONF_GLOB_BATCH];
-	int                       rc;
-	int                       n = 0;
-
-	M0_PRE(0 <= svc_type && svc_type < M0_CST_NR);
-
-	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, fs->cf_obj.co_cache,
-			  &fs->cf_obj, M0_CONF_FILESYSTEM_NODES_FID,
-			  M0_CONF_ANY_FID, M0_CONF_NODE_PROCESSES_FID,
-			  M0_CONF_ANY_FID, M0_CONF_PROCESS_SERVICES_FID,
-			  M0_CONF_ANY_FID);
-	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0)
-		n += m0_count(i, rc, svc_type == 0 ||
-			      M0_CONF_CAST(objv[i], m0_conf_service)->cs_type ==
-			      svc_type);
-	if (rc < 0) {
-		(void)m0_conf_glob_error(&glob, buf, buflen);
-		return M0_ERR(rc);
-	}
-	return n;
-}
-
-/* ------------------------------------------------------------------
- * Conf validation rules
- * ------------------------------------------------------------------ */
 
 static char *conf_orphans_error(const struct m0_conf_cache *cache,
 				char *buf, size_t buflen)
@@ -154,52 +117,227 @@ static char *conf_orphans_error(const struct m0_conf_cache *cache,
 	return NULL;
 }
 
-static char *conf_filesystem_error(const struct m0_conf_cache *cache,
-				   char *buf, size_t buflen)
+/**
+ * Applies `func' to each m0_conf_filesystem object in the `cache'.
+ *
+ * Returns error message in case of error, NULL otherwise.
+ */
+static char *conf_fs_apply(char *(*func)(const struct m0_conf_filesystem *fs,
+					 char *buf, size_t buflen),
+			   const struct m0_conf_cache *cache,
+			   char *buf, size_t buflen)
 {
-	struct m0_conf_glob              glob;
-	const struct m0_conf_obj        *obj;
-	const struct m0_conf_filesystem *fs;
-	uint32_t                         redundancy;
-	int                              nr_ios;
-	int                              rc;
+	struct m0_conf_glob       glob;
+	const struct m0_conf_obj *obj;
+	char                     *err;
+	int                       rc;
 
 	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, cache, NULL,
 			  M0_CONF_ROOT_PROFILES_FID, M0_CONF_ANY_FID,
 			  M0_CONF_PROFILE_FILESYSTEM_FID);
 	while ((rc = m0_conf_glob(&glob, 1, &obj)) > 0) {
-		fs = M0_CONF_CAST(obj, m0_conf_filesystem);
-		redundancy = fs->cf_redundancy;
-		if (redundancy == 0)
-			return m0_vsnprintf(
-				buf, buflen, FID_F": `redundancy' is 0."
-				" Should be in (0, nr_ioservices] range",
-				FID_P(&obj->co_id));
-		nr_ios = conf_services_count(fs, M0_CST_IOS, buf, buflen);
-		if (nr_ios < 0)
-			return buf;
-		if (nr_ios == 0)
-			return m0_vsnprintf(buf, buflen,
-					    FID_F": No IO services",
-					    FID_P(&obj->co_id));
-		if (redundancy > nr_ios)
-			return m0_vsnprintf(
-				buf, buflen, FID_F": `redundancy' exceeds"
-				" the number of IO services (%u)",
-				FID_P(&obj->co_id), nr_ios);
-		obj = m0_conf_cache_lookup(cache, &fs->cf_mdpool);
-		if (obj == NULL)
-			return m0_vsnprintf(
-				buf, buflen, FID_F": `mdpool' "FID_F" is"
-				" missing", FID_P(&fs->cf_obj.co_id),
-				FID_P(&fs->cf_mdpool));
-		if (m0_conf_obj_grandparent(obj) != &fs->cf_obj)
-			return m0_vsnprintf(
-				buf, buflen, FID_F": `mdpool' "FID_F" belongs"
-				" another filesystem", FID_P(&fs->cf_obj.co_id),
-				FID_P(&fs->cf_mdpool));
+		err = func(M0_CONF_CAST(obj, m0_conf_filesystem), buf, buflen);
+		if (err != NULL)
+			return err;
 	}
 	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
+}
+
+static char *conf_filesystem_stats_get(const struct m0_conf_filesystem *fs,
+				       struct conf_filesystem_stats *stats,
+				       char *buf, size_t buflen)
+{
+	struct m0_conf_glob           glob;
+	const struct m0_conf_obj     *objv[CONF_GLOB_BATCH];
+	const struct m0_conf_service *svc;
+	int                           i;
+	int                           rc;
+
+	M0_SET0(stats);
+	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, fs->cf_obj.co_cache,
+			  &fs->cf_obj, M0_CONF_FILESYSTEM_NODES_FID,
+			  M0_CONF_ANY_FID, M0_CONF_NODE_PROCESSES_FID,
+			  M0_CONF_ANY_FID, M0_CONF_PROCESS_SERVICES_FID,
+			  M0_CONF_ANY_FID);
+	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0) {
+		for (i = 0; i < rc; ++i) {
+			svc = M0_CONF_CAST(objv[i], m0_conf_service);
+			if (svc->cs_type == M0_CST_IOS) {
+				++stats->cs_nr_ioservices;
+				stats->cs_nr_iodevices +=
+					m0_conf_dir_elems_count(svc->cs_sdevs);
+			}
+		}
+	}
+	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
+}
+
+static char *_conf_filesystem_error(const struct m0_conf_filesystem *fs,
+				    char *buf, size_t buflen)
+{
+	struct conf_filesystem_stats stats;
+	const struct m0_conf_obj    *obj;
+	char                        *err;
+
+	if (fs->cf_redundancy == 0)
+		/*
+		 * Non-oostore mode: meta-data is stored at MD services,
+		 * not IO services. No special MD pool is required.
+		 */
+		return NULL;
+
+	err = conf_filesystem_stats_get(fs, &stats, buf, buflen);
+	if (err != NULL)
+		return err;
+	if (stats.cs_nr_ioservices == 0)
+		return m0_vsnprintf(buf, buflen, FID_F": No IO services",
+				    FID_P(&fs->cf_obj.co_id));
+	if (fs->cf_redundancy > stats.cs_nr_ioservices)
+		return m0_vsnprintf(
+			buf, buflen, FID_F": `redundancy' (%u) exceeds"
+			" the number of IO services (%u)",
+			FID_P(&fs->cf_obj.co_id), fs->cf_redundancy,
+			stats.cs_nr_ioservices);
+	obj = m0_conf_cache_lookup(fs->cf_obj.co_cache, &fs->cf_mdpool);
+	if (obj == NULL)
+		return m0_vsnprintf(
+			buf, buflen, FID_F": `mdpool' "FID_F" is missing",
+			FID_P(&fs->cf_obj.co_id), FID_P(&fs->cf_mdpool));
+	if (m0_conf_obj_grandparent(obj) != &fs->cf_obj)
+		return m0_vsnprintf(
+			buf, buflen, FID_F": `mdpool' "FID_F" belongs another"
+			" filesystem", FID_P(&fs->cf_obj.co_id),
+			FID_P(&fs->cf_mdpool));
+	return NULL;
+}
+
+static char *conf_filesystem_error(const struct m0_conf_cache *cache,
+				   char *buf, size_t buflen)
+{
+	return conf_fs_apply(_conf_filesystem_error, cache, buf, buflen);
+}
+
+static char *conf_iodev_error(const struct m0_conf_sdev *sdev,
+			      const struct m0_conf_sdev **iodevs,
+			      uint32_t nr_iodevs, char *buf, size_t buflen)
+{
+	uint32_t j;
+
+	if (iodevs[sdev->sd_dev_idx] != sdev &&
+	    M0_CONF_CAST(m0_conf_obj_grandparent(&sdev->sd_obj),
+			 m0_conf_service)->cs_type == M0_CST_IOS) {
+		if (sdev->sd_dev_idx >= nr_iodevs)
+			return m0_vsnprintf(
+				buf, buflen, FID_F": dev_idx (%u) does not"
+				" belong [0, P) range; P=%u",
+				FID_P(&sdev->sd_obj.co_id), sdev->sd_dev_idx,
+				nr_iodevs);
+		if (iodevs[sdev->sd_dev_idx] != NULL)
+			return m0_vsnprintf(
+				buf, buflen, FID_F": dev_idx is not unique,"
+				" duplicates that of "FID_F,
+				FID_P(&sdev->sd_obj.co_id),
+				FID_P(&iodevs[sdev->sd_dev_idx]->sd_obj.co_id));
+		if (m0_exists(i, nr_iodevs, iodevs[j = i] != NULL &&
+			      m0_streq(iodevs[i]->sd_filename,
+				       sdev->sd_filename)))
+			return m0_vsnprintf(
+				buf, buflen, FID_F": filename is not unique,"
+				" duplicates that of "FID_F,
+				FID_P(&sdev->sd_obj.co_id),
+				FID_P(&iodevs[j]->sd_obj.co_id));
+		iodevs[sdev->sd_dev_idx] = sdev;
+	}
+	return NULL;
+}
+
+/**
+ * Checks that P attribute of the pool version (pver) does not exceed
+ * the total number of IO storage devices.
+ * Also validates .sd_dev_idx and .sd_filename attributes of IO storage
+ * devices, reachable from this pver.
+ */
+static char *conf_pver_error(const struct m0_conf_pver *pver,
+			     const struct m0_conf_sdev **iodevs,
+			     uint32_t nr_iodevs, char *buf, size_t buflen)
+{
+	struct m0_conf_glob        glob;
+	const struct m0_conf_obj  *objv[CONF_GLOB_BATCH];
+	const struct m0_conf_objv *diskv;
+	char                      *err;
+	int                        i;
+	int                        rc;
+
+	if (pver->pv_attr.pa_P > nr_iodevs)
+		return m0_vsnprintf(buf, buflen,
+				    FID_F": Pool width (%u) exceeds total"
+				    " number of IO devices (%u)",
+				    FID_P(&pver->pv_obj.co_id),
+				    pver->pv_attr.pa_P, nr_iodevs);
+	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, NULL, &pver->pv_obj,
+			  M0_CONF_PVER_RACKVS_FID, M0_CONF_ANY_FID,
+			  M0_CONF_RACKV_ENCLVS_FID, M0_CONF_ANY_FID,
+			  M0_CONF_ENCLV_CTRLVS_FID, M0_CONF_ANY_FID,
+			  M0_CONF_CTRLV_DISKVS_FID, M0_CONF_ANY_FID);
+	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0) {
+		for (i = 0; i < rc; ++i) {
+			diskv = M0_CONF_CAST(objv[i], m0_conf_objv);
+			err = conf_iodev_error(
+				M0_CONF_CAST(diskv->cv_real,
+					     m0_conf_disk)->ck_dev,
+				iodevs, nr_iodevs, buf, buflen);
+			if (err != NULL)
+				return err;
+		}
+	}
+	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
+}
+
+static char *
+_conf_pvers_error(const struct m0_conf_filesystem *fs, char *buf, size_t buflen)
+{
+	struct conf_filesystem_stats stats;
+	const struct m0_conf_sdev  **iodevs;
+	struct m0_conf_glob          glob;
+	const struct m0_conf_obj    *objv[CONF_GLOB_BATCH];
+	char                        *err = NULL;
+	int                          i;
+	int                          rc;
+
+	err = conf_filesystem_stats_get(fs, &stats, buf, buflen);
+	if (err != NULL)
+		return err;
+	if (stats.cs_nr_iodevices == 0)
+		return m0_vsnprintf(buf, buflen, FID_F": No IO devices",
+				    FID_P(&fs->cf_obj.co_id));
+
+	M0_ALLOC_ARR(iodevs, stats.cs_nr_iodevices);
+	if (iodevs == NULL)
+		return m0_vsnprintf(buf, buflen, "Insufficient memory");
+
+	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, NULL, &fs->cf_obj,
+			  M0_CONF_FILESYSTEM_POOLS_FID, M0_CONF_ANY_FID,
+			  M0_CONF_POOL_PVERS_FID, M0_CONF_ANY_FID);
+	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0) {
+		for (i = 0; i < rc; ++i) {
+			err = conf_pver_error(M0_CONF_CAST(objv[i],
+							   m0_conf_pver),
+					      iodevs, stats.cs_nr_iodevices,
+					      buf, buflen);
+			if (err != NULL)
+				break;
+		}
+	}
+	m0_free(iodevs);
+	if (err != NULL)
+		return err;
+	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
+}
+
+static char *
+conf_pvers_error(const struct m0_conf_cache *cache, char *buf, size_t buflen)
+{
+	return conf_fs_apply(_conf_pvers_error, cache, buf, buflen);
 }
 
 static char *conf_process_endpoint_error(const struct m0_conf_process *proc,
@@ -238,8 +376,9 @@ static char *
 conf_endpoint_error(const struct m0_conf_cache *cache, char *buf, size_t buflen)
 {
 	struct m0_conf_glob       glob;
-	const struct m0_conf_obj *obj;
+	const struct m0_conf_obj *objv[CONF_GLOB_BATCH];
 	char                     *err;
+	int                       i;
 	int                       rc;
 
 	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, cache, NULL,
@@ -247,17 +386,20 @@ conf_endpoint_error(const struct m0_conf_cache *cache, char *buf, size_t buflen)
 			  M0_CONF_PROFILE_FILESYSTEM_FID,
 			  M0_CONF_FILESYSTEM_NODES_FID, M0_CONF_ANY_FID,
 			  M0_CONF_NODE_PROCESSES_FID, M0_CONF_ANY_FID);
-	while ((rc = m0_conf_glob(&glob, 1, &obj)) > 0) {
-		err = conf_process_endpoint_error(
-			M0_CONF_CAST(obj, m0_conf_process), buf, buflen);
-		if (err != NULL)
-			return err;
+	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0) {
+		for (i = 0; i < rc; ++i) {
+			err = conf_process_endpoint_error(
+				M0_CONF_CAST(objv[i], m0_conf_process),
+				buf, buflen);
+			if (err != NULL)
+				return err;
+		}
 	}
 	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
 }
 
-static char *conf_profile_svc_type_error(const struct m0_conf_profile *prof,
-					 char *buf, size_t buflen)
+static char *_conf_service_type_error(const struct m0_conf_filesystem *fs,
+				      char *buf, size_t buflen)
 {
 	struct m0_conf_glob       glob;
 	const struct m0_conf_obj *objv[CONF_GLOB_BATCH];
@@ -267,8 +409,7 @@ static char *conf_profile_svc_type_error(const struct m0_conf_profile *prof,
 	int                       i;
 	int                       rc;
 
-	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, NULL, &prof->cp_obj,
-			  M0_CONF_PROFILE_FILESYSTEM_FID,
+	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, NULL, &fs->cf_obj,
 			  M0_CONF_FILESYSTEM_NODES_FID, M0_CONF_ANY_FID,
 			  M0_CONF_NODE_PROCESSES_FID, M0_CONF_ANY_FID,
 			  M0_CONF_PROCESS_SERVICES_FID, M0_CONF_ANY_FID);
@@ -291,136 +432,46 @@ static char *conf_profile_svc_type_error(const struct m0_conf_profile *prof,
 		return m0_conf_glob_error(&glob, buf, buflen);
 	if (confd_p && mds_p)
 		return NULL;
-	return m0_vsnprintf(buf, buflen, "No %s service defined for profile "
+	return m0_vsnprintf(buf, buflen, "No %s service defined for filesystem "
 			    FID_F, confd_p ? "meta-data" : "confd",
-			    FID_P(&prof->cp_obj.co_id));
+			    FID_P(&fs->cf_obj.co_id));
 }
 
-static char *
-conf_svc_type_error(const struct m0_conf_cache *cache, char *buf, size_t buflen)
+static char *conf_service_type_error(const struct m0_conf_cache *cache,
+				     char *buf, size_t buflen)
 {
-	struct m0_conf_glob       glob;
-	const struct m0_conf_obj *obj;
-	char                     *err;
-	int                       rc;
-
-	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, cache, NULL,
-			  M0_CONF_ROOT_PROFILES_FID, M0_CONF_ANY_FID);
-	while ((rc = m0_conf_glob(&glob, 1, &obj)) > 0) {
-		err = conf_profile_svc_type_error(
-			M0_CONF_CAST(obj, m0_conf_profile), buf, buflen);
-		if (err != NULL)
-			return err;
-	}
-	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
+	return conf_fs_apply(_conf_service_type_error, cache, buf, buflen);
 }
 
-static char *conf_io_dev_error(const struct m0_conf_sdev *sdev,
-			       uint32_t pool_width, uint32_t *nr_io_devs,
-			       const struct m0_conf_sdev **io_devs,
-			       char *buf, size_t buflen)
+static char *conf_service_sdevs_error(const struct m0_conf_cache *cache,
+				      char *buf, size_t buflen)
 {
-	uint32_t j;
-
-	if (M0_CONF_CAST(m0_conf_obj_grandparent(&sdev->sd_obj),
-			 m0_conf_service)->cs_type == M0_CST_IOS) {
-		M0_CNT_INC(*nr_io_devs);
-		if (sdev->sd_dev_idx >= pool_width)
-			return m0_vsnprintf(
-				buf, buflen, FID_F": dev_idx (%u) does not"
-				" belong [0, P) range; P=%u",
-				FID_P(&sdev->sd_obj.co_id), sdev->sd_dev_idx,
-				pool_width);
-		if (io_devs[sdev->sd_dev_idx] != NULL)
-			return m0_vsnprintf(
-				buf, buflen, FID_F": dev_idx is not unique,"
-				" duplicates that of "FID_F,
-				FID_P(&sdev->sd_obj.co_id),
-				FID_P(&io_devs[sdev->sd_dev_idx]->
-				      sd_obj.co_id));
-		if (m0_exists(i, pool_width, io_devs[j = i] != NULL &&
-			      m0_streq(io_devs[i]->sd_filename,
-				       sdev->sd_filename)))
-			return m0_vsnprintf(
-				buf, buflen, FID_F": filename is not unique,"
-				" duplicates that of "FID_F,
-				FID_P(&sdev->sd_obj.co_id),
-				FID_P(&io_devs[j]->sd_obj.co_id));
-		io_devs[sdev->sd_dev_idx] = sdev;
-	}
-	return NULL;
-}
-
-/**
- * Checks that P attribute of the pool version (pver) does not exceed
- * the number of IO storage devices, reachable from this pver.
- * Also validates dev_idx attribute of such storage devices.
- */
-static char *
-conf_pver_error(const struct m0_conf_pver *pver, char *buf, size_t buflen)
-{
-	struct m0_conf_glob         glob;
-	const struct m0_conf_obj   *objv[CONF_GLOB_BATCH];
-	const struct m0_conf_objv  *diskv;
-	const struct m0_conf_sdev **io_devs;
-	uint32_t                    nr_io_devs = 0;
-	char                       *err = NULL;
-	int                         i;
-	int                         rc;
-
-	M0_ALLOC_ARR(io_devs, pver->pv_attr.pa_P);
-	if (io_devs == NULL)
-		return m0_vsnprintf(buf, buflen, "Insufficient memory");
-
-	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, NULL, &pver->pv_obj,
-			  M0_CONF_PVER_RACKVS_FID, M0_CONF_ANY_FID,
-			  M0_CONF_RACKV_ENCLVS_FID, M0_CONF_ANY_FID,
-			  M0_CONF_ENCLV_CTRLVS_FID, M0_CONF_ANY_FID,
-			  M0_CONF_CTRLV_DISKVS_FID, M0_CONF_ANY_FID);
-	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0) {
-		for (i = 0; i < rc; ++i) {
-			diskv = M0_CONF_CAST(objv[i], m0_conf_objv);
-			err = conf_io_dev_error(
-				M0_CONF_CAST(diskv->cv_real,
-					     m0_conf_disk)->ck_dev,
-				pver->pv_attr.pa_P, &nr_io_devs, io_devs,
-				buf, buflen);
-			if (err != NULL)
-				goto out;
-		}
-	}
-out:
-	m0_free(io_devs);
-	if (rc < 0)
-		return m0_conf_glob_error(&glob, buf, buflen);
-	if (err != NULL)
-		return err;
-	return pver->pv_attr.pa_P > nr_io_devs ?
-		m0_vsnprintf(buf, buflen, FID_F": Pool width (%u) exceeds"
-			     " the number of IO devices (%u)",
-			     FID_P(&pver->pv_obj.co_id), pver->pv_attr.pa_P,
-			     nr_io_devs) :
-		NULL;
-}
-
-static char *conf_pvers_error(const struct m0_conf_cache *cache,
-			      char *buf, size_t buflen)
-{
-	struct m0_conf_glob       glob;
-	const struct m0_conf_obj *obj;
-	char                     *err;
-	int                       rc;
+	struct m0_conf_glob           glob;
+	const struct m0_conf_obj     *objv[CONF_GLOB_BATCH];
+	const struct m0_conf_service *svc;
+	int                           i;
+	int                           rc;
 
 	m0_conf_glob_init(&glob, M0_CONF_GLOB_ERR, NULL, cache, NULL,
 			  M0_CONF_ROOT_PROFILES_FID, M0_CONF_ANY_FID,
 			  M0_CONF_PROFILE_FILESYSTEM_FID,
-			  M0_CONF_FILESYSTEM_POOLS_FID, M0_CONF_ANY_FID,
-			  M0_CONF_POOL_PVERS_FID, M0_CONF_ANY_FID);
-	while ((rc = m0_conf_glob(&glob, 1, &obj)) > 0) {
-		err = conf_pver_error(M0_CONF_CAST(obj, m0_conf_pver),
-				      buf, buflen);
-		if (err != NULL)
-			return err;
+			  M0_CONF_FILESYSTEM_NODES_FID, M0_CONF_ANY_FID,
+			  M0_CONF_NODE_PROCESSES_FID, M0_CONF_ANY_FID,
+			  M0_CONF_PROCESS_SERVICES_FID, M0_CONF_ANY_FID);
+	while ((rc = m0_conf_glob(&glob, ARRAY_SIZE(objv), objv)) > 0) {
+		for (i = 0; i < rc; ++i) {
+			svc = M0_CONF_CAST(objv[i], m0_conf_service);
+			if ((svc->cs_type == M0_CST_IOS) ==
+			    m0_conf_dir_tlist_is_empty(
+				    &svc->cs_sdevs->cd_items))
+				return m0_vsnprintf(
+					buf, buflen,
+					FID_F": `sdevs' of %s be empty",
+					FID_P(&objv[i]->co_id),
+					(svc->cs_type == M0_CST_IOS ?
+					 "an IO service may not" :
+					 "a non-IO service must"));
+		}
 	}
 	return rc < 0 ? m0_conf_glob_error(&glob, buf, buflen) : NULL;
 }
@@ -432,7 +483,8 @@ static const struct m0_conf_ruleset conf_rules = {
 		_ENTRY(conf_orphans_error),
 		_ENTRY(conf_filesystem_error),
 		_ENTRY(conf_endpoint_error),
-		_ENTRY(conf_svc_type_error),
+		_ENTRY(conf_service_type_error),
+		_ENTRY(conf_service_sdevs_error),
 		_ENTRY(conf_pvers_error),
 #undef _ENTRY
 		{ NULL, NULL }
