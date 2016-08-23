9ff64289f3944a3267c24f610982f976616f5ebb
Dmytro Podgornyi <dmytro.podgornyi@seagate.com>
2016-08-23 03:10:50 +0300

    st/m0d-device-detach: fix m0spiel crash and other errors
    The system test starts rconfc, but doesn't stop it on an error. This
    leads to m0spiel segfault. Add spiel.rconfc_stop() to the error path.
    
    lctl fails when lnet module is not loaded. Move its usage below to let
    the module load first.
    
    Change directory to SANDBOX_DIR in order to store m0trace files there.
    
    References: MERO-1929
    Change-Id: I2376f4ab13a9dcc7aac6d1addda55fda85102f08

--- st/m0d-device-detach-test.sh
+++ st/m0d-device-detach-test.sh
@@ -10,42 +10,9 @@ M0_SRC_DIR=`readlink -f $0`
 M0_SRC_DIR=${M0_SRC_DIR%/*/*}
 
 # Number of test iterations
-ITER_NR=5
-
-LNET_NID=`lctl list_nids | head -1`
-SPIEL_ENDPOINT="$LNET_NID:12345:34:2001"
-HA_ENDPOINT="$LNET_NID:12345:45:1"
-M0_SPIEL_OPTS="-l $M0_SRC_DIR/mero/.libs/libmero.so --client $SPIEL_ENDPOINT \
-               --ha $HA_ENDPOINT"
+ITER_NR=10
 
 . $M0_SRC_DIR/utils/functions # report_and_exit
-
-function spiel_cmd {
-    $M0_SRC_DIR/utils/spiel/m0spiel $M0_SPIEL_OPTS <<EOF
-fids = {'profile'       : Fid(0x7000000000000001, 0),
-        'disk0'         : Fid(0x6b00000000000001, 2)
-}
-
-if spiel.cmd_profile_set(str(fids['profile'])):
-    sys.exit('cannot set profile {0}'.format(fids['profile']))
-
-if spiel.rconfc_start():
-    sys.exit('cannot start rconfc')
-
-device_commands = [('$1', fids['disk0'])]
-for command in device_commands:
-    try:
-        if getattr(spiel, command[0])(*command[1:]) != 0:
-            sys.exit("an error occurred while {0} executing, device fid\
-                     {1}".format(command[0], command[1]))
-    except:
-        sys.exit("an error occurred while {0} executing, device fid \
-                 {1}".format(command[0], command[1]))
-
-spiel.rconfc_stop()
-EOF
-    return $?
-}
 
 # install "mero" Python module required by m0spiel tool
 cd $M0_SRC_DIR/utils/spiel
@@ -73,6 +40,41 @@ systemctl start mero-singlenode
 sleep 10 # allow mero to finish its startup
 
 echo "Perform device-detach test"
+cd $SANDBOX_DIR
+
+LNET_NID=`lctl list_nids | head -1`
+SPIEL_ENDPOINT="$LNET_NID:12345:34:2001"
+HA_ENDPOINT="$LNET_NID:12345:45:1"
+M0_SPIEL_OPTS="-l $M0_SRC_DIR/mero/.libs/libmero.so --client $SPIEL_ENDPOINT \
+               --ha $HA_ENDPOINT"
+
+function spiel_cmd {
+    $M0_SRC_DIR/utils/spiel/m0spiel $M0_SPIEL_OPTS <<EOF
+fids = {'profile'       : Fid(0x7000000000000001, 0),
+        'disk0'         : Fid(0x6b00000000000001, 2)
+}
+
+if spiel.cmd_profile_set(str(fids['profile'])):
+    sys.exit('cannot set profile {0}'.format(fids['profile']))
+
+if spiel.rconfc_start():
+    sys.exit('cannot start rconfc')
+
+device_commands = [('$1', fids['disk0'])]
+for command in device_commands:
+    try:
+        if getattr(spiel, command[0])(*command[1:]) != 0:
+            sys.exit("an error occurred while {0} executing, device fid {1}"
+                     .format(command[0], command[1]))
+    except:
+        spiel.rconfc_stop()
+        sys.exit("an error occurred while {0} executing, device fid {1}"
+                 .format(command[0], command[1]))
+
+spiel.rconfc_stop()
+EOF
+    return $?
+}
 
 rc=0
 for I in $(seq 1 $ITER_NR); do
@@ -108,6 +110,7 @@ if [ $rc -eq 0 ]; then
     rc=$mero_rc
 fi
 
+cd $M0_SRC_DIR
 scripts/install-mero-service -u
 utils/m0setup -v -P 3 -N 1 -K 1 -i 1 -d /var/mero/img -s 8 -c
 
