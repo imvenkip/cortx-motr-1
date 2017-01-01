b4c42601af439f3b004e6877408973194a32cb21
Maxim Medved <max.medved@seagate.com>
2017-01-01 18:48:06 +0200

    sm/sm: don't skip poisoned ast, just panic
    Change-Id: Ib8aa8a4c01bc0765e468a7ddbb446839c54d9121

--- sm/sm.c
+++ sm/sm.c
@@ -119,9 +119,8 @@ M0_INTERNAL void m0_sm_asts_run(struct m0_sm_group *grp)
 		M0_ASSERT(ast->sa_next != NULL);
 
 		ast->sa_next = NULL;
-		if (m0_is_poisoned(ast->sa_cb)) {
-			M0_LOG(M0_FATAL, "Skipping poisoned ast.");
-		} else if (grp->s_addb2 == NULL) {
+		M0_ASSERT(!m0_is_poisoned(ast->sa_cb));
+		if (grp->s_addb2 == NULL) {
 			ast->sa_cb(grp, ast);
 		} else {
 			M0_ADDB2_TIMED(grp->s_addb2->ga_forq,
