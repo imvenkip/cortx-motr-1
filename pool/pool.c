02f9593f264fc10a9e7997d154eb0f28cda3dab8
Madhavrao Vemuri <madhav.vemuri@seagate.com>
2019-02-27 17:32:00 +0530

    MERO-2877 Fix a panic in entry point callback during active rm restart
    In multiple confd + rm setup, after active rm is restarted,
    new entrypoint is requested. Then in service_ctx_ha_entrypoint_cb()
    while comparing with the rm fid from entrypoint reply, old rm ctx is
    referred. When it is already cleaned up, it causes a panic.
    
    This is fixed by checking pc->pc_rm_ctx is NULL or not. If it is NULL
    then new context is directly created for the rm from entrpoint reply.
    
    Reviewed-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>
    
    Change-Id: I8a3a00a995fcf744798f63d0766e55f7e14b1aa7

--- pool/pool.c
+++ pool/pool.c
@@ -993,7 +993,8 @@ static bool service_ctx_ha_entrypoint_cb(struct m0_clink *clink)
 	if (state == M0_HEC_AVAILABLE &&
 	    rep->hae_control != M0_HA_ENTRYPOINT_QUERY &&
 	    m0_fid_is_set(&rep->hae_active_rm_fid) &&
-	    !m0_fid_eq(&pc->pc_rm_ctx->sc_fid, &rep->hae_active_rm_fid)) {
+	    (pc->pc_rm_ctx == NULL || !m0_fid_eq(&pc->pc_rm_ctx->sc_fid,
+						 &rep->hae_active_rm_fid))) {
 		m0_mutex_lock(&pc->pc_rm_lock);
 		pc->pc_rm_ctx = active_rm_ctx_find(pc);
 		M0_LOG(M0_DEBUG, "new RM="FID_F" pc_rm_ctx=%p",
